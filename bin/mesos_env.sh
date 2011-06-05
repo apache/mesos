c0ff251096bd7bc0949642cb61d608f70a2c390e
Benjamin Hindman <benh@apache.org>
2011-06-05 06:38:32 +0000

    Resimplifying the deploy scripts to work with conf file more intuitively (i.e. not assigning yet another set of defaults or defining conf vars is shell script that can be defined in conf file).
    Also breaking making start-mesos be heirarchical so that you can also just call start-master or start-slaves.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131936 13f79535-47bb-0310-9956-ffa450edef68

--- bin/mesos_env.sh
+++ bin/mesos_env.sh
@@ -4,15 +4,11 @@ bin=`cd "$bin"; pwd`
 echo bin is $bin
 
 # Set PATH to include Scala
-export PATH=$PATH:/root/scala-2.7.7.final/bin
+#export PATH=$PATH:/root/scala-2.7.7.final/bin
 
 #files that list master(s) and slaves
 MASTER=`cat $bin/master`
 SLAVES=`cat $bin/slaves`
-
-MASTER_PORT=9999
-MASTER_WEBUI_PORT=9090
-SLAVE_WEBUI_PORT=9091
 
 #The dir where Mesos deployment scripts live
 MESOS_ROOT=`cd $bin/..;pwd`
@@ -20,8 +16,8 @@ echo "MESOS_ROOT is $MESOS_ROOT"
 
 MESOS_HOME=`cd $bin/../src;pwd`
 
-export GOOGLE_LOG_DIR=$MESOS_HOME/logs
-export MESOS_LOGS=$MESOS_HOME/logs
+export GOOGLE_LOG_DIR=/tmp/mesos-logs
+export MESOS_LOGS=/tmp/mesos-output
 
 #the dir where Hadoop is installed
 HADOOP_HOME=/root/hadoop-0.20.2
@@ -31,12 +27,8 @@ JAVA_HOME=/usr/lib/jvm/java-6-sun
 
 #options for ssh'ing
 SSH_OPTS="-o stricthostkeychecking=no -o connecttimeout=2"
+SSH_OPTS+=" -i /fs-2/e/pzsavola/mesos/mesoskey"
 
-#LIBPROCESS_IP_GETTER
-
-#LIBPROCESS_IP_GETTER="echo $MASTER"
-
-#LIBPROCESS_IP_GETTER="hostname -i" #works on older versions of hostname, not on osx
-
-#FULL_IP="hostname --all-ip-addresses" # newer versions of hostname only
-#LIBPROCESS_IP_GETTER=`echo $FULL_IP|sed 's/\([^ ]*\) .*/\1/'`
+#LIBPROCESS_IP="hostname -i" #works on older versions of hostname, not on osx
+FULL_IP="hostname --all-ip-addresses" # newer versions of hostname only
+export LIBPROCESS_IP=`echo $FULL_IP|sed 's/\([^ ]*\) .*/\1/'`
