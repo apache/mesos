5f978432899dbd3c3963d3ae81fb32a56f72c42e
Benjamin Hindman <benh@apache.org>
2011-06-05 09:08:46 +0000

    Oops, forgot to add the new common/build.* files.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1132258 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/common/build.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+using std::string;
+
+
+namespace mesos { namespace internal { namespace build {
+
+extern const string DATE = BUILD_DATE;
+extern const string USER = BUILD_USER;
+extern const string FLAGS = BUILD_FLAGS;
+
+}}} // namespace mesos { namespace internal { namespace build {
---
+++ src/common/build.hpp
@@ -0,0 +1,10 @@
+#include <string>
+
+
+namespace mesos { namespace internal { namespace build {
+
+extern const std::string DATE;
+extern const std::string USER;
+extern const std::string FLAGS;
+
+}}} // namespace mesos { namespace internal { namespace build {
