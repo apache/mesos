71815f67d39b9f9f62ee983c1d9a8acf183c3fff
Benjamin Hindman <benh@apache.org>
2011-06-05 09:20:34 +0000

    Added an implementation of multimap that is quite a bit more user-friendly than the one in the STL. I'd prefer not to be duplicating/overriding data structures in the codebase, but the benefit that this version of multimap provides to code readability makes this case worthwhile.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1132292 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/tests/multimap_test.cpp
@@ -0,0 +1,124 @@
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include <common/foreach.hpp>
+#include <common/multimap.hpp>
+
+using std::string;
+
+using namespace mesos;
+using namespace mesos::internal;
+
+
+TEST(Multimap, Insert)
+{
+  multimap<string, uint16_t> map;
+
+  map.insert("foo", 1024);
+  ASSERT_EQ(1, map.count("foo"));
+
+  map.insert("foo", 1025);
+  ASSERT_EQ(2, map.count("foo"));
+
+  ASSERT_EQ(1, map.size());
+
+  map.insert("bar", 1024);
+  ASSERT_EQ(1, map.count("bar"));
+
+  map.insert("bar", 1025);
+  ASSERT_EQ(2, map.count("bar"));
+
+  ASSERT_EQ(2, map.size());
+}
+
+
+TEST(Multimap, Erase)
+{
+  multimap<string, uint16_t> map;
+
+  map.insert("foo", 1024);
+  map.erase("foo", 1024);
+  ASSERT_EQ(0, map.count("foo"));
+
+  ASSERT_EQ(0, map.size());
+
+  map.insert("foo", 1024);
+  map.insert("foo", 1025);
+  ASSERT_EQ(2, map.count("foo"));
+
+  ASSERT_EQ(1, map.size());
+
+  map.erase("foo");
+  ASSERT_EQ(0, map.count("foo"));
+  ASSERT_EQ(0, map.size());
+}
+
+
+TEST(Multimap, Count)
+{
+  multimap<string, uint16_t> map;
+
+  map.insert("foo", 1024);
+  map.insert("foo", 1025);
+  ASSERT_EQ(2, map.count("foo"));
+  ASSERT_EQ(1, map.count("foo", 1024));
+  ASSERT_EQ(1, map.count("foo", 1025));
+
+  map.insert("bar", 1024);
+  map.insert("bar", 1025);
+  ASSERT_EQ(2, map.count("bar"));
+  ASSERT_EQ(1, map.count("bar", 1024));
+  ASSERT_EQ(1, map.count("bar", 1025));
+}
+
+
+TEST(Multimap, Iterator)
+{
+  multimap<string, uint16_t> map;
+
+  map.insert("foo", 1024);
+  map.insert("foo", 1025);
+  ASSERT_EQ(2, map.count("foo"));
+  ASSERT_EQ(1, map.count("foo", 1024));
+  ASSERT_EQ(1, map.count("foo", 1025));
+
+  multimap<string, uint16_t>::iterator i = map.begin();
+
+  ASSERT_TRUE(i != map.end());
+
+  ASSERT_EQ("foo", i->first);
+  ASSERT_EQ(1024, i->second);
+
+  ++i;
+  ASSERT_TRUE(i != map.end());
+
+  ASSERT_EQ("foo", i->first);
+  ASSERT_EQ(1025, i->second);
+
+  ++i;
+  ASSERT_TRUE(i == map.end());
+}
+
+
+TEST(Multimap, Foreach)
+{
+  multimap<string, uint16_t> map;
+
+  map.insert("foo", 1024);
+  map.insert("bar", 1025);
+  ASSERT_EQ(1, map.count("foo"));
+  ASSERT_EQ(1, map.count("foo"));
+  ASSERT_EQ(1, map.count("foo", 1024));
+  ASSERT_EQ(1, map.count("bar", 1025));
+
+  foreachpair (const string& key, uint16_t value, map) {
+    if (key == "foo") {
+      ASSERT_EQ(1024, value);
+    } else if (key == "bar") {
+      ASSERT_EQ(1025, value);
+    } else {
+      FAIL() << "Unexpected key/value in multimap";
+    }
+  }
+}
