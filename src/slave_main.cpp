456f881593446055636b3bda67e6b484319c13d9
Benjamin Hindman <benh@apache.org>
2011-06-05 03:30:38 +0000

    bug fix needed to make everything compile
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131619 13f79535-47bb-0310-9956-ffa450edef68

--- src/slave_main.cpp
+++ src/slave_main.cpp
@@ -71,7 +71,7 @@ int main(int argc, char **argv)
 
   if (!quiet)
     google::SetStderrLogging(google::INFO);
-  else if (isFT)
+  else
     LeaderDetector::setQuiet(true);
 
   FLAGS_logbufsecs = 1;
