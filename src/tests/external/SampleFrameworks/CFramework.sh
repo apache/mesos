6c7ec700843cfe4852451b4969c6d187b311d1ac
Benjamin Hindman <benh@apache.org>
2011-06-05 08:22:11 +0000

    Fixed the C and C++ external tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1132001 13f79535-47bb-0310-9956-ffa450edef68

--- src/tests/external/SampleFrameworks/CFramework.sh
+++ src/tests/external/SampleFrameworks/CFramework.sh
@@ -4,4 +4,4 @@
 export MESOS_SLAVES=3
 
 # Check that the C test framework executes without crashing (returns 0).
-exec $MESOS_HOME/test-framework local
+exec $MESOS_HOME/bin/examples/test-framework local
--- src/tests/external/SampleFrameworks/CFrameworkInvalidEnv.sh
+++ src/tests/external/SampleFrameworks/CFrameworkInvalidEnv.sh
@@ -2,7 +2,7 @@
 
 # Run the C framework with an invalid slaves parameter (not an integer)
 # set through the environment, and check that it reports the error.
-MESOS_SLAVES=blah $MESOS_HOME/test-framework local > framework.out 2>&1
+MESOS_SLAVES=blah $MESOS_HOME/bin/examples/test-framework local > framework.out 2>&1
 if grep -e "Configuration error" framework.out; then
   exit 0
 else
