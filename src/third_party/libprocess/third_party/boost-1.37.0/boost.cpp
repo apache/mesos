a2bc0d11cea555b783fcaeb9e55f2716a82b0330
Benjamin Hindman <benh@apache.org>
2011-06-05 06:40:23 +0000

    Included more Boost headers.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131945 13f79535-47bb-0310-9956-ffa450edef68

--- src/third_party/libprocess/third_party/boost-1.37.0/boost.cpp
+++ src/third_party/libprocess/third_party/boost-1.37.0/boost.cpp
@@ -2,4 +2,7 @@
 #include <boost/variant/variant.hpp>
 #include <boost/variant/get.hpp>
 #include <boost/type_traits.hpp>
-#include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/unordered_set.hpp>
+#include <boost/bind.hpp>
