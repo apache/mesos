95691d63e2b3213117449be78cce2221fcf8da1e
Benjamin Hindman <benh@apache.org>
2011-06-05 06:44:30 +0000

    Re-structured tuples support (in particular, made all serialization be inline, removed the need for a Tuple class, made heavy use of Boost tuples including making the tuple type extend from Boost tuple.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131981 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/third_party/libprocess/tuples/details.hpp
@@ -0,0 +1,170 @@
+template <MSGID ID> class tuple;
+
+#undef IDENTITY
+#define IDENTITY(...) __VA_ARGS__
+
+#undef TUPLE
+#define TUPLE(ID, types)                                                \
+  template <> class tuple<ID> : public boost::tuple<IDENTITY types>     \
+  {                                                                     \
+  public:                                                               \
+    tuple(const boost::tuple<IDENTITY types> &t)                        \
+      : boost::tuple<IDENTITY types>(t) {}                              \
+                                                                        \
+    tuple(const std::string &data)                                      \
+    {                                                                   \
+      std::istringstream is(data);                                      \
+      process::tuples::deserializer d(is);                              \
+      deserialize(d, *this);                                            \
+    }                                                                   \
+                                                                        \
+    operator std::string () const                                       \
+    {                                                                   \
+      std::ostringstream os;                                            \
+      process::tuples::serializer s(os);                                \
+      serialize(s, *this);                                              \
+      return os.str();                                                  \
+    }                                                                   \
+  }
+
+
+inline void serialize(process::tuples::serializer &s,
+                      const boost::tuples::null_type &)
+{
+}
+
+
+template <typename H, typename T>
+inline void serialize(process::tuples::serializer &s,
+                      const boost::tuples::cons<H, T> &c)
+{
+  s & c.get_head();
+  serialize(s, c.get_tail());
+}
+
+
+inline void deserialize(process::tuples::deserializer &d,
+                        const boost::tuples::null_type &)
+{
+}
+
+
+template <typename H, typename T>
+inline void deserialize(process::tuples::deserializer &d,
+                        boost::tuples::cons<H, T> &c)
+{
+  d & c.get_head();
+  deserialize(d, c.get_tail());
+}
+
+
+template <MSGID ID>
+tuple<ID> pack()
+{
+  return tuple<ID>(::boost::make_tuple());
+}
+
+
+template <MSGID ID, typename T0>
+tuple<ID> pack(const T0 &t0)
+{
+  return tuple<ID>(::boost::make_tuple(t0));
+}
+
+
+template <MSGID ID, typename T0, typename T1>
+tuple<ID> pack(const T0 &t0, const T1 &t1)
+{
+  return tuple<ID>(::boost::make_tuple(t0, t1));
+}
+
+
+template <MSGID ID, typename T0, typename T1, typename T2>
+tuple<ID> pack(const T0 &t0, const T1 &t1, const T2 &t2)
+{
+  return tuple<ID>(::boost::make_tuple(t0, t1, t2));
+}
+
+
+template <MSGID ID,
+          typename T0, typename T1, typename T2, typename T3>
+tuple<ID> pack(const T0 &t0, const T1 &t1, const T2 &t2, const T3 &t3)
+{
+  return tuple<ID>(::boost::make_tuple(t0, t1, t2, t3));
+}
+
+
+template <MSGID ID,
+          typename T0, typename T1, typename T2, typename T3, typename T4>
+tuple<ID> pack(const T0 &t0, const T1 &t1, const T2 &t2, const T3 &t3,
+               const T4 &t4)
+{
+  return tuple<ID>(::boost::make_tuple(t0, t1, t2, t3, t4));
+}
+
+
+template <MSGID ID,
+          typename T0, typename T1, typename T2, typename T3, typename T4,
+          typename T5>
+tuple<ID> pack(const T0 &t0, const T1 &t1, const T2 &t2, const T3 &t3,
+               const T4 &t4, const T5 &t5)
+{
+  return tuple<ID>(::boost::make_tuple(t0, t1, t2, t3, t4, t5));
+}
+
+
+template <MSGID ID,
+          typename T0, typename T1, typename T2, typename T3, typename T4,
+          typename T5, typename T6>
+tuple<ID> pack(const T0 &t0, const T1 &t1, const T2 &t2, const T3 &t3,
+               const T4 &t4, const T5 &t5, const T6 &t6)
+{
+  return tuple<ID>(::boost::make_tuple(t0, t1, t2, t3, t4, t5, t6));
+}
+
+
+template <MSGID ID,
+          typename T0, typename T1, typename T2, typename T3, typename T4,
+          typename T5, typename T6, typename T7>
+tuple<ID> pack(const T0 &t0, const T1 &t1, const T2 &t2, const T3 &t3,
+               const T4 &t4, const T5 &t5, const T6 &t6, const T7 &t7)
+{
+  return tuple<ID>(::boost::make_tuple(t0, t1, t2, t3, t4, t5, t6, t7));
+}
+
+
+template <MSGID ID,
+          typename T0, typename T1, typename T2, typename T3, typename T4,
+          typename T5, typename T6, typename T7, typename T8>
+tuple<ID> pack(const T0 &t0, const T1 &t1, const T2 &t2, const T3 &t3,
+               const T4 &t4, const T5 &t5, const T6 &t6, const T7 &t7,
+               const T8 &t8)
+{
+  return tuple<ID>(::boost::make_tuple(t0, t1, t2, t3, t4, t5, t6, t7, t8));
+}
+
+
+template <MSGID ID,
+          typename T0, typename T1, typename T2, typename T3, typename T4,
+          typename T5, typename T6, typename T7, typename T8, typename T9>
+tuple<ID> pack(const T0 &t0, const T1 &t1, const T2 &t2, const T3 &t3,
+               const T4 &t4, const T5 &t5, const T6 &t6, const T7 &t7,
+               const T8 &t8, const T9 &t9)
+{
+  return tuple<ID>(::boost::make_tuple(t0, t1, t2, t3, t4, t5, t6, t7, t8, t9));
+}
+
+
+template <MSGID ID>
+tuple<ID> unpack(const std::string &data)
+{
+  return tuple<ID>(data);
+}
+
+
+template <MSGID ID, int N>
+typename boost::tuples::element<N, tuple<ID> >::type unpack(
+  const std::string &data)
+{
+  return boost::tuples::get<N>(unpack<ID>(data));
+}
---
+++ src/third_party/libprocess/tuples/tuples.hpp
@@ -0,0 +1,154 @@
+#ifndef TUPLES_HPP
+#define TUPLES_HPP
+
+#include <stdlib.h>
+
+#include <arpa/inet.h>
+
+#include <sstream>
+#include <string>
+#include <utility>
+
+#include <boost/tuple/tuple.hpp>
+
+
+namespace process { namespace tuples {
+
+/* TODO(benh): Check stream errors! Report errors! Ahhhh! */
+
+struct serializer
+{
+  std::ostringstream& stream;
+
+  serializer(std::ostringstream& s) : stream(s) {}
+
+  void operator & (const int32_t & i)
+  {
+    uint32_t netInt = htonl((uint32_t) i);
+    stream.write((char *) &netInt, sizeof(netInt));
+  }
+
+  void operator & (const int64_t & i)
+  {
+    uint32_t hiInt = htonl((uint32_t) (i >> 32));
+    uint32_t loInt = htonl((uint32_t) (i & 0xFFFFFFFF));
+    stream.write((char *) &hiInt, sizeof(hiInt));
+    stream.write((char *) &loInt, sizeof(loInt));
+  }
+
+#ifdef __APPLE__
+  void operator & (const intptr_t &i)
+  {
+    if (sizeof(intptr_t) == sizeof(int32_t))
+      *this & ((int32_t &) i);
+    else if (sizeof(intptr_t) == sizeof(int64_t))
+      *this & ((int64_t &) i);
+    else
+      abort();
+  }
+#endif
+
+  void operator & (const size_t &i)
+  {
+    if (sizeof(size_t) == sizeof(int32_t))
+      *this & ((int32_t &) i);
+    else if (sizeof(size_t) == sizeof(int64_t))
+      *this & ((int64_t &) i);
+    else
+      abort();
+  }
+
+  void operator & (const double &d)
+  {
+    // TODO(*): Deal with endian issues?
+    stream.write((char *) &d, sizeof(d));
+  }
+
+  void operator & (const std::string &s)
+  {
+    size_t size = s.size();
+    *this & (size);
+    stream.write(s.data(), size);
+  }
+
+  void operator & (const PID &pid)
+  {
+    *this & ((int32_t) pid.pipe);
+    *this & ((int32_t) pid.ip);
+    *this & ((int32_t) pid.port);
+  }
+};
+
+
+struct deserializer
+{
+  std::istringstream &stream;
+
+  deserializer(std::istringstream &s) : stream(s) {}
+
+  void operator & (int32_t &i)
+  {
+    uint32_t netInt;
+    stream.read((char *) &netInt, sizeof(netInt));
+    i = ntohl(netInt);
+  }
+
+  void operator & (int64_t &i)
+  {
+    uint32_t hiInt, loInt;
+    stream.read((char *) &hiInt, sizeof(hiInt));
+    stream.read((char *) &loInt, sizeof(loInt));
+    int64_t hi64 = ntohl(hiInt);
+    int64_t lo64 = ntohl(loInt);
+    i = (hi64 << 32) | lo64;
+  }
+
+#ifdef __APPLE__
+  void operator & (intptr_t &i)
+  {
+    if (sizeof(intptr_t) == sizeof(int32_t))
+      *this & ((int32_t &) i);
+    else if (sizeof(intptr_t) == sizeof(int64_t))
+      *this & ((int64_t &) i);
+    else
+      abort();
+  }
+#endif
+
+  void operator & (size_t &i)
+  {
+    if (sizeof(size_t) == sizeof(int32_t))
+      *this & ((int32_t &) i);
+    else if (sizeof(size_t) == sizeof(int64_t))
+      *this & ((int64_t &) i);
+    else
+      abort();
+  }
+
+  void operator & (double &d)
+  {
+    // TODO(*): Deal with endian issues?
+    stream.read((char *) &d, sizeof(d));
+  }
+
+  void operator & (std::string &s)
+  {
+    size_t size;
+    *this & (size);
+    s.resize(size);
+    stream.read((char *) s.data(), size);
+  }
+
+  void operator & (PID &pid)
+  {
+    *this & ((int32_t &) pid.pipe);
+    *this & ((int32_t &) pid.ip);
+    *this & ((int32_t &) pid.port);
+  }
+};
+
+
+}} /* namespace process { namespace tuples { */
+
+
+#endif /* TUPLES_HPP */
