b0d16fdb8b6f44916393a5baa81c4fd8c6568318
Benjamin Hindman <benh@apache.org>
2011-06-05 09:06:41 +0000

    Added a standalone Java multiple executors test, this isn't linked into the Makefile or testing infrastructure yet though.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1132244 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/examples/java/TestMultipleExecutorsFramework.java
@@ -0,0 +1,185 @@
+import com.google.protobuf.ByteString;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import mesos.*;
+import mesos.Protos.*;
+
+
+public class TestMultipleExecutorsFramework {
+  static class MyScheduler implements Scheduler {
+    int launchedTasks = 0;
+    int finishedTasks = 0;
+    int totalTasks = 5;
+
+    public MyScheduler() {}
+
+    public MyScheduler(int numTasks) {
+      totalTasks = numTasks;
+    }
+
+    @Override
+    public String getFrameworkName(SchedulerDriver driver) {
+      return "Java test framework";
+    }
+
+    @Override
+    public ExecutorInfo getExecutorInfo(SchedulerDriver driver) {
+      try {
+        File file = new File("./test_executor");
+        return ExecutorInfo.newBuilder()
+          .setExecutorId(ExecutorID.newBuilder().setValue("default").build())
+          .setUri(file.getCanonicalPath())
+          .build();
+      } catch (Throwable t) {
+        throw new RuntimeException(t);
+      }
+    }
+
+    @Override
+    public void registered(SchedulerDriver driver, FrameworkID frameworkId) {
+      System.out.println("Registered! ID = " + frameworkId.getValue());
+    }
+
+    @Override
+    public void resourceOffer(SchedulerDriver driver,
+                              OfferID offerId,
+                              List<SlaveOffer> offers) {
+
+      List<TaskDescription> tasks = new ArrayList<TaskDescription>();
+
+      try {
+        File file = new File("./test_executor");
+
+        for (SlaveOffer offer : offers) {
+          if (!fooLaunched) {
+            TaskID taskId = TaskID.newBuilder()
+              .setValue("foo")
+              .build();
+
+            System.out.println("Launching task " + taskId.getValue());
+
+            TaskDescription task = TaskDescription.newBuilder()
+              .setName("task " + taskId.getValue())
+              .setTaskId(taskId)
+              .setSlaveId(offer.getSlaveId())
+              .addResources(Resource.newBuilder()
+                            .setName("cpus")
+                            .setType(Resource.Type.SCALAR)
+                            .setScalar(Resource.Scalar.newBuilder()
+                                       .setValue(1)
+                                       .build())
+                            .build())
+              .addResources(Resource.newBuilder()
+                            .setName("mem")
+                            .setType(Resource.Type.SCALAR)
+                            .setScalar(Resource.Scalar.newBuilder()
+                                       .setValue(128)
+                                       .build())
+                            .build())
+              .setExecutor(ExecutorInfo.newBuilder()
+                           .setExecutorId(ExecutorID.newBuilder()
+                                          .setValue("executor-foo")
+                                          .build())
+                               .setUri(file.getCanonicalPath())
+                               .build())
+              .setData(ByteString.copyFromUtf8("100000"))
+              .build();
+
+            tasks.add(task);
+
+            fooLaunched = true;
+          }
+
+          if (!barLaunched) {
+            TaskID taskId = TaskID.newBuilder()
+              .setValue("bar")
+              .build();
+
+            System.out.println("Launching task " + taskId.getValue());
+
+            TaskDescription task = TaskDescription.newBuilder()
+              .setName("task " + taskId.getValue())
+              .setTaskId(taskId)
+              .setSlaveId(offer.getSlaveId())
+              .addResources(Resource.newBuilder()
+                            .setName("cpus")
+                            .setType(Resource.Type.SCALAR)
+                            .setScalar(Resource.Scalar.newBuilder()
+                                       .setValue(1)
+                                       .build())
+                            .build())
+              .addResources(Resource.newBuilder()
+                            .setName("mem")
+                            .setType(Resource.Type.SCALAR)
+                            .setScalar(Resource.Scalar.newBuilder()
+                                       .setValue(128)
+                                       .build())
+                            .build())
+              .setExecutor(ExecutorInfo.newBuilder()
+                           .setExecutorId(ExecutorID.newBuilder()
+                                          .setValue("executor-bar")
+                                          .build())
+                           .setUri(file.getCanonicalPath())
+                           .build())
+              .setData(ByteString.copyFrom("100000".getBytes()))
+              .build();
+
+            tasks.add(task);
+
+            barLaunched = true;
+          }
+
+          driver.replyToOffer(offerId, tasks);
+        }
+      } catch (Throwable t) {
+        throw new RuntimeException(t);
+      }
+    }
+
+    @Override
+    public void offerRescinded(SchedulerDriver driver, OfferID offerId) {}
+
+    @Override
+    public void statusUpdate(SchedulerDriver driver, TaskStatus status) {
+      System.out.println("Status update: task " + status.getTaskId() +
+                         " is in state " + status.getState());
+      if (status.getState() == TaskState.TASK_FINISHED) {
+        finishedTasks++;
+        System.out.println("Finished tasks: " + finishedTasks);
+        if (finishedTasks == totalTasks)
+          driver.stop();
+      }
+    }
+
+    @Override
+    public void frameworkMessage(SchedulerDriver driver, FrameworkMessage message) {}
+
+    @Override
+    public void slaveLost(SchedulerDriver driver, SlaveID slaveId) {}
+
+    @Override
+    public void error(SchedulerDriver driver, int code, String message) {
+      System.out.println("Error: " + message);
+    }
+
+    private boolean fooLaunched = false;
+    private boolean barLaunched = false;
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (args.length < 1 || args.length > 2) {
+      System.out.println("Invalid use: please specify a master");
+    } else if (args.length == 1) {
+      new MesosSchedulerDriver(new MyScheduler(),args[0]).run();
+    } else {
+      new MesosSchedulerDriver(new MyScheduler(Integer.parseInt(args[1])), args[0]).run();
+    }
+  }
+}
