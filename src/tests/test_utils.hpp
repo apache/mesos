5a163f07bd3effda0186ce84827c7e9a8216687a
Benjamin Hindman <benh@apache.org>
2011-06-05 05:36:45 +0000

    Expanding some documentation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131815 13f79535-47bb-0310-9956-ffa450edef68

--- src/tests/test_utils.hpp
+++ src/tests/test_utils.hpp
@@ -3,6 +3,9 @@
 
 #include <string>
 
-extern std::string MESOS_HOME; // Defined and initialized in main.cpp
+// The location where Mesos is installed, used by tests to locate various
+// frameworks and binaries. For now it points to the src directory, until
+// we clean up our directory structure a little. Initialized in main.cpp.
+extern std::string MESOS_HOME;
 
 #endif /* __TEST_UTILS_HPP__ */
