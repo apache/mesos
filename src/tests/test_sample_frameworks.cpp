d4bb77fb53bca0eccff46ba6d70df99ba864b456
Benjamin Hindman <benh@apache.org>
2011-06-05 05:47:37 +0000

    Pass MESOS_HAS_JAVA and MESOS_HAS_PYTHON through config.hpp rather than through -D flags in CXXFLAGS.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131908 13f79535-47bb-0310-9956-ffa450edef68

--- src/tests/test_sample_frameworks.cpp
+++ src/tests/test_sample_frameworks.cpp
@@ -1,15 +1,16 @@
 #include <gtest/gtest.h>
 
+#include "config.hpp"
 #include "external_test.hpp"
 
 // Run each of the sample frameworks in local mode
 TEST_EXTERNAL(SampleFrameworks, CFramework)
 TEST_EXTERNAL(SampleFrameworks, CppFramework)
-#ifdef MESOS_HAS_JAVA
+#if MESOS_HAS_JAVA
   TEST_EXTERNAL(SampleFrameworks, JavaFramework)
   TEST_EXTERNAL(SampleFrameworks, JavaExceptionFramework)
 #endif 
-#ifdef MESOS_HAS_PYTHON
+#if MESOS_HAS_PYTHON
   TEST_EXTERNAL(SampleFrameworks, PythonFramework)
 #endif
 
