0b6a401fb9c5ee53f8e6dea66a97ceb9cbcfc171
Benjamin Hindman <benh@apache.org>
2011-06-05 05:28:52 +0000

    My last commit removed the timeout on receive, which is bad as pointed out by benh
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131780 13f79535-47bb-0310-9956-ffa450edef68

--- src/slave.cpp
+++ src/slave.cpp
@@ -180,7 +180,7 @@ void Slave::operator () ()
   string data;
 
   while (true) {
-    switch (receive()) {
+    switch (receive(2)) {
       case NEW_MASTER_DETECTED: {
 	string masterSeq;
 	PID masterPid;
