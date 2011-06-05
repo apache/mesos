5b4672845b68fc3b1d2e138099e55f324f277d98
Benjamin Hindman <benh@apache.org>
2011-06-05 05:50:36 +0000

    Next round of deploy script updates. Inspired by R Cluster deployment.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131915 13f79535-47bb-0310-9956-ffa450edef68

--- bin/mesos_env.sh
+++ bin/mesos_env.sh
@@ -10,7 +10,9 @@ export PATH=$PATH:/root/scala-2.7.7.final/bin
 MASTER=`cat $bin/master`
 SLAVES=`cat $bin/slaves`
 
-MASTER_PORT=1111
+MASTER_PORT=9999
+MASTER_WEBUI_PORT=9090
+SLAVE_WEBUI_PORT=9091
 
 #The dir where Mesos deployment scripts live
 MESOS_ROOT=`cd $bin/..;pwd`
@@ -30,3 +32,11 @@ JAVA_HOME=/usr/lib/jvm/java-6-sun
 #options for ssh'ing
 SSH_OPTS="-o stricthostkeychecking=no -o connecttimeout=2"
 
+#LIBPROCESS_IP_GETTER
+
+#LIBPROCESS_IP_GETTER="echo $MASTER"
+
+#LIBPROCESS_IP_GETTER="hostname -i" #works on older versions of hostname, not on osx
+
+#FULL_IP="hostname --all-ip-addresses" # newer versions of hostname only
+#LIBPROCESS_IP_GETTER=`echo $FULL_IP|sed 's/\([^ ]*\) .*/\1/'`
