8dffeb83bab40bc095f72da9d8693fe62d078f48
Benjamin Hindman <benh@apache.org>
2011-06-05 06:37:55 +0000

    Renamed the executable and fixed an argv[0] bug
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131930 13f79535-47bb-0310-9956-ffa450edef68

--- src/get_conf.cpp
+++ src/get_conf.cpp
@@ -6,14 +6,14 @@ using namespace mesos::internal;
 
 int main(int argc, char **argv) {
   if (argc == 1) {
-    cout << "Usage:\nget_conf OPTION [arg1 arg2 ... ]\n";
+    cout << "Usage:  mesos-getconf OPTION [arg1 arg2 ... ]\n\n";
     cout << "OPTION\t\tis the name option whose value is to be extracted\n";
     cout << "arg1 ...\tOptional arguments passed to mesos, e.g. --quiet\n\n";
+    cout << "This utility gives shell scripts access Mesos parameters.\n\n";
     return 1;
   }
-  char **argv2 = &argv[1];
   Configurator conf;
-  conf.load(argc-1, argv2);
+  conf.load(argc, argv, true);
   string param(argv[1]);
   transform(param.begin(), param.end(), param.begin(), tolower);
   if (conf.getParams().contains(param)) {
