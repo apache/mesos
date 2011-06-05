7418550da4346460989134ea3e618767f91373ab
Benjamin Hindman <benh@apache.org>
2011-06-05 05:40:33 +0000

    Removed use of glog before InitGoogleLogging.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131849 13f79535-47bb-0310-9956-ffa450edef68

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <string>
 
+#include "fatal.hpp"
 #include "testing_utils.hpp"
 
 using namespace nexus::internal::test;
@@ -16,9 +17,8 @@ int main(int argc, char **argv) {
   // Get absolute path to Mesos home direcotry (really src right now)
   char buf[4096];
   if (realpath(dirname(argv[0]), buf) == 0)
-    PLOG(FATAL) << "Failed to find location of alltests using realpath";
+    fatalerror("Failed to find location of alltests using realpath");
   mesosHome = buf;
-  LOG(INFO) << "Mesos home is " << mesosHome;
 
   google::InitGoogleLogging("alltests");
   testing::InitGoogleTest(&argc, argv);
