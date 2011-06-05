e04540f8862b145c698a52628a7ca3a39a010ad1
Benjamin Hindman <benh@apache.org>
2011-06-05 05:47:23 +0000

    Using specialized templates to avoid RTTI typeid() calls to find out if a default parameter is of type bool.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1131906 13f79535-47bb-0310-9956-ffa450edef68

--- src/option.hpp
+++ src/option.hpp
@@ -6,7 +6,6 @@
 #include <fstream>
 #include <string>
 #include <map>
-#include <typeinfo>
 #include <glog/logging.h>
 #include "params.hpp"
 #include "foreach.hpp"
@@ -28,26 +27,18 @@ using boost::bad_lexical_cast;
  **/
 class ValidatorBase {
 public:
-  virtual bool isValid(const string& val) const = 0;
   virtual ValidatorBase* clone() const = 0;
   virtual bool isBool() const = 0;
-};
-
-
-/**
- * Validator that checks if a string can be cast to its templated type.
- **/
-template <class T>
-class Validator : public ValidatorBase {
-public:
-  Validator() {}
-
   /**
    * Checks if the provided string can be cast to a T.
    * @param val value associated with some option
    * @return true if val can be cast to a T, otherwise false.
    **/
-  virtual bool isValid(const string& val) const
+  virtual bool isValid(const string& val) const = 0;
+
+protected:
+  template <class T>
+  bool isValidInternal(const string& val) const
   {
     try {
       lexical_cast<T>(val);
@@ -57,17 +48,42 @@ public:
     }
     return true;
   }
+};
 
-  virtual ValidatorBase* clone() const
-  {
-    return new Validator<T>();
+
+/**
+ * Validator that checks if a string can be cast to its templated type.
+ **/
+template <typename T>
+class Validator : public ValidatorBase {
+public:
+  virtual bool isValid(const string& val) const { 
+    return isValidInternal<T>(val); 
+  }
+  
+  virtual bool isBool() const { return false; }
+
+  virtual ValidatorBase* clone() const  { 
+    return new Validator<T>(); 
+  }
+};
+
+
+/**
+ * Validator for bools that checks if a string can be cast to its templated type.
+ **/
+template <>
+class Validator <bool> : public ValidatorBase {
+public:
+  bool isValid(const string& val) const { 
+    return isValidInternal<bool>(val); 
   }
 
-  virtual bool isBool() const
-  {
-    return (typeid(bool) == typeid(T));
-  }
+  bool isBool() const { return true; }
 
+  ValidatorBase* clone() const { 
+    return new Validator<bool>(); 
+  }
 };
 
 
