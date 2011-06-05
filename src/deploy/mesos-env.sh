429036c654254a030550892a37ccab998c2a8528
Benjamin Hindman <benh@apache.org>
2011-06-05 08:23:16 +0000

    Further tweaks to deploy scripts
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1132008 13f79535-47bb-0310-9956-ffa450edef68

--- src/deploy/mesos-env.sh
+++ src/deploy/mesos-env.sh
@@ -3,21 +3,31 @@
 DEPLOY_DIR=`dirname "$0"`
 DEPLOY_DIR=`cd "$DEPLOY_DIR"; pwd`
 
-echo DEPLOY_DIR is $DEPLOY_DIR
-
-#files that list master(s) and slaves
-MASTER=`cat $DEPLOY_DIR/master`
-SLAVES=`cat $DEPLOY_DIR/slaves`
-
-#The dir where Mesos deployment scripts live
+# Locate MESOS_HOME relative to deploy directory
 MESOS_HOME=`cd "$DEPLOY_DIR/.."; pwd`
-echo "MESOS_HOME is $MESOS_HOME"
 
-MESOS_LOGS=$MESOS_HOME/logs
+# Find files that list master and slaves
+MASTER_FILE="$MESOS_HOME/conf/master"
+if [ -e "$MASTER_FILE" ]; then
+  MASTER=`cat "$MASTER_FILE"`
+else
+  echo "Error: $MASTER_FILE does not exist" >&2
+  exit 1
+fi
+SLAVES_FILE="$MESOS_HOME/conf/slaves"
+if [ -e "$SLAVES_FILE" ]; then
+  SLAVES=`cat "$SLAVES_FILE"`
+else
+  echo "Error: $SLAVES_FILE does not exist" >&2
+  exit 1
+fi
 
-#options for ssh'ing
+# Options for SSH
 SSH_OPTS="-o StrictHostKeyChecking=no -o ConnectTimeout=2"
 
-#LIBPROCESS_IP="hostname -i" #works on older versions of hostname, not on osx
+# Set LIBPROCESS_IP to change the address to which the master and slaves bind
+# if the default address chosen by the system is not the right one. We include
+# two examples below that try to resolve the IP from the node's hostname.
+#LIBPROCESS_IP="hostname -i" #works on older versions of hostname, not on OS X
 #FULL_IP="hostname --all-ip-addresses" # newer versions of hostname only
 #export LIBPROCESS_IP=`echo $FULL_IP|sed 's/\([^ ]*\) .*/\1/'`
