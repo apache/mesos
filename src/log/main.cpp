1c79430d13f249618ea15bc9c06b1948176b79fc
Benjamin Hindman <benh@apache.org>
2012-04-25 18:10:01 +0000

    Cleaned up log utility.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1330454 13f79535-47bb-0310-9956-ffa450edef68

--- src/log/main.cpp
+++ src/log/main.cpp
@@ -20,12 +20,14 @@
 #include <list>
 #include <string>
 
-#include <process/dispatch.hpp>
 #include <process/process.hpp>
 
-#include "common/fatal.hpp"
 #include "common/foreach.hpp"
-#include "common/result.hpp"
+#include "common/logging.hpp"
+#include "common/utils.hpp"
+
+#include "configurator/configurator.hpp"
+#include "configurator/configuration.hpp"
 
 #include "log/replica.hpp"
 
@@ -33,40 +35,76 @@ using namespace mesos;
 using namespace mesos::internal;
 using namespace mesos::internal::log;
 
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+void usage(const char* argv0, const Configurator& configurator)
+{
+  cerr << "Usage: " << utils::os::basename(argv0) << " [...] path/to/log"
+       << endl
+       << endl
+       << "Supported options:" << endl
+       << configurator.getUsage();
+}
+
 
 int main(int argc, char** argv)
 {
-  if (argc < 2) {
-    fatal("Usage: %s file <from> <to>", argv[0]);
+  Configurator configurator;
+  Logging::registerOptions(&configurator);
+
+  configurator.addOption<uint64_t>(
+      "from",
+      "Position from which to start reading in the log");
+
+  configurator.addOption<uint64_t>(
+      "to",
+      "Position from which to stop reading in the log");
+
+  configurator.addOption<bool>(
+      "help",
+      'h',
+      "Prints this usage message");
+
+  Configuration conf;
+  try {
+    conf = configurator.load(argc, argv);
+  } catch (const ConfigurationException& e) {
+    cerr << "Configuration error: " << e.what() << endl;
+    usage(argv[0], configurator);
+    exit(1);
   }
 
-  std::string file = argv[1];
-
-  process::initialize(true);
-
-  Replica replica(file);
-
-  uint64_t from, to;
-
-  if (argc != 4) {
-    process::Future<uint64_t> begin = replica.beginning();
-    process::Future<uint64_t> end = replica.ending();
-
-    begin.await();
-    end.await();
-
-    CHECK(begin.isReady());
-    CHECK(end.isReady());
-
-    from = begin.get();
-    to = end.get();
-  } else {
-    from = atoi(argv[2]);
-    to = atoi(argv[3]);
+  if (argc < 2 || conf.contains("help")) {
+    usage(argv[0], configurator);
+    exit(1);
   }
 
-  std::cout << std::endl << "Attempting to read the log from "
-            << from << " to " << to << std::endl << std::endl;
+  Logging::init(argv[0], conf);
+
+  string path = argv[argc - 1];
+
+  process::initialize(false);
+
+  Replica replica(path);
+
+  process::Future<uint64_t> begin = replica.beginning();
+  process::Future<uint64_t> end = replica.ending();
+
+  begin.await();
+  end.await();
+
+  CHECK(begin.isReady());
+  CHECK(end.isReady());
+
+  uint64_t from = conf.get<uint64_t>("from", begin.get());
+  uint64_t to = conf.get<uint64_t>("to", end.get());
+
+  cerr << endl << "Attempting to read the log from "
+       << from << " to " << to << endl << endl;
 
   process::Future<std::list<Action> > actions = replica.read(from, to);
 
@@ -77,7 +115,7 @@ int main(int argc, char** argv)
   CHECK(actions.isReady());
 
   foreach (const Action& action, actions.get()) {
-    std::cout << "----------------------------------------------" << std::endl;
+    cout << "----------------------------------------------" << endl;
     action.PrintDebugString();
   }
 
