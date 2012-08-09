94356288e81885e4971722f3c108c04dac083d88
Benjamin Hindman <benh@apache.org>
2012-08-09 18:09:39 +0000

    Some webui improvements (contributed by Ben Mahler, https://reviews.apache.org/r/6472).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1371352 13f79535-47bb-0310-9956-ffa450edef68

--- src/webui/master/static/controllers.js
+++ src/webui/master/static/controllers.js
@@ -1,5 +1,13 @@
 'use strict';
 
+function hasSelectedText () {
+  if (window.getSelection) {  // All browsers except IE before version 9.
+    var range = window.getSelection();
+    return range.toString().length > 0;
+  }
+  return false;
+}
+
 
 // Update the outermost scope with the new state.
 function update($scope, $defer, data) {
@@ -8,7 +16,6 @@ function update($scope, $defer, data) {
     return true; // Continue polling.
   }
 
-  $scope.data = data;
   $scope.state = $.parseJSON(data);
 
   // Determine if there is a leader (and redirect if not the leader).
@@ -37,6 +44,41 @@ function update($scope, $defer, data) {
     }
   }
 
+  // Check for selected text, and allow up to 20 seconds to pass before
+  // potentially wiping the user highlighted text.
+  // TODO(bmahler): This is to avoid the annoying loss of highlighting when
+  // the tables update. Once we can have tighter granularity control on the
+  // angular.js dynamic table updates, we should remove this hack.
+  $scope.time_since_update += $scope.delay;
+
+  if (hasSelectedText() && $scope.time_since_update < 20000) {
+    return true;
+  }
+
+  $scope.data = data;
+
+  // Update the maps.
+  $scope.slaves = {};
+  $scope.frameworks = {};
+  $scope.offers = {};
+  $scope.completed_frameworks = {};
+
+  _.each($scope.state.slaves, function(slave) {
+    $scope.slaves[slave.id] = slave;
+  });
+
+  _.each($scope.state.frameworks, function(framework) {
+    $scope.frameworks[framework.id] = framework;
+    _.each(framework.offers, function(offer) {
+      $scope.offers[offer.id] = offer;
+    });
+  });
+
+  _.each($scope.state.completed_frameworks, function(framework) {
+    $scope.completed_frameworks[framework.id] = framework;
+  });
+
+  // Update the stats.
   $scope.total_cpus = 0;
   $scope.total_mem = 0;
   $scope.used_cpus = 0;
@@ -44,48 +86,31 @@ function update($scope, $defer, data) {
   $scope.offered_cpus = 0;
   $scope.offered_mem = 0;
 
-  $scope.slaves = {};
-
-  _.each($scope.state.slaves, function(slave) {
+  _.each($scope.slaves, function(slave) {
     $scope.total_cpus += slave.resources.cpus;
     $scope.total_mem += slave.resources.mem;
-
-    $scope.slaves[slave.id] = slave;
   });
 
-  $scope.frameworks = {};
-  $scope.offers = {};
-
-  _.each($scope.state.frameworks, function(framework) {
+  _.each($scope.frameworks, function(framework) {
       $scope.used_cpus += framework.resources.cpus;
       $scope.used_mem += framework.resources.mem;
 
-      _.each(framework.offers, function(offer) {
-        $scope.offered_cpus = offer.resources.cpus;
-        $scope.offered_mem = offer.resources.mem;
-
-        $scope.offers[offer.id] = offer;
-      });
-
-      $scope.frameworks[framework.id] = framework;
-
+      framework.cpus_share = 0;
       if ($scope.total_cpus > 0) {
-        $scope.frameworks[framework.id].cpus_share =
-          framework.resources.cpus / $scope.total_cpus;
-      } else {
-        $scope.frameworks[framework.id].cpus_share = 0;
+        framework.cpus_share = framework.resources.cpus / $scope.total_cpus;
       }
 
+      framework.mem_share = 0;
       if ($scope.total_mem > 0) {
-        $scope.frameworks[framework.id].mem_share =
-          framework.resources.mem / $scope.total_mem;
-      } else {
-        $scope.frameworks[framework.id].mem_share = 0;
+        framework.mem_share = framework.resources.mem / $scope.total_mem;
       }
 
-      $scope.frameworks[framework.id].max_share =
-        Math.max($scope.frameworks[framework.id].cpus_share,
-                 $scope.frameworks[framework.id].mem_share);
+      framework.max_share = Math.max(framework.cpus_share, framework.mem_share);
+  });
+
+  _.each($scope.offers, function(offer) {
+    $scope.offered_cpus += offer.resources.cpus;
+    $scope.offered_mem += offer.resources.mem;
   });
 
   $scope.used_cpus -= $scope.offered_cpus;
@@ -94,16 +119,12 @@ function update($scope, $defer, data) {
   $scope.idle_cpus = $scope.total_cpus - ($scope.offered_cpus + $scope.used_cpus);
   $scope.idle_mem = $scope.total_mem - ($scope.offered_mem + $scope.used_mem);
 
-  $scope.completed_frameworks = {};
-
-  _.each($scope.state.completed_frameworks, function(framework) {
-      $scope.completed_frameworks[framework.id] = framework;
-  });
-
+  $scope.time_since_update = 0;
   $.event.trigger('state_updated');
 
   return true; // Continue polling.
 }
+
 
 // Main controller that can be used to handle "global" events. E.g.,:
 //     $scope.$on('$afterRouteChange', function() { ...; });
@@ -123,6 +144,7 @@ function MainCntl($scope, $http, $route, $routeParams, $location, $defer) {
   $scope.$location = $location;
   $scope.delay = 2000;
   $scope.retry = 0;
+  $scope.time_since_update = 0;
 
   var poll = function() {
     $http.get('master/state.json',
