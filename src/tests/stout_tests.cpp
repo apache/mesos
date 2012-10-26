cf66e2b1f00bec4ed9458255118f234d7bd75e3c
Benjamin Hindman <benh@apache.org>
2012-10-26 23:41:15 +0000

    Adding gzip compression in stout.
    From: Ben Mahler <benjamin.mahler@gmail.com>
    Review: https://reviews.apache.org/r/7675
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1402720 13f79535-47bb-0310-9956-ffa450edef68

--- src/tests/stout_tests.cpp
+++ src/tests/stout_tests.cpp
@@ -1,5 +1,6 @@
 #include <gmock/gmock.h>
 
+#include <cstdlib> // For rand.
 #include <map>
 #include <string>
 #include <vector>
@@ -9,6 +10,7 @@
 #include <stout/fatal.hpp>
 #include <stout/foreach.hpp>
 #include <stout/format.hpp>
+#include <stout/gzip.hpp>
 #include <stout/hashmap.hpp>
 #include <stout/hashset.hpp>
 #include <stout/json.hpp>
@@ -23,6 +25,8 @@
 #include <stout/strings.hpp>
 #include <stout/try.hpp>
 #include <stout/uuid.hpp>
+
+#include "tests/utils.hpp"
 
 #include "tests/utils.hpp"
 
@@ -621,3 +625,46 @@ TEST(StoutMultihashmapTest, Foreach)
     }
   }
 }
+
+
+#ifdef HAVE_LIBZ
+TEST(StoutCompressionTest, Gzip)
+{
+  // Test bad compression levels, outside of [-1, Z_BEST_COMPRESSION].
+  ASSERT_ERROR(gzip::compress("", -2));
+  ASSERT_ERROR(gzip::compress("", Z_BEST_COMPRESSION + 1));
+
+  string s =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
+    "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad "
+    "minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
+    "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit "
+    "in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
+    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui "
+    "officia deserunt mollit anim id est laborum.";
+
+  Try<string> compressed = gzip::compress(s);
+  ASSERT_SOME(compressed);
+  Try<string> decompressed = gzip::decompress(compressed.get());
+  ASSERT_SOME(decompressed);
+  ASSERT_EQ(s, decompressed.get());
+
+  // Test with a 1MB random string!
+  s = "";
+  while (s.length() < (1024 * 1024)) {
+    s.append(1, ' ' + (rand() % ('~' - ' ')));
+  }
+  compressed = gzip::compress(s);
+  ASSERT_SOME(compressed);
+  decompressed = gzip::decompress(compressed.get());
+  ASSERT_SOME(decompressed);
+  ASSERT_EQ(s, decompressed.get());
+
+  s = "";
+  compressed = gzip::compress(s);
+  ASSERT_SOME(compressed);
+  decompressed = gzip::decompress(compressed.get());
+  ASSERT_SOME(decompressed);
+  ASSERT_EQ(s, decompressed.get());
+}
+#endif // HAVE_LIBZ
