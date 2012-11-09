34c69e994dd132a2f6465b889af13ea21e2bd2e4
Benjamin Hindman <benh@apache.org>
2012-11-09 00:21:18 +0000

    Adding missing files to the distribution.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1407326 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/slave/constants.cpp
@@ -0,0 +1,35 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "slave/constants.hpp"
+
+namespace mesos {
+namespace internal {
+namespace slave {
+
+const Duration EXECUTOR_SHUTDOWN_GRACE_PERIOD = Seconds(5.0);
+const Duration STATUS_UPDATE_RETRY_INTERVAL = Seconds(10.0);
+const Duration GC_DELAY = Weeks(1.0);
+const Duration DISK_WATCH_INTERVAL = Minutes(1.0);
+const uint32_t MAX_COMPLETED_FRAMEWORKS = 50;
+const uint32_t MAX_COMPLETED_EXECUTORS_PER_FRAMEWORK = 150;
+const uint32_t MAX_COMPLETED_TASKS_PER_EXECUTOR = 200;
+
+} // namespace slave {
+} // namespace internal {
+} // namespace mesos {
