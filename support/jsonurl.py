1b8087fc2e9236b67b8f99710ab6750356790bd1
Benjamin Hindman <benh@apache.org>
2013-02-20 22:30:53 +0000

    Added a general comment describing jsonurl.py and updated the error handling of recursive key lookup to avoid printing the Python "stack trace" when there is an exception (i.e., more becoming of a utility).
    Review: https://reviews.apache.org/r/9376
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1448446 13f79535-47bb-0310-9956-ffa450edef68

--- support/jsonurl.py
+++ support/jsonurl.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+# A utility that expects JSON data at a particular URL and lets you
+# recursively extract keys from the JSON object as specified on the
+# command line (each argument on the command line after the first will
+# be used to recursively index into the JSON object). The name is a
+# play off of 'curl'.
+
 import json
 import sys
 import urllib2
@@ -9,7 +15,11 @@ url = sys.argv[1]
 data = json.loads(urllib2.urlopen(url).read())
 
 for arg in sys.argv[2:]:
-  temp = data[arg]
-  data = temp
+  try:
+    temp = data[arg]
+    data = temp
+  except KeyError:
+    print >> sys.stderr, "'" + arg + "' was not found"
+    sys.exit(1)
 
 print data
