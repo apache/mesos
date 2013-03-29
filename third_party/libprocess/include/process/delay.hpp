0a7a8fc29dcf865b836db56dc325417cd4a9fe10
Benjamin Hindman <benh@apache.org>
2013-03-29 22:41:03 +0000

    Simplified variadic 'delay' implementation.
    Review: https://reviews.apache.org/r/10177
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/mesos/trunk@1462665 13f79535-47bb-0310-9956-ffa450edef68

--- third_party/libprocess/include/process/delay.hpp
+++ third_party/libprocess/include/process/delay.hpp
@@ -7,6 +7,7 @@
 #include <process/timer.hpp>
 
 #include <stout/duration.hpp>
+#include <stout/preprocessor.hpp>
 
 namespace process {
 
@@ -55,194 +56,57 @@ Timer delay(const Duration& duration,
 }
 
 
-template <typename T, typename P1, typename A1>
-Timer delay(const Duration& duration,
-            const PID<T>& pid,
-            void (T::*method)(P1),
-            A1 a1)
-{
-  std::tr1::shared_ptr<std::tr1::function<void(T*)> > thunk(
-      new std::tr1::function<void(T*)>(
-          std::tr1::bind(method, std::tr1::placeholders::_1, a1)));
+#define TEMPLATE(Z, N, DATA)                                            \
+  template <typename T,                                                 \
+            ENUM_PARAMS(N, typename P),                                 \
+            ENUM_PARAMS(N, typename A)>                                 \
+  Timer delay(const Duration& duration,                                 \
+              const PID<T>& pid,                                        \
+              void (T::*method)(ENUM_PARAMS(N, P)),                     \
+              ENUM_BINARY_PARAMS(N, A, a))                              \
+  {                                                                     \
+    std::tr1::shared_ptr<std::tr1::function<void(T*)> > thunk(          \
+        new std::tr1::function<void(T*)>(                               \
+            std::tr1::bind(method,                                      \
+                           std::tr1::placeholders::_1,                  \
+                           ENUM_PARAMS(N, a))));                        \
+                                                                        \
+    std::tr1::shared_ptr<std::tr1::function<void(ProcessBase*)> > dispatcher( \
+        new std::tr1::function<void(ProcessBase*)>(                     \
+            std::tr1::bind(&internal::vdispatcher<T>,                   \
+                           std::tr1::placeholders::_1,                  \
+                           thunk)));                                    \
+                                                                        \
+    std::tr1::function<void(void)> dispatch =                           \
+      std::tr1::bind(internal::dispatch, pid, dispatcher);              \
+                                                                        \
+    return Timer::create(duration, dispatch);                           \
+  }                                                                     \
+                                                                        \
+  template <typename T,                                                 \
+            ENUM_PARAMS(N, typename P),                                 \
+            ENUM_PARAMS(N, typename A)>                                 \
+  Timer delay(const Duration& duration,                                 \
+              const Process<T>& process,                                \
+              void (T::*method)(ENUM_PARAMS(N, P)),                     \
+              ENUM_BINARY_PARAMS(N, A, a))                              \
+  {                                                                     \
+    return delay(duration, process.self(), method, ENUM_PARAMS(N, a));  \
+  }                                                                     \
+                                                                        \
+  template <typename T,                                                 \
+            ENUM_PARAMS(N, typename P),                                 \
+            ENUM_PARAMS(N, typename A)>                                 \
+  Timer delay(const Duration& duration,                                 \
+              const Process<T>* process,                                \
+              void (T::*method)(ENUM_PARAMS(N, P)),                     \
+              ENUM_BINARY_PARAMS(N, A, a))                              \
+  {                                                                     \
+    return delay(duration, process->self(), method, ENUM_PARAMS(N, a)); \
+  }
 
-  std::tr1::shared_ptr<std::tr1::function<void(ProcessBase*)> > dispatcher(
-      new std::tr1::function<void(ProcessBase*)>(
-          std::tr1::bind(&internal::vdispatcher<T>,
-                         std::tr1::placeholders::_1,
-                         thunk)));
-
-  std::tr1::function<void(void)> dispatch =
-    std::tr1::bind(internal::dispatch, pid, dispatcher);
-
-  return Timer::create(duration, dispatch);
-}
-
-
-template <typename T, typename P1, typename A1>
-Timer delay(const Duration& duration,
-            const Process<T>& process,
-            void (T::*method)(P1),
-            A1 a1)
-{
-  return delay(duration, process.self(), method, a1);
-}
-
-
-template <typename T, typename P1, typename A1>
-Timer delay(const Duration& duration,
-            const Process<T>* process,
-            void (T::*method)(P1),
-            A1 a1)
-{
-  return delay(duration, process->self(), method, a1);
-}
-
-
-template <typename T,
-          typename P1, typename P2,
-          typename A1, typename A2>
-Timer delay(const Duration& duration,
-            const PID<T>& pid,
-            void (T::*method)(P1, P2),
-            A1 a1, A2 a2)
-{
-  std::tr1::shared_ptr<std::tr1::function<void(T*)> > thunk(
-      new std::tr1::function<void(T*)>(
-          std::tr1::bind(method, std::tr1::placeholders::_1, a1, a2)));
-
-  std::tr1::shared_ptr<std::tr1::function<void(ProcessBase*)> > dispatcher(
-      new std::tr1::function<void(ProcessBase*)>(
-          std::tr1::bind(&internal::vdispatcher<T>,
-                         std::tr1::placeholders::_1,
-                         thunk)));
-
-  std::tr1::function<void(void)> dispatch =
-    std::tr1::bind(internal::dispatch, pid, dispatcher);
-
-  return Timer::create(duration, dispatch);
-}
-
-
-template <typename T,
-          typename P1, typename P2,
-          typename A1, typename A2>
-Timer delay(const Duration& duration,
-            const Process<T>& process,
-            void (T::*method)(P1, P2),
-            A1 a1, A2 a2)
-{
-  return delay(duration, process.self(), method, a1, a2);
-}
-
-
-template <typename T,
-          typename P1, typename P2,
-          typename A1, typename A2>
-Timer delay(const Duration& duration,
-            const Process<T>* process,
-            void (T::*method)(P1, P2),
-            A1 a1, A2 a2)
-{
-  return delay(duration, process->self(), method, a1, a2);
-}
-
-
-template <typename T,
-          typename P1, typename P2, typename P3,
-          typename A1, typename A2, typename A3>
-Timer delay(const Duration& duration,
-            const PID<T>& pid,
-            void (T::*method)(P1, P2, P3),
-            A1 a1, A2 a2, A3 a3)
-{
-  std::tr1::shared_ptr<std::tr1::function<void(T*)> > thunk(
-      new std::tr1::function<void(T*)>(
-          std::tr1::bind(method, std::tr1::placeholders::_1, a1, a2, a3)));
-
-  std::tr1::shared_ptr<std::tr1::function<void(ProcessBase*)> > dispatcher(
-      new std::tr1::function<void(ProcessBase*)>(
-          std::tr1::bind(&internal::vdispatcher<T>,
-                         std::tr1::placeholders::_1,
-                         thunk)));
-
-  std::tr1::function<void(void)> dispatch =
-    std::tr1::bind(internal::dispatch, pid, dispatcher);
-
-  return Timer::create(duration, dispatch);
-}
-
-
-template <typename T,
-          typename P1, typename P2, typename P3,
-          typename A1, typename A2, typename A3>
-Timer delay(const Duration& duration,
-            const Process<T>& process,
-            void (T::*method)(P1, P2, P3),
-            A1 a1, A2 a2, A3 a3)
-{
-  return delay(duration, process.self(), method, a1, a2, a3);
-}
-
-
-template <typename T,
-          typename P1, typename P2, typename P3,
-          typename A1, typename A2, typename A3>
-Timer delay(const Duration& duration,
-            const Process<T>* process,
-            void (T::*method)(P1, P2, P3),
-            A1 a1, A2 a2, A3 a3)
-{
-  return delay(duration, process->self(), method, a1, a2, a3);
-}
-
-
-template <typename T,
-          typename P1, typename P2, typename P3, typename P4,
-          typename A1, typename A2, typename A3, typename A4>
-Timer delay(const Duration& duration,
-            const PID<T>& pid,
-            void (T::*method)(P1, P2, P3, P4),
-            A1 a1, A2 a2, A3 a3, A4 a4)
-{
-  std::tr1::shared_ptr<std::tr1::function<void(T*)> > thunk(
-      new std::tr1::function<void(T*)>(
-          std::tr1::bind(method, std::tr1::placeholders::_1, a1, a2, a3, a4)));
-
-  std::tr1::shared_ptr<std::tr1::function<void(ProcessBase*)> > dispatcher(
-      new std::tr1::function<void(ProcessBase*)>(
-          std::tr1::bind(&internal::vdispatcher<T>,
-                         std::tr1::placeholders::_1,
-                         thunk)));
-
-  std::tr1::function<void(void)> dispatch =
-    std::tr1::bind(internal::dispatch, pid, dispatcher);
-
-  return Timer::create(duration, dispatch);
-}
-
-
-template <typename T,
-          typename P1, typename P2, typename P3, typename P4,
-          typename A1, typename A2, typename A3, typename A4>
-Timer delay(const Duration& duration,
-            const Process<T>& process,
-            void (T::*method)(P1, P2, P3, P4),
-            A1 a1, A2 a2, A3 a3, A4 a4)
-{
-  return delay(duration, process.self(), method, a1, a2, a3, a4);
-}
-
-
-template <typename T,
-          typename P1, typename P2, typename P3, typename P4,
-          typename A1, typename A2, typename A3, typename A4>
-Timer delay(const Duration& duration,
-            const Process<T>* process,
-            void (T::*method)(P1, P2, P3, P4),
-            A1 a1, A2 a2, A3 a3, A4 a4)
-{
-  return delay(duration, process->self(), method, a1, a2, a3, a4);
-}
+  REPEAT_FROM_TO(1, 11, TEMPLATE, _) // Args A0 -> A9.
+#undef TEMPLATE
 
 } // namespace process {
 
