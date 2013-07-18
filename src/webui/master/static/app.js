04d5d9b405ef98b1c668c211b81cce2626e55fc7
Benjamin Mahler <bmahler@twitter.com>
2013-07-18 16:13:29 -0700

    Replaced ID / timestamp popovers with copy-to-clipboard.
    From: Ross Allen <ross+rb@mesosphe.re>
    Review: https://reviews.apache.org/r/12730

--- src/webui/master/static/app.js
+++ src/webui/master/static/app.js
@@ -16,6 +16,9 @@ angular.module('mesos', ['ui.bootstrap']).
 
     $dialogProvider.options({dialogFade: true});
 
+    ZeroClipboard.setDefaults({
+      moviePath: '/static/obj/zeroclipboard-1.1.7.swf'
+    });
   }])
   .filter('truncateMesosID', function() {
     return function(id) {
@@ -64,12 +67,12 @@ angular.module('mesos', ['ui.bootstrap']).
       } else {
         return (bytes / (1024 * 1024 * 1024)).toFixed() + ' GB';
       }
-    }
+    };
   })
   // Defines the ui-if tag. This removes/adds an element from the DOM depending on a condition
   // Originally created by @tigbro, for the @jquery-mobile-angular-adapter
   // https://github.com/tigbro/jquery-mobile-angular-adapter
-  .directive('uiIf', [function () {
+  .directive('uiIf', [function() {
     return {
       transclude: 'element',
       priority: 1000,
@@ -102,6 +105,82 @@ angular.module('mesos', ['ui.bootstrap']).
             iterStartElement.parent().trigger("$childrenChanged");
           });
         };
+      }
+    };
+  }])
+  // Defines the 'clipboard' directive, which integrates copying to the user's
+  // clipboard with an Adobe Flash object via the ZeroClipboard library.
+  //
+  // Text to be copied on click is specified with the 'data-clipboard-text'
+  // attribute.
+  //
+  // The 'mouseenter' and 'mouseleave' events from the Flash object are exposed
+  // to the directive's element via the 'clipboardhover' event. There is no
+  // differentiation between enter/leave; they are both called 'clipboardhover'.
+  //
+  // Example:
+  //
+  //     <button class="btn btn-mini" clipboard
+  //         data-clipboard-text="I'm in your clipboard!">
+  //     </button>
+  //
+  // See: http://zeroclipboard.github.io/ZeroClipboard/
+  .directive('clipboard', [function() {
+    return {
+      restrict: 'A',
+      scope: true,
+      template: '<i class="icon-file"></i>',
+
+      link: function(scope, element, attrs) {
+        var clip = new ZeroClipboard(element[0]);
+
+        clip.on('mouseover', function() {
+          $(this).trigger('clipboardhover');
+        });
+
+        clip.on('mouseout', function() {
+          // TODO(ssorallen): Why is 'scope' incorrect here? It has to be
+          // retrieved from the element explicitly to be correct.
+          var elScope = angular.element(this).scope();
+
+          // Restore tooltip content to its original value if it was changed by
+          // this Clipboard instance.
+          if (elScope && elScope.tt_content_orig) {
+            elScope.tt_content = elScope.tt_content_orig;
+            delete elScope.tt_content_orig;
+          }
+
+          $(this).trigger('clipboardhover');
+        });
+
+        clip.on('complete', function() {
+          // TODO(ssorallen): Why is 'scope' incorrect here? It has to be
+          // retrieved from the element explicitly to be correct.
+          var elScope = angular.element(this).scope();
+
+          if (elScope) {
+            // Store the tooltip's original content so it can be restored when
+            // the tooltip is hidden.
+            elScope.tt_content_orig = elScope.tt_content;
+
+            // Angular UI's Tooltip sets content on the element's scope in a
+            // variable named 'tt_content'. The Tooltip has no public interface,
+            // so set the value directly here to change the value of the tooltip
+            // when content is successfully copied.
+            elScope.tt_content = 'copied!';
+            elScope.$apply();
+          }
+        });
+
+        clip.on('load', function() {
+          // The 'load' event fires only if the Flash file loads successfully.
+          // The copy buttons will only display if the class 'flash' exists
+          // on an ancestor.
+          //
+          // Browsers with no flash support will not append the 'flash' class
+          // and will therefore not see the copy buttons.
+          $('html').addClass('flash');
+        });
       }
     };
   }]);
