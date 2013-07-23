e88b1bde44e0efbe5121531a3c64293e3afd415a
Ross Allen <reallen@gmail.com>
2013-07-23 16:33:37 -0700

    Remove 'ui-if' direction in favor of 'ng-show'
    Angular provides 'ng-show' to hide elements when not needed. They are
    rendered in the DOM unlike with 'ui-if', but using native Angular
    prevents needing outside code or dependencies for a directive used in
    only one template.
    
    Review: http://reviews.apache.org/r/12888/

--- src/webui/master/static/js/app.js
+++ src/webui/master/static/js/app.js
@@ -79,45 +79,6 @@
         }
       };
     })
-    // Defines the ui-if tag. This removes/adds an element from the DOM depending on a condition
-    // Originally created by @tigbro, for the @jquery-mobile-angular-adapter
-    // https://github.com/tigbro/jquery-mobile-angular-adapter
-    .directive('uiIf', [function() {
-      return {
-        transclude: 'element',
-        priority: 1000,
-        terminal: true,
-        restrict: 'A',
-        compile: function (element, attr, linker) {
-          return function (scope, iterStartElement, attr) {
-            iterStartElement[0].doNotMove = true;
-            var expression = attr.uiIf;
-            var lastElement;
-            var lastScope;
-            scope.$watch(expression, function (newValue) {
-              if (lastElement) {
-                lastElement.remove();
-                lastElement = null;
-              }
-              if (lastScope) {
-                lastScope.$destroy();
-                lastScope = null;
-              }
-              if (newValue) {
-                lastScope = scope.$new();
-                linker(lastScope, function (clone) {
-                  lastElement = clone;
-                  iterStartElement.after(clone);
-                });
-              }
-              // Note: need to be parent() as jquery cannot trigger events on comments
-              // (angular creates a comment node when using transclusion, as ng-repeat does).
-              iterStartElement.parent().trigger("$childrenChanged");
-            });
-          };
-        }
-      };
-    }])
     // Defines the 'clipboard' directive, which integrates copying to the user's
     // clipboard with an Adobe Flash object via the ZeroClipboard library.
     //
