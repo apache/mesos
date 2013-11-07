39017657fc5f938f581dbffa636bb07c91ba1f11
Benjamin Hindman <benjamin.hindman@gmail.com>
2013-11-06 20:11:50 -1000

    Created 'usage' and moved 'get' helper to cli.py.
    Review: https://reviews.apache.org/r/15300

--- src/cli/python/mesos/cli.py
+++ src/cli/python/mesos/cli.py
@@ -1,4 +1,10 @@
-import subprocess
+# Helper for printing out a message and then the "usage" then exiting.
+def usage(message, parser):
+    import sys
+    sys.stderr.write(message + '\n')
+    parser.print_help()
+    sys.exit(-1)
+
 
 # Helper that uses 'mesos-resolve' to resolve a master IP:port from
 # one of:
@@ -6,19 +12,21 @@ import subprocess
 #     zk://username:password@host1:port1,host2:port2,.../path
 #     file://path/to/file (where file contains one of the above)
 def resolve(master):
-  process = subprocess.Popen(
-    ['mesos-resolve', master],
-    stdin=None,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    shell=False)
+    import subprocess
 
-  status = process.wait()
-  if status != 0:
-    raise Exception('Failed to execute \'mesos-resolve %s\':\n%s'
-                    % (master, process.stderr.read()))
+    process = subprocess.Popen(
+        ['mesos-resolve', master],
+        stdin=None,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=False)
 
-  result = process.stdout.read()
-  process.stdout.close()
-  process.stderr.close()
-  return result
+    status = process.wait()
+    if status != 0:
+        raise Exception('Failed to execute \'mesos-resolve %s\':\n%s'
+                        % (master, process.stderr.read()))
+
+    result = process.stdout.read()
+    process.stdout.close()
+    process.stderr.close()
+    return result
---
+++ src/cli/python/mesos/http.py
@@ -0,0 +1,27 @@
+# Helper for doing an HTTP GET given a PID, a path, and a query dict.
+# For example:
+#
+#     get('foo@1.2.3.4:123',
+#         '/endpoint',
+#         {'first': 'ben',
+#          'last': 'hindman'})
+#
+# Would yield: 1.2.3.4:123/endpoint?first='ben'&last='hindman'
+#
+# Note that you can also pass an IP:port (or hostname:port) for 'pid'
+# (i.e., you can omit the ID component of the PID, e.g., 'foo@').
+def get(pid, path, query=None):
+    import json
+    import urllib2
+
+    from contextlib import closing
+
+    url = 'http://' + pid[(pid.find('@') + 1):] + path
+
+    if query is not None and len(query) > 0:
+        url += '?' + '&'.join(
+            ['%s=%s' % (urllib2.quote(str(key)), urllib2.quote(str(value)))
+             for (key, value) in query.items()])
+
+    with closing(urllib2.urlopen(url)) as file:
+        return json.loads(file.read())
