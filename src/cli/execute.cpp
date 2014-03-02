73136daed511b22d0caab52bf05be8a3b26472f0
Till Toenshoff <toenshoff@me.com>
2014-03-01 18:53:29 -0800

    Fixed incorrect include of "common" headers in mesos-execute.
    Review: https://reviews.apache.org/r/18667

--- src/cli/execute.cpp
+++ src/cli/execute.cpp
@@ -30,8 +30,8 @@
 #include <stout/option.hpp>
 #include <stout/os.hpp>
 
-#include <common/type_utils.hpp>
-#include <common/protobuf_utils.hpp>
+#include "common/type_utils.hpp"
+#include "common/protobuf_utils.hpp"
 
 #include "hdfs/hdfs.hpp"
 
