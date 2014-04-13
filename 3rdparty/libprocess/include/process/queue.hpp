c7adb9d76ae4a667d74873690ab5b1a3fd3b1ad6
Benjamin Hindman <benjamin.hindman@gmail.com>
2014-04-12 19:17:57 -0600

    Added a concurrent 'Queue' to libprocess.
    Review: https://reviews.apache.org/r/20308

---
+++ 3rdparty/libprocess/include/process/queue.hpp
@@ -0,0 +1,87 @@
+#ifndef __PROCESS_QUEUE_HPP__
+#define __PROCESS_QUEUE_HPP__
+
+#include <deque>
+#include <queue>
+
+#include <process/future.hpp>
+#include <process/internal.hpp>
+#include <process/owned.hpp>
+
+#include <stout/memory.hpp>
+
+namespace process {
+
+template <typename T>
+class Queue
+{
+public:
+  Queue() : data(new Data()) {}
+
+  void put(const T& t)
+  {
+    Owned<Promise<T> > promise;
+
+    internal::acquire(&data->lock);
+    {
+      if (data->promises.empty()) {
+        data->elements.push(t);
+      } else {
+        promise = data->promises.front();
+        data->promises.pop_front();
+      }
+    }
+    internal::release(&data->lock);
+
+    if (promise.get() != NULL) {
+      promise->set(t);
+    }
+  }
+
+  Future<T> get()
+  {
+    Future<T> future;
+
+    internal::acquire(&data->lock);
+    {
+      if (data->elements.empty()) {
+        data->promises.push_back(Owned<Promise<T> >(new Promise<T>()));
+        future = data->promises.back()->future();
+      } else {
+        future = Future<T>(data->elements.front());
+        data->elements.pop();
+      }
+    }
+    internal::release(&data->lock);
+
+    return future;
+  }
+
+private:
+  struct Data
+  {
+    Data() : lock(0) {}
+
+    ~Data()
+    {
+      // TODO(benh): Fail promises?
+    }
+
+    // Rather than use a process to serialize access to the queue's
+    // internal data we use a low-level "lock" which we acquire and
+    // release using atomic builtins.
+    int lock;
+
+    // Represents "waiters" for elements from the queue.
+    std::deque<Owned<Promise<T> > > promises;
+
+    // Represents elements already put in the queue.
+    std::queue<T> elements;
+  };
+
+  memory::shared_ptr<Data> data;
+};
+
+} // namespace process {
+
+#endif // __PROCESS_QUEUE_HPP__
---
+++ 3rdparty/libprocess/src/tests/queue_tests.cpp
@@ -0,0 +1,78 @@
+#include <string>
+
+#include <gmock/gmock.h>
+
+#include <process/future.hpp>
+#include <process/queue.hpp>
+
+using namespace process;
+
+using std::string;
+
+TEST(Queue, block)
+{
+  Queue<string> q;
+
+  // A 'get' with an empty queue should block.
+  Future<string> get = q.get();
+
+  EXPECT_TRUE(get.isPending());
+
+  // After putting something the 'get' should be completed.
+  q.put("hello world");
+
+  EXPECT_TRUE(get.isReady());
+  EXPECT_EQ("hello world", get.get());
+}
+
+
+TEST(Queue, noblock)
+{
+  Queue<string> q;
+
+  // Doing a 'put' should cause a 'get' to be completed immediately.
+  q.put("world hello");
+
+  Future<string> get = q.get();
+
+  EXPECT_TRUE(get.isReady());
+  EXPECT_EQ("world hello", get.get());
+}
+
+
+TEST(Queue, queue)
+{
+  Queue<string> q;
+
+  // Multiple calls to 'get' should cause blocking until there have
+  // been enough corresponding calls to 'put'.
+  Future<string> get1 = q.get();
+  Future<string> get2 = q.get();
+  Future<string> get3 = q.get();
+
+  EXPECT_TRUE(get1.isPending());
+  EXPECT_TRUE(get2.isPending());
+  EXPECT_TRUE(get3.isPending());
+
+  q.put("hello");
+
+  EXPECT_TRUE(get1.isReady());
+  EXPECT_TRUE(get2.isPending());
+  EXPECT_TRUE(get3.isPending());
+
+  q.put("pretty");
+
+  EXPECT_TRUE(get1.isReady());
+  EXPECT_TRUE(get2.isReady());
+  EXPECT_TRUE(get3.isPending());
+
+  q.put("world");
+
+  EXPECT_TRUE(get1.isReady());
+  EXPECT_TRUE(get2.isReady());
+  EXPECT_TRUE(get3.isReady());
+
+  EXPECT_EQ("hello", get1.get());
+  EXPECT_EQ("pretty", get2.get());
+  EXPECT_EQ("world", get3.get());
+}
