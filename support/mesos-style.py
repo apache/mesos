5764fa54a3e01a502f40669cc9965439dffe76f9
Adam B <adam@mesosphere.io>
2014-04-25 10:15:54 -0700

    Updated mesos-style.py with already-passing rules.
    Review: https://reviews.apache.org/r/20649

--- support/mesos-style.py
+++ support/mesos-style.py
@@ -8,71 +8,16 @@ import re
 import subprocess
 import sys
 
-# Build active rules from list:
-#
-# build/class
-# build/deprecated
-# build/endif_comment
-# build/explicit_make_pair
-# build/forward_decl
-# build/header_guard
-# build/include
-# build/include_alpha
-# build/include_order
-# build/include_what_you_use
-# build/namespaces
-# build/printf_format
-# build/storage_class
-# legal/copyright
-# readability/alt_tokens
-# readability/braces
-# readability/casting
-# readability/check
-# readability/constructors
-# readability/fn_size
-# readability/function
-# readability/multiline_comment
-# readability/multiline_string
-# readability/namespace
-# readability/nolint
-# readability/streams
-# readability/todo
-# readability/utf8
-# runtime/arrays
-# runtime/casting
-# runtime/explicit
-# runtime/int
-# runtime/init
-# runtime/invalid_increment
-# runtime/member_string_references
-# runtime/memset
-# runtime/operator
-# runtime/printf
-# runtime/printf_format
-# runtime/references
-# runtime/rtti
-# runtime/sizeof
-# runtime/string
-# runtime/threadsafe_fn
-# whitespace/blank_line
-# whitespace/braces
-# whitespace/comma
-# whitespace/comments
-# whitespace/empty_loop_body
-# whitespace/end_of_line
-# whitespace/ending_newline
-# whitespace/forcolon
-# whitespace/indent
-# whitespace/labels
-# whitespace/line_length
-# whitespace/newline
-# whitespace/operators
-# whitespace/parens
-# whitespace/semicolon
-# whitespace/tab
-# whitespace/todo
-
-active_rules = ['build/class', 'build/deprecated', 'whitespace/tab']
+# See cpplint.py for full list of rules.
+active_rules = ['build/class',
+                'build/deprecated',
+                'build/endif_comment',
+                'runtime/vlog',
+                'whitespace/comma',
+                'whitespace/ending_newline',
+                'whitespace/forcolon',
+                'whitespace/tab',
+                'whitespace/todo']
 
 # Root source paths (will be traversed recursively).
 source_dirs = ['src',
