a8cb44346fa5101844d4de145354481f84417fb6
Till Toenshoff <toenshoff@me.com>
2014-05-25 23:26:28 +0200

    Added workaround to allow the ExternalContainerizer containers to get recovered via slave state.
    The slave currently expects a forked executor PID within its states
    when recovering. For the ExternalContainerizer however this does not
    make sense but is introduced in this patch to get around the problem
    as described in MESOS-1328 and MESOS-923.
    
    Review: https://reviews.apache.org/r/21677

--- src/slave/containerizer/external_containerizer.cpp
+++ src/slave/containerizer/external_containerizer.cpp
@@ -437,6 +437,35 @@ Future<Nothing> ExternalContainerizerProcess::launch(
                    "' failed: " + invoked.error());
   }
 
+  // Checkpoint the executor's pid if requested.
+  // NOTE: Containerizer(s) currently rely on their state being
+  // persisted in the slave. However, that responsibility should have
+  // been delegated to the containerizer.
+  // To work around the mandatory forked pid recovery, we need to
+  // checkpoint one. See MESOS-1328 and MESOS-923.
+  // TODO(tillt): Remove this entirely as soon as MESOS-923 is fixed.
+  if (checkpoint) {
+    const string& path = slave::paths::getForkedPidPath(
+        slave::paths::getMetaRootDir(flags.work_dir),
+        slaveId,
+        executor.framework_id(),
+        executor.executor_id(),
+        containerId);
+
+    LOG(INFO) << "Checkpointing executor's forked pid " << invoked.get().pid()
+              << " to '" << path <<  "'";
+
+    Try<Nothing> checkpointed =
+      slave::state::checkpoint(path, stringify(invoked.get().pid()));
+
+    if (checkpointed.isError()) {
+      LOG(ERROR) << "Failed to checkpoint executor's forked pid to '"
+                 << path << "': " << checkpointed.error();
+
+      return Failure("Could not checkpoint executor's pid");
+    }
+  }
+
   // Record the container launch intend.
   actives.put(containerId, Owned<Container>(new Container(sandbox)));
 
