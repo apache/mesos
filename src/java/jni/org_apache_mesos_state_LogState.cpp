61ce00fe8adda1e6883a5642bae93fdf54aa43f7
Benjamin Hindman <benjamin.hindman@gmail.com>
2014-08-05 13:19:15 -0700

    Added Java replicated log implementation of State.
    Review: https://reviews.apache.org/r/24341

---
+++ src/java/jni/org_apache_mesos_state_LogState.cpp
@@ -0,0 +1,114 @@
+#include <jni.h>
+
+#include <string>
+
+#include <stout/duration.hpp>
+
+#include "log/log.hpp"
+
+#include "state/state.hpp"
+#include "state/log.hpp"
+
+#include "construct.hpp"
+#include "convert.hpp"
+
+using namespace mesos::internal::log;
+using namespace mesos::internal::state;
+
+using std::string;
+
+extern "C" {
+
+/*
+ * Class:     org_apache_mesos_state_LogState
+ * Method:    initialize
+ * Signature: (Ljava/lang/String;JLjava/util/concurrent/TimeUnit;Ljava/lang/String;JLjava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_org_apache_mesos_state_LogState_initialize
+  (JNIEnv* env,
+   jobject thiz,
+   jstring jservers,
+   jlong jtimeout,
+   jobject junit,
+   jstring jznode,
+   jlong jquorum,
+   jstring jpath)
+{
+  string servers = construct<string>(env, jservers);
+
+  jclass clazz = env->GetObjectClass(junit);
+
+  // long seconds = unit.toSeconds(time);
+  jmethodID toSeconds = env->GetMethodID(clazz, "toSeconds", "(J)J");
+
+  jlong jseconds = env->CallLongMethod(junit, toSeconds, jtimeout);
+
+  Seconds timeout(jseconds);
+
+  string znode = construct<string>(env, jznode);
+
+  long quorum = jquorum;
+
+  string path = construct<string>(env, jpath);
+
+  // Create the Log instance using ZooKeeper.
+  Log* log = new Log(quorum, path, servers, timeout, znode);
+
+  // Create the C++ Storage and State instances and initialize the
+  // __storage and __state variables.
+  Storage* storage = new LogStorage(log);
+  State* state = new State(storage);
+
+  clazz = env->GetObjectClass(thiz);
+
+  jfieldID __log = env->GetFieldID(clazz, "__log", "J");
+  env->SetLongField(thiz, __log, (jlong) log);
+
+  clazz = env->GetSuperclass(clazz);
+
+  jfieldID __storage = env->GetFieldID(clazz, "__storage", "J");
+  env->SetLongField(thiz, __storage, (jlong) storage);
+
+  jfieldID __state = env->GetFieldID(clazz, "__state", "J");
+  env->SetLongField(thiz, __state, (jlong) state);
+}
+
+
+/*
+ * Class:     org_apache_mesos_state_LogState
+ * Method:    finalize
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_org_apache_mesos_state_LogState_finalize
+  (JNIEnv* env, jobject thiz)
+{
+  // TODO(benh): Consider calling 'finalize' on the super class
+  // instead of deleting 'state' and 'storage' here manually. We'd
+  // still need to delete 'log' ourselves, of course.
+
+  jclass clazz = env->GetObjectClass(thiz);
+
+  clazz = env->GetSuperclass(clazz);
+
+  jfieldID __state = env->GetFieldID(clazz, "__state", "J");
+
+  State* state = (State*) env->GetLongField(thiz, __state);
+
+  delete state;
+
+  jfieldID __storage = env->GetFieldID(clazz, "__storage", "J");
+
+  Storage* storage = (Storage*) env->GetLongField(thiz, __storage);
+
+  delete storage;
+
+  clazz = env->GetObjectClass(thiz);
+
+  jfieldID __log = env->GetFieldID(clazz, "__log", "J");
+
+  Log* log = (Log*) env->GetLongField(thiz, __log);
+
+  delete log;
+}
+
+} // extern "C" {
---
+++ src/java/src/org/apache/mesos/state/LogState.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.mesos.state;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Implementation of State that uses a replicated log to store
+ * variables/values.
+ */
+public class LogState extends AbstractState {
+  /**
+   * Constructs a new instance of LogState.
+   *
+   * @param servers List of ZooKeeper servers, e.g., 'ip1:port1,ip2:port2'.
+   * @param timeout ZooKeeper session timeout.
+   * @param unit    Unit for session timeout.
+   * @param znode   Path to znode where log replicas should be found.
+   * @param quorum  Number of replicas necessary to persist a write.
+   * @param path    Path the local replica uses to read/write data.
+   */
+  public LogState(String servers,
+                  long timeout,
+                  TimeUnit unit,
+                  String znode,
+                  long quorum,
+                  String path) {
+    initialize(servers, timeout, unit, znode, quorum, path);
+  }
+
+  protected native void initialize(String servers,
+                                   long timeout,
+                                   TimeUnit unit,
+                                   String znode,
+                                   long quorum,
+                                   String path);
+
+  protected native void finalize();
+
+  private long __log;
+}
