9257e768c780bb98749e39b3fcf05d6012deb21c
Vinod Kone <vinodkone@gmail.com>
2014-08-06 17:18:38 -0700

    Re-enabled low level scheduler tests.

--- src/tests/examples_tests.cpp
+++ src/tests/examples_tests.cpp
@@ -27,10 +27,10 @@ TEST_SCRIPT(ExamplesTest, NoExecutorFramework, "no_executor_framework_test.sh")
 
 // Disabled the low lever scheduler tests for now due to MESOS-1612
 // and MESOS-1614.
-//TEST_SCRIPT(ExamplesTest, LowLevelSchedulerLibprocess,
-//            "low_level_scheduler_libprocess_test.sh")
-//TEST_SCRIPT(ExamplesTest, LowLevelSchedulerPthread,
-//            "low_level_scheduler_pthread_test.sh")
+TEST_SCRIPT(ExamplesTest, LowLevelSchedulerLibprocess,
+            "low_level_scheduler_libprocess_test.sh")
+TEST_SCRIPT(ExamplesTest, LowLevelSchedulerPthread,
+            "low_level_scheduler_pthread_test.sh")
 
 #ifdef MESOS_HAS_JAVA
 TEST_SCRIPT(ExamplesTest, JavaFramework, "java_framework_test.sh")
