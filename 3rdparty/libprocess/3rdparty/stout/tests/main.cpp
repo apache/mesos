d665e526d897629a4f43f8893c278bd48bc1b989
Vinod Kone <vinodkone@gmail.com>
2014-08-13 15:31:06 -0700

    Added GLOG signal handler to print stack traces on segfault in stout.
    Review: https://reviews.apache.org/r/24675

--- 3rdparty/libprocess/3rdparty/stout/tests/main.cpp
+++ 3rdparty/libprocess/3rdparty/stout/tests/main.cpp
@@ -1,11 +1,17 @@
-#include <gtest/gtest.h>
+#include <glog/logging.h>
 
 #include <gmock/gmock.h>
+
+#include <gtest/gtest.h>
 
 int main(int argc, char** argv)
 {
   // Initialize Google Mock/Test.
   testing::InitGoogleMock(&argc, argv);
+
+  // Handles SIGSEGV, SIGILL, SIGFPE, SIGABRT, SIGBUS, SIGTERM
+  // by default.
+  google::InstallFailureSignalHandler();
 
   return RUN_ALL_TESTS();
 }
