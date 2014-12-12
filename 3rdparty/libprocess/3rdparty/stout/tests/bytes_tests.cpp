edab96ee5c1017a4c6d12e043266d58dfe67308f
Benjamin Mahler <benjamin.mahler@gmail.com>
2014-12-11 22:58:36 -0800

    Split apart the Bytes test.
    Review: https://reviews.apache.org/r/28984

--- 3rdparty/libprocess/3rdparty/stout/tests/bytes_tests.cpp
+++ 3rdparty/libprocess/3rdparty/stout/tests/bytes_tests.cpp
@@ -8,20 +8,51 @@
 #include <stout/try.hpp>
 
 
-TEST(Stout, Bytes)
+TEST(BytesTest, Parse)
 {
-  Try<Bytes> _1terabyte = Bytes::parse("1TB");
+  EXPECT_SOME_EQ(Terabytes(1), Bytes::parse("1TB"));
+  EXPECT_SOME_EQ(Gigabytes(1), Bytes::parse("1GB"));
+  EXPECT_SOME_EQ(Megabytes(1), Bytes::parse("1MB"));
+  EXPECT_SOME_EQ(Kilobytes(1), Bytes::parse("1KB"));
+  EXPECT_SOME_EQ(Bytes(1), Bytes::parse("1B"));
 
-  EXPECT_SOME_EQ(Terabytes(1), _1terabyte);
-  EXPECT_SOME_EQ(Gigabytes(1024), _1terabyte);
-  EXPECT_SOME_EQ(Megabytes(1024 * 1024), _1terabyte);
-  EXPECT_SOME_EQ(Kilobytes(1024 * 1024 * 1024), _1terabyte);
-  EXPECT_SOME_EQ(Bytes(1024LLU * 1024 * 1024 * 1024), _1terabyte);
+  // Cannot have fractional bytes.
+  EXPECT_ERROR(Bytes::parse("1.5B"));
+
+  // Parsing fractions is unsupported.
+  EXPECT_ERROR(Bytes::parse("1.5GB"));
+
+  // Unknown unit.
+  EXPECT_ERROR(Bytes::parse("1PB"));
+}
+
+
+TEST(Stout, Arithmetic)
+{
+  EXPECT_EQ(Terabytes(1), Gigabytes(512) + Gigabytes(512));
+  EXPECT_EQ(Terabytes(1), Terabytes(2) - Terabytes(1));
+
+  EXPECT_EQ(Terabytes(1), Gigabytes(1) * 1024);
+
+  EXPECT_EQ(Gigabytes(1), Terabytes(1) / 1024);
+}
+
+
+TEST(BytesTest, Comparison)
+{
+  EXPECT_GT(Terabytes(1), Gigabytes(1));
+  EXPECT_GT(Gigabytes(1), Megabytes(1));
+  EXPECT_GT(Megabytes(1), Kilobytes(1));
+  EXPECT_GT(Kilobytes(1), Bytes(1));
 
   EXPECT_EQ(Bytes(1024), Kilobytes(1));
   EXPECT_LT(Bytes(1023), Kilobytes(1));
   EXPECT_GT(Bytes(1025), Kilobytes(1));
+}
 
+
+TEST(BytesTest, Stringify)
+{
   EXPECT_NE(Megabytes(1023), Gigabytes(1));
 
   EXPECT_EQ("0B", stringify(Bytes()));
