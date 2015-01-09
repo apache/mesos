82bd2e6e653e817a65ad0ea463d882b27d6a9cf6
Alexander Rukletsov <alex@mesosphere.io>
2015-01-09 11:51:52 +0100

    Updated WhitelistWatcher behaviour and introduced initial whitelist.
    The subscriber may now provide an initial whitelist and will be
    notified only when the parsed whitelist differs from the initial one.
    The subscriber is not explicitly notified that there is no whitelist
    unless a valid initial whitelist has been provided before. This change
    suppresses gmock warnings for uninteresting mock function calls.
    
    Review: https://reviews.apache.org/r/28514

--- src/watcher/whitelist_watcher.hpp
+++ src/watcher/whitelist_watcher.hpp
@@ -31,16 +31,32 @@
 namespace mesos {
 namespace internal {
 
+// A whitelist may be (1) absent, (2) empty, (3) non-empty. The
+// watcher notifies the subscriber if the state of the whitelist
+// changes or if the contents changes in case the whitelist is in
+// state (3) non-empty.
 class WhitelistWatcher : public process::Process<WhitelistWatcher>
 {
 public:
+  // By default the initial policy is assumed to be permissive, i.e.
+  // initial whitelist is in state (1), meaning that peers are
+  // accepted before a whitelist is first loaded. In this case a
+  // subscriber is notified only if a set of peers (possibly empty)
+  // is loaded from the whitelist file.
+  // If a subscriber initially uses a nonpermissive policy, i.e.
+  // initial whitelist is in state (2) or (3), peers before first
+  // loaded whitelist are rejected. In this case a subscriber must
+  // provide the initial whitelist and will be notified if the policy
+  // becomes permissive (no whitelist file) or if a set of peers
+  // loaded from the whitelist file changes.
   // NOTE: The caller should ensure a callback exists throughout
   // WhitelistWatcher's lifetime.
   WhitelistWatcher(
       const std::string& path,
       const Duration& watchInterval,
       const lambda::function<
-        void(const Option<hashset<std::string>>& whitelist)>& subscriber);
+        void(const Option<hashset<std::string>>& whitelist)>& subscriber,
+      const Option<hashset<std::string>>& initialWhitelist = None());
 
 protected:
   virtual void initialize();
