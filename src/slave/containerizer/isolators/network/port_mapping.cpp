f855fb7f21552e9d44ec5ab4f6afaf51f1944dc4
Ian Downes <idownes@twitter.com>
2015-06-01 15:29:19 -0700

    Add missing argument for prepare() in network isolator.

--- src/slave/containerizer/isolators/network/port_mapping.cpp
+++ src/slave/containerizer/isolators/network/port_mapping.cpp
@@ -1795,6 +1795,7 @@ Future<Option<CommandInfo>> PortMappingIsolatorProcess::prepare(
     const ContainerID& containerId,
     const ExecutorInfo& executorInfo,
     const string& directory,
+    const Option<string>& rootfs,
     const Option<string>& user)
 {
   if (unmanaged.contains(containerId)) {
