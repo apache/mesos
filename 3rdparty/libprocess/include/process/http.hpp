feb1f568de837c881986d76543761349306aa8ba
Isabel Jimenez <contact@isabeljimenez.com>
2015-07-01 14:07:53 -0700

    Added a Conflict HTTP Response.
    Review: https://reviews.apache.org/r/35939

--- 3rdparty/libprocess/include/process/http.hpp
+++ 3rdparty/libprocess/include/process/http.hpp
@@ -452,6 +452,21 @@ struct NotAcceptable : Response
 };
 
 
+struct Conflict : Response
+{
+  Conflict()
+  {
+    status = "409 Conflict";
+  }
+
+  explicit Conflict(const std::string& body)
+    : Response(body)
+  {
+    status = "409 Conflict";
+  }
+};
+
+
 struct InternalServerError : Response
 {
   InternalServerError()
