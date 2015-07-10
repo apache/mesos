90ed30d4d9f99ad9a469e6aaf9fd0fc2ea781772
Vinod Kone <vinodkone@gmail.com>
2015-07-10 12:59:51 -0700

    Updated scheduler driver to send REVIVE call.
    Review: https://reviews.apache.org/r/36465

--- src/sched/sched.cpp
+++ src/sched/sched.cpp
@@ -1167,10 +1167,14 @@ protected:
       return;
     }
 
-    ReviveOffersMessage message;
-    message.mutable_framework_id()->MergeFrom(framework.id());
+    Call call;
+
+    CHECK(framework.has_id());
+    call.mutable_framework_id()->CopyFrom(framework.id());
+    call.set_type(Call::REVIVE);
+
     CHECK_SOME(master);
-    send(master.get(), message);
+    send(master.get(), call);
   }
 
   void acknowledgeStatusUpdate(
