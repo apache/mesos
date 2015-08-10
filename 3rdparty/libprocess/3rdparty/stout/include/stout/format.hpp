87611dda43bdf9c443a54882f2ec15021a14daef
Michael Park <mpark@apache.org>
2015-08-10 01:47:42 -0400

    Added a NOTE regarding undefined behavior of 'va_start'.

--- 3rdparty/libprocess/3rdparty/stout/include/stout/format.hpp
+++ 3rdparty/libprocess/3rdparty/stout/include/stout/format.hpp
@@ -79,6 +79,9 @@ inline Try<std::string> format(const std::string& fmt, va_list args)
 }
 
 
+// NOTE: 'fmt' cannot be 'const std::string&' because passing an
+// argument of reference type as the second argument of 'va_start'
+// results in undefined behavior.
 inline Try<std::string> format(const std::string fmt, ...)
 {
   va_list args;
