1d44ebac17b7bb1d6fb625f7ff1b9d23569574ef
Isabel Jimenez <contact@isabeljimenez.com>
2015-09-25 11:41:16 -0700

    Added missing header guards for master/validation.hpp.
    Review: https://reviews.apache.org/r/38619

--- src/master/validation.hpp
+++ src/master/validation.hpp
@@ -16,6 +16,9 @@
  * limitations under the License.
  */
 
+#ifndef __MASTER_VALIDATION_HPP__
+#define __MASTER_VALIDATION_HPP__
+
 #include <google/protobuf/repeated_field.h>
 
 #include <mesos/mesos.hpp>
@@ -133,3 +136,5 @@ Option<Error> validate(
 } // namespace master {
 } // namespace internal {
 } // namespace mesos {
+
+#endif // __MASTER_VALIDATION_HPP__
