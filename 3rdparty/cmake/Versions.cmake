e9c64fbe0a145c746402b1eee0c9116376bba016
Alex Clemmer <clemmer.alexander@gmail.com>
2015-09-27 15:41:24 -0700

    CMake: Added libevent version, configured Windows to use as default.
    Mesos will need to use libevent to build the process library on Windows.
    
    This commit will add a default version of libevent, which we will
    eventually retrieve from the 3rdparty GitHub repository, which for now
    is our "official" distribution channel for out-of-band third-party
    dependencies (especially on Windows, which has no package manager).
    
    Review: https://reviews.apache.org/r/38549

--- 3rdparty/cmake/Versions.cmake
+++ 3rdparty/cmake/Versions.cmake
@@ -4,5 +4,7 @@ set(GLOG_VERSION        "0.3.3")
 set(GMOCK_VERSION       "1.7.0")
 set(HTTP_PARSER_VERSION "1c3624a")
 set(LIBEV_VERSION       "4.15")
+# TODO(hausdorff): (MESOS-3529) transition this back to a non-beta version.
+set(LIBEVENT_VERSION    "2.1.5-beta")
 set(PICOJSON_VERSION    "1.3.0")
 set(PROTOBUF_VERSION    "2.5.0")
