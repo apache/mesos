b610deba0441b81acd77745b72540f257883fb7d
Alex Clemmer <clemmer.alexander@gmail.com>
2015-09-27 15:41:22 -0700

    CMake: Added `Versions.cmake` as an analog to `versions.am`.
    As we prepare to add more third-party dependencies, it will be helpful
    to have all the version information of our third-party dependencies in
    one place, rather than scattered everywhere in the project.
    
    This commit will introduce `Versions.cmake`, which will hold all this
    information.
    
    Review: https://reviews.apache.org/r/38541

---
+++ 3rdparty/cmake/Versions.cmake
@@ -0,0 +1,8 @@
+set(BOOST_VERSION       "1.53.0")
+set(CURL_VERSION        "7.43.0")
+set(GLOG_VERSION        "0.3.3")
+set(GMOCK_VERSION       "1.7.0")
+set(HTTP_PARSER_VERSION "1c3624a")
+set(LIBEV_VERSION       "4.15")
+set(PICOJSON_VERSION    "1.3.0")
+set(PROTOBUF_VERSION    "2.5.0")
