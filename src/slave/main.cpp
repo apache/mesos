3d726e0f24ca23597b188c4dbae196dc0c8135be
Neil Conway <neil.conway@gmail.com>
2015-10-05 18:59:56 -0700

    Fixed "--help" output for "--master" in mesos-slave.
    Review: https://reviews.apache.org/r/39034

--- src/slave/main.cpp
+++ src/slave/main.cpp
@@ -103,6 +103,7 @@ int main(int argc, char** argv)
   flags.add(&master,
             "master",
             "May be one of:\n"
+            "  host:port\n"
             "  zk://host1:port1,host2:port2,.../path\n"
             "  zk://username:password@host1:port1,host2:port2,.../path\n"
             "  file:///path/to/file (where file contains one of the above)");
