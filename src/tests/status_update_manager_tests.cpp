ebde436d0ea3c4723a9498deda522ec3a1672970
Vinod Kone <vinodkone@gmail.com>
2015-10-16 10:51:09 -0700

    Fixed style issue in status_update_manager_tests.cpp.

--- src/tests/status_update_manager_tests.cpp
+++ src/tests/status_update_manager_tests.cpp
@@ -924,7 +924,9 @@ TEST_F(StatusUpdateManagerTest, DuplicatedTerminalStatusUpdate)
   AWAIT_READY(_statusUpdateAcknowledgement2);
 
   // Verify the latest task status.
-  Future<process::http::Response> tasks = process::http::get(master.get(), "tasks");
+  Future<process::http::Response> tasks =
+    process::http::get(master.get(), "tasks");
+
   AWAIT_EXPECT_RESPONSE_STATUS_EQ(process::http::OK().status, tasks);
 
   AWAIT_EXPECT_RESPONSE_HEADER_EQ(
