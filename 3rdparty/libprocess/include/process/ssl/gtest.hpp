68619b0f6963884cde502176e43d3704d0fe9db6
Joseph Wu <joseph@mesosphere.io>
2015-10-28 14:38:33 -0500

    SSLTest refactor: Made SSLTest inherit from TemporaryDirectoryTest.
    This simplifies some of the cleanup logic, since temporary directories
    are cleaned up by the super class, or by the OS.
    
    Review: https://reviews.apache.org/r/39498

--- 3rdparty/libprocess/include/process/ssl/gtest.hpp
+++ 3rdparty/libprocess/include/process/ssl/gtest.hpp
@@ -21,6 +21,8 @@
 
 #ifdef USE_SSL_SOCKET
 
+#include <string>
+
 #include <openssl/rsa.h>
 #include <openssl/bio.h>
 #include <openssl/x509.h>
@@ -30,6 +32,13 @@
 #include <process/socket.hpp>
 
 #include <process/ssl/utilities.hpp>
+
+#include <stout/none.hpp>
+#include <stout/option.hpp>
+#include <stout/try.hpp>
+#include <stout/result.hpp>
+
+#include <stout/tests/utils.hpp>
 
 namespace process {
 namespace network {
@@ -57,7 +66,7 @@ void reinitialize();
  * SSLTest::launch_client that factor out common behavior used in
  * tests.
  */
-class SSLTest : public ::testing::Test
+class SSLTest : public TemporaryDirectoryTest
 {
 protected:
   SSLTest() : data("Hello World!") {}
@@ -65,176 +74,39 @@ protected:
   /**
    * @return The path to the authorized private key.
    */
-  static const Path& key_path()
+  Path key_path()
   {
-    static Path path(path::join(os::getcwd(), "key.pem"));
-    return path;
+    return Path(path::join(os::getcwd(), "key.pem"));
   }
 
   /**
    * @return The path to the authorized certificate.
    */
-  static const Path& certificate_path()
+  Path certificate_path()
   {
-    static Path path(path::join(os::getcwd(), "cert.pem"));
-    return path;
+    return Path(path::join(os::getcwd(), "cert.pem"));
   }
 
   /**
    * @return The path to the unauthorized private key.
    */
-  static const Path& scrap_key_path()
+  Path scrap_key_path()
   {
-    static Path path(path::join(os::getcwd(), "scrap_key.pem"));
-    return path;
+    return Path(path::join(os::getcwd(), "scrap_key.pem"));
   }
 
   /**
    * @return The path to the unauthorized certificate.
    */
-  static const Path& scrap_certificate_path()
+  Path scrap_certificate_path()
   {
-    static Path path(path::join(os::getcwd(), "scrap_cert.pem"));
-    return path;
-  }
-
-  static void cleanup_directories()
-  {
-    os::rm(key_path().value);
-    os::rm(certificate_path().value);
-    os::rm(scrap_key_path().value);
-    os::rm(scrap_certificate_path().value);
-  }
-
-  static void SetUpTestCase()
-  {
-    // We store the allocated objects in these results so that we can
-    // have a consolidated 'cleanup()' function. This makes all the
-    // 'EXIT()' calls more readable and less error prone.
-    Result<EVP_PKEY*> private_key = None();
-    Result<X509*> certificate = None();
-    Result<EVP_PKEY*> scrap_key = None();
-    Result<X509*> scrap_certificate = None();
-
-    auto cleanup = [&private_key, &certificate, &scrap_key, &scrap_certificate](
-        bool failure = true) {
-      if (private_key.isSome()) { EVP_PKEY_free(private_key.get()); }
-      if (certificate.isSome()) { X509_free(certificate.get()); }
-      if (scrap_key.isSome()) { EVP_PKEY_free(scrap_key.get()); }
-      if (scrap_certificate.isSome()) { X509_free(scrap_certificate.get()); }
-
-      // If we are under a failure condition, clean up any files we
-      // already generated. The expected behavior is that they will be
-      // cleaned up in 'TearDownTestCase()'; however, we call ABORT
-      // during 'SetUpTestCase()' failures.
-      if (failure) {
-        cleanup_directories();
-      }
-    };
-
-    // Generate the authority key.
-    private_key = process::network::openssl::generate_private_rsa_key();
-    if (private_key.isError()) {
-      ABORT("Could not generate private key: " + private_key.error());
-    }
-
-    // Figure out the hostname that 'INADDR_LOOPBACK' will bind to.
-    // Set the hostname of the certificate to this hostname so that
-    // hostname verification of the certificate will pass.
-    Try<std::string> hostname = net::getHostname(net::IP(INADDR_LOOPBACK));
-    if (hostname.isError()) {
-      cleanup();
-      ABORT("Could not determine hostname of 'INADDR_LOOPBACK': " +
-            hostname.error());
-    }
-
-    // Generate an authorized certificate.
-    certificate = process::network::openssl::generate_x509(
-        private_key.get(),
-        private_key.get(),
-        None(),
-        1,
-        365,
-        hostname.get());
-
-    if (certificate.isError()) {
-      cleanup();
-      ABORT("Could not generate certificate: " + certificate.error());
-    }
-
-    // Write the authority key to disk.
-    Try<Nothing> key_write =
-      process::network::openssl::write_key_file(private_key.get(), key_path());
-
-    if (key_write.isError()) {
-      cleanup();
-      ABORT("Could not write private key to disk: " + key_write.error());
-    }
-
-    // Write the authorized certificate to disk.
-    Try<Nothing> certificate_write =
-      process::network::openssl::write_certificate_file(
-          certificate.get(),
-          certificate_path());
-
-    if (certificate_write.isError()) {
-      cleanup();
-      ABORT("Could not write certificate to disk: " +
-            certificate_write.error());
-    }
-
-    // Generate a scrap key.
-    scrap_key = process::network::openssl::generate_private_rsa_key();
-    if (scrap_key.isError()) {
-      cleanup();
-      ABORT("Could not generate a scrap private key: " + scrap_key.error());
-    }
-
-    // Write the scrap key to disk.
-    key_write = process::network::openssl::write_key_file(
-        scrap_key.get(),
-        scrap_key_path());
-
-    if (key_write.isError()) {
-      cleanup();
-      ABORT("Could not write scrap key to disk: " + key_write.error());
-    }
-
-    // Generate a scrap certificate.
-    scrap_certificate = process::network::openssl::generate_x509(
-        scrap_key.get(),
-        scrap_key.get());
-
-    if (scrap_certificate.isError()) {
-      cleanup();
-      ABORT("Could not generate a scrap certificate: " +
-            scrap_certificate.error());
-    }
-
-    // Write the scrap certificate to disk.
-    certificate_write = process::network::openssl::write_certificate_file(
-        scrap_certificate.get(),
-        scrap_certificate_path());
-
-    if (certificate_write.isError()) {
-      cleanup();
-      ABORT("Could not write scrap certificate to disk: " +
-            certificate_write.error());
-    }
-
-    // Since we successfully set up all our state, we call cleanup
-    // with failure set to 'false'.
-    cleanup(false);
-  }
-
-  static void TearDownTestCase()
-  {
-    // Clean up all the pem files we generated.
-    cleanup_directories();
+    return Path(path::join(os::getcwd(), "scrap_cert.pem"));
   }
 
   virtual void SetUp()
   {
+    TemporaryDirectoryTest::SetUp();
+
     // This unsets all the SSL environment variables. Necessary for
     // ensuring a clean starting slate between tests.
     os::unsetenv("SSL_ENABLED");
@@ -251,6 +123,114 @@ protected:
     os::unsetenv("SSL_ENABLE_TLS_V1_0");
     os::unsetenv("SSL_ENABLE_TLS_V1_1");
     os::unsetenv("SSL_ENABLE_TLS_V1_2");
+
+    // We store the allocated objects in these results so that we can
+    // have a consolidated 'cleanup()' function. This makes all the
+    // 'EXIT()' calls more readable and less error prone.
+    Result<EVP_PKEY*> private_key = None();
+    Result<X509*> certificate = None();
+    Result<EVP_PKEY*> scrap_key = None();
+    Result<X509*> scrap_certificate = None();
+
+    auto cleanup = [&private_key, &certificate, &scrap_key, &scrap_certificate](
+        const Option<std::string> abort_message = None()) {
+      if (private_key.isSome()) { EVP_PKEY_free(private_key.get()); }
+      if (certificate.isSome()) { X509_free(certificate.get()); }
+      if (scrap_key.isSome()) { EVP_PKEY_free(scrap_key.get()); }
+      if (scrap_certificate.isSome()) { X509_free(scrap_certificate.get()); }
+
+      // We abort here because failure during setup indicates that something
+      // is horribly and irrecoverably wrong.
+      if (abort_message.isSome()) {
+        ABORT(abort_message.get());
+      }
+    };
+
+    // Generate the authority key.
+    private_key = process::network::openssl::generate_private_rsa_key();
+    if (private_key.isError()) {
+      cleanup("Could not generate private key: " + private_key.error());
+    }
+
+    // Figure out the hostname that 'INADDR_LOOPBACK' will bind to.
+    // Set the hostname of the certificate to this hostname so that
+    // hostname verification of the certificate will pass.
+    Try<std::string> hostname = net::getHostname(net::IP(INADDR_LOOPBACK));
+    if (hostname.isError()) {
+      cleanup("Could not determine hostname of 'INADDR_LOOPBACK': " +
+              hostname.error());
+    }
+
+    // Generate an authorized certificate.
+    certificate = process::network::openssl::generate_x509(
+        private_key.get(),
+        private_key.get(),
+        None(),
+        1,
+        365,
+        hostname.get());
+
+    if (certificate.isError()) {
+      cleanup("Could not generate certificate: " + certificate.error());
+    }
+
+    // Write the authority key to disk.
+    Try<Nothing> key_write =
+      process::network::openssl::write_key_file(private_key.get(), key_path());
+
+    if (key_write.isError()) {
+      cleanup("Could not write private key to disk: " + key_write.error());
+    }
+
+    // Write the authorized certificate to disk.
+    Try<Nothing> certificate_write =
+      process::network::openssl::write_certificate_file(
+          certificate.get(),
+          certificate_path());
+
+    if (certificate_write.isError()) {
+      cleanup("Could not write certificate to disk: " +
+              certificate_write.error());
+    }
+
+    // Generate a scrap key.
+    scrap_key = process::network::openssl::generate_private_rsa_key();
+    if (scrap_key.isError()) {
+      cleanup("Could not generate a scrap private key: " + scrap_key.error());
+    }
+
+    // Write the scrap key to disk.
+    key_write = process::network::openssl::write_key_file(
+        scrap_key.get(),
+        scrap_key_path());
+
+    if (key_write.isError()) {
+      cleanup("Could not write scrap key to disk: " + key_write.error());
+    }
+
+    // Generate a scrap certificate.
+    scrap_certificate = process::network::openssl::generate_x509(
+        scrap_key.get(),
+        scrap_key.get());
+
+    if (scrap_certificate.isError()) {
+      cleanup("Could not generate a scrap certificate: " +
+              scrap_certificate.error());
+    }
+
+    // Write the scrap certificate to disk.
+    certificate_write = process::network::openssl::write_certificate_file(
+        scrap_certificate.get(),
+        scrap_certificate_path());
+
+    if (certificate_write.isError()) {
+      cleanup("Could not write scrap certificate to disk: " +
+              certificate_write.error());
+    }
+
+    // Since we successfully set up all our state, we call cleanup
+    // without an abort message (so as not to abort).
+    cleanup();
   }
 
   /**
