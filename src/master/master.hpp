1d85aee1b1448af30b850bfa76e9d6e1f0414ec1
Felix Bechstein <felix.bechstein@otto.de>
2015-11-11 10:39:10 +0100

    MESOS-3307 Configurable size of completed task / framework history
    Running many frameworks makes mesos master becoming very slow.
    A huge state results in mesos-master occupying all of it's CPU just for generating the state.json blocking everything else.
    
    This change lets users limit the state size.

--- src/master/master.hpp
+++ src/master/master.hpp
@@ -1143,9 +1143,9 @@ private:
 
   struct Frameworks
   {
-    Frameworks() : completed(MAX_COMPLETED_FRAMEWORKS) {}
-
     hashmap<FrameworkID, Framework*> registered;
+
+    // Initialized via flags.
     boost::circular_buffer<std::shared_ptr<Framework>> completed;
 
     // Principals of frameworks keyed by PID.
@@ -1468,8 +1468,10 @@ struct Framework
       connected(true),
       active(true),
       registeredTime(time),
-      reregisteredTime(time),
-      completedTasks(MAX_COMPLETED_TASKS_PER_FRAMEWORK) {}
+      reregisteredTime(time)
+  {
+    completedTasks = boost::circular_buffer<std::shared_ptr<Task>>(master->flags.max_completed_tasks_per_framework);
+  }
 
   Framework(Master* const _master,
             const FrameworkInfo& _info,
@@ -1481,8 +1483,10 @@ struct Framework
       connected(true),
       active(true),
       registeredTime(time),
-      reregisteredTime(time),
-      completedTasks(MAX_COMPLETED_TASKS_PER_FRAMEWORK) {}
+      reregisteredTime(time)
+  {
+    completedTasks = boost::circular_buffer<std::shared_ptr<Task>>(master->flags.max_completed_tasks_per_framework);
+  }
 
   ~Framework()
   {
@@ -1820,6 +1824,7 @@ struct Framework
   // NOTE: We use a shared pointer for Task because clang doesn't like
   // Boost's implementation of circular_buffer with Task (Boost
   // attempts to do some memset's which are unsafe).
+  // Initialized via flags.
   boost::circular_buffer<std::shared_ptr<Task>> completedTasks;
 
   hashset<Offer*> offers; // Active offers for framework.
