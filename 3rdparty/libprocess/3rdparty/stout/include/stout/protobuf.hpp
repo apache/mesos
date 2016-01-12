d9db0363e16b9f1400f511d95c084f8c8ba264dc
Gilbert Song <songzihao1990@gmail.com>
2016-01-11 16:08:21 -0800

    Fixed stout's protobuf::parse to support JSON null values.
    Review: https://reviews.apache.org/r/42166/

--- 3rdparty/libprocess/3rdparty/stout/include/stout/protobuf.hpp
+++ 3rdparty/libprocess/3rdparty/stout/include/stout/protobuf.hpp
@@ -488,7 +488,10 @@ struct Parser : boost::static_visitor<Try<Nothing> >
 
   Try<Nothing> operator()(const JSON::Null&) const
   {
-    return Error("Not expecting a JSON null");
+    // We treat 'null' as an unset field. Note that we allow
+    // unset required fields here since the top-level parse
+    // function is responsible for checking 'IsInitialized'.
+    return Nothing();
   }
 
 private:
