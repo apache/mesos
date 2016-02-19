0c39033fcf1e03963b150c17ef1aad30a865e6d5
James Peach <jpeach@apache.org>
2016-02-19 10:23:47 -0500

    Accept the 3-field version of HDFS du output.
    The HDFS du command can output 3 fields in later Hadoop versions.
    We should accept both 2-field and 3-field versions for better
    compatibility.
    
    Review: https://reviews.apache.org/r/42370/

--- src/hdfs/hdfs.hpp
+++ src/hdfs/hdfs.hpp
@@ -130,9 +130,13 @@ struct HDFS
     foreach (const std::string& line, strings::tokenize(out.get(), "\n")) {
       // Note that we use tokenize() rather than split() since fields can be
       // delimited by multiple spaces.
-      std::vector<std::string> fields = strings::tokenize(line, " ");
+      std::vector<std::string> fields = strings::tokenize(line, "\t");
 
-      if (fields.size() == 2 && fields[1] == path) {
+      // There might be 2 or 3 fields, see HADOOP-6857. The 2-field
+      // version contains object size and path, the 3-field version
+      // contains object size, object storage size and path.
+      if ((fields.size() == 2 || fields.size() == 3) &&
+          fields.back() == path) {
         Result<size_t> size = numify<size_t>(fields[0]);
         if (size.isError()) {
           return Error("HDFS du returned unexpected format: " + size.error());
