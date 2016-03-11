51bf7e2f3b5ed1a95279ac19b18a7abdc51f82f8
Joris Van Remoortere <joris.van.remoortere@gmail.com>
2016-03-11 12:15:06 -0500

    Stout: Fixed style issues in `os/posix/shell.hpp`.

--- 3rdparty/libprocess/3rdparty/stout/include/stout/os/posix/shell.hpp
+++ 3rdparty/libprocess/3rdparty/stout/include/stout/os/posix/shell.hpp
@@ -31,6 +31,7 @@
 namespace os {
 
 namespace Shell {
+
 // Canonical constants used as platform-dependent args to `exec` calls.
 // name() is the command name, arg0() is the first argument received
 // by the callee, usualy the command name and arg1() is the second
@@ -39,6 +40,7 @@ namespace Shell {
   constexpr const char* name = "sh";
   constexpr const char* arg0 = "sh";
   constexpr const char* arg1 = "-c";
+
 } // namespace Shell {
 
 /**
@@ -112,6 +114,7 @@ Try<std::string> shell(const std::string& fmt, const T&... t)
   return stdout.str();
 }
 
+
 // Executes a command by calling "/bin/sh -c <command>", and returns
 // after the command has been completed. Returns 0 if succeeds, and
 // return -1 on error (e.g., fork/exec/waitpid failed). This function
@@ -140,6 +143,7 @@ inline int system(const std::string& command)
     return status;
   }
 }
+
 
 template<typename... T>
 inline int execlp(const char* file, T... t)
