c7602b2023825c153aca91e0aca846f17a8da23c
Klaus Ma <klaus1982.cn@gmail.com>
2016-03-15 17:20:26 -0700

    Fixed formating of "/maintanence/schedule" GET/POST description.
    Review: https://reviews.apache.org/r/44313/

--- src/master/http.cpp
+++ src/master/http.cpp
@@ -1985,6 +1985,7 @@ string Master::Http::MAINTENANCE_SCHEDULE_HELP()
         "Returns or updates the cluster's maintenance schedule."),
     DESCRIPTION(
         "GET: Returns the current maintenance schedule as JSON.",
+        "",
         "POST: Validates the request body as JSON",
         "  and updates the maintenance schedule."));
 }
