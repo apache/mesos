cb2298ebe7d7dd9ed3c7816011f461848a9d52fa
Gilbert Song <songzihao1990@gmail.com>
2016-03-23 15:29:26 -0700

    Fixed containerizer potential race destroy while provisioning.
    Review: https://reviews.apache.org/r/45092/

--- src/slave/containerizer/mesos/containerizer.hpp
+++ src/slave/containerizer/mesos/containerizer.hpp
@@ -285,6 +285,7 @@ private:
 
   enum State
   {
+    PROVISIONING,
     PREPARING,
     ISOLATING,
     FETCHING,
@@ -301,6 +302,11 @@ private:
     // executor because we'll only get a single notification when
     // the executor exits.
     process::Future<Option<int>> status;
+
+    // We keep track of the future that is waiting for the provisioner's
+    // `ProvisionInfo`, so that destroy will only start calling
+    // provisioner->destroy after provisioner->provision has finished.
+    std::list<process::Future<ProvisionInfo>> provisionInfos;
 
     // We keep track of the future that is waiting for all the
     // isolators' prepare futures, so that destroy will only start
