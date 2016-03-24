4e19c3e6f09eaa2793f4717e414429e0e6335e0f
Daniel Pravat <dpravat@outlook.com>
2016-03-24 09:33:05 +0100

    Windows: [2/2] Lifted socket API into Stout.
    Review: https://reviews.apache.org/r/44139/

--- 3rdparty/libprocess/include/process/network.hpp
+++ 3rdparty/libprocess/include/process/network.hpp
@@ -18,34 +18,13 @@
 #include <stout/net.hpp>
 #include <stout/try.hpp>
 
+#include <stout/os/socket.hpp>
+
+
 namespace process {
 namespace network {
 
-/**
- * Returns a socket file descriptor for the specified options.
- *
- * **NOTE:** on OS X, the returned socket will have the SO_NOSIGPIPE
- * option set.
- */
-inline Try<int> socket(int family, int type, int protocol)
-{
-  int s;
-  if ((s = ::socket(family, type, protocol)) == -1) {
-    return ErrnoError();
-  }
-
-#ifdef __APPLE__
-  // Disable SIGPIPE via setsockopt because OS X does not support
-  // the MSG_NOSIGNAL flag on send(2).
-  const int enable = 1;
-  if (setsockopt(s, SOL_SOCKET, SO_NOSIGPIPE, &enable, sizeof(int)) == -1) {
-    return ErrnoError();
-  }
-#endif // __APPLE__
-
-  return s;
-}
-
+using net::socket;
 
 // TODO(benh): Remove and defer to Socket::accept.
 inline Try<int> accept(int s)
