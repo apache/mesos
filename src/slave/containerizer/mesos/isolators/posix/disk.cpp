5914f571ea569777c8bda497a3eb1e6878ea8eb5
Joerg Schad <joerg@mesosphere.io>
2016-03-28 17:35:19 +0200

    Subprocess: [7/7] Added watchdog to 'du' disk isolator process.
    The disk isolator process should also be killed when
    the parent process dies.
    
    Review: https://reviews.apache.org/r/45245/

--- src/slave/containerizer/mesos/isolators/posix/disk.cpp
+++ src/slave/containerizer/mesos/isolators/posix/disk.cpp
@@ -487,13 +487,21 @@ private:
     // Add path on which 'du' must be run.
     command.push_back(entry->path);
 
-    // TODO(joerg84): Add correct killing behavior.
+    // NOTE: The monitor watchdog will watch the parent process and kill
+    // the 'du' process in case that the parent die.
     Try<Subprocess> s = subprocess(
         "du",
         command,
         Subprocess::PATH("/dev/null"),
         Subprocess::PIPE(),
-        Subprocess::PIPE());
+        Subprocess::PIPE(),
+        process::NO_SETSID,
+        None(),
+        None(),
+        None(),
+        Subprocess::Hook::None(),
+        None(),
+        process::MONITOR);
 
     if (s.isError()) {
       entry->promise.fail("Failed to exec 'du': " + s.error());
