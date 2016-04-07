7ca296cfc0fb88dfbed9f84494a4566c31ead90a
Anand Mazumdar <mazumdar.anand@gmail.com>
2016-04-07 13:32:37 -0700

    Move long lived executor to use the v1 API.
    Review: https://reviews.apache.org/r/45798/

--- src/examples/long_lived_executor.cpp
+++ src/examples/long_lived_executor.cpp
@@ -18,52 +18,239 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <queue>
 #include <thread>
 
-#include <mesos/executor.hpp>
+#include <mesos/v1/executor.hpp>
+#include <mesos/v1/mesos.hpp>
+
+#include <process/defer.hpp>
+#include <process/delay.hpp>
+#include <process/owned.hpp>
+#include <process/process.hpp>
 
 #include <stout/duration.hpp>
-#include <stout/lambda.hpp>
+#include <stout/linkedhashmap.hpp>
+#include <stout/option.hpp>
 #include <stout/os.hpp>
+#include <stout/uuid.hpp>
 
-using namespace mesos;
-
+using std::cerr;
 using std::cout;
 using std::endl;
+using std::queue;
 using std::string;
 
+using mesos::v1::ExecutorID;
+using mesos::v1::FrameworkID;
+using mesos::v1::TaskID;
+using mesos::v1::TaskInfo;
+using mesos::v1::TaskState;
+using mesos::v1::TaskStatus;
 
-class LongLivedExecutor : public Executor
+using mesos::v1::executor::Call;
+using mesos::v1::executor::Event;
+using mesos::v1::executor::Mesos;
+
+
+class LongLivedExecutor : public process::Process<LongLivedExecutor>
 {
 public:
-  virtual void launch(ExecutorDriver* driver, const TaskInfo& task)
+  LongLivedExecutor(
+      const FrameworkID& _frameworkId,
+      const ExecutorID& _executorId)
+    : frameworkId(_frameworkId),
+      executorId(_executorId),
+      state(DISCONNECTED) {}
+
+  virtual ~LongLivedExecutor() = default;
+
+protected:
+  virtual void initialize()
+  {
+    // We initialize the library here to ensure that callbacks are only invoked
+    // after the process has spawned.
+    mesos.reset(new Mesos(
+        mesos::ContentType::PROTOBUF,
+        process::defer(self(), &Self::connected),
+        process::defer(self(), &Self::disconnected),
+        process::defer(self(), &Self::received, lambda::_1)));
+  }
+
+  void connected()
+  {
+    state = CONNECTED;
+
+    doReliableRegistration();
+  }
+
+  void disconnected()
+  {
+    state = DISCONNECTED;
+  }
+
+  void received(queue<Event> events)
+  {
+    while (!events.empty()) {
+      Event event = events.front();
+      events.pop();
+
+      cout << "Received " << event.type() << " event" << endl;
+
+      switch (event.type()) {
+        case Event::SUBSCRIBED: {
+          cout << "Subscribed executor on "
+               << event.subscribed().agent_info().hostname() << endl;
+
+          state = SUBSCRIBED;
+          break;
+        }
+
+        case Event::LAUNCH: {
+          launch(event.launch().task());
+          break;
+        }
+
+        case Event::ACKNOWLEDGED: {
+          // Remove the corresponding update.
+          updates.erase(UUID::fromBytes(event.acknowledged().uuid()));
+
+          // Remove the corresponding task.
+          tasks.erase(event.acknowledged().task_id());
+          break;
+        }
+
+        case Event::ERROR: {
+          cerr << "Error: " << event.error().message() << endl;
+          break;
+        }
+
+        case Event::KILL:
+        case Event::MESSAGE:
+        case Event::SHUTDOWN: {
+          break;
+        }
+
+        default: {
+          UNREACHABLE();
+        }
+      }
+    }
+  }
+
+  void doReliableRegistration()
+  {
+    if (state == SUBSCRIBED || state == DISCONNECTED) {
+      return;
+    }
+
+    Call call;
+    call.mutable_framework_id()->CopyFrom(frameworkId);
+    call.mutable_executor_id()->CopyFrom(executorId);
+
+    call.set_type(Call::SUBSCRIBE);
+
+    Call::Subscribe* subscribe = call.mutable_subscribe();
+
+    // Send all unacknowledged updates.
+    foreach (const Call::Update& update, updates.values()) {
+      subscribe->add_unacknowledged_updates()->MergeFrom(update);
+    }
+
+    // Send all unacknowledged tasks.
+    foreach (const TaskInfo& task, tasks.values()) {
+      subscribe->add_unacknowledged_tasks()->MergeFrom(task);
+    }
+
+    mesos->send(call);
+
+    process::delay(Seconds(1), self(), &Self::doReliableRegistration);
+  }
+
+  void update(const TaskInfo& task, const TaskState& state)
+  {
+    UUID uuid = UUID::random();
+
+    TaskStatus status;
+    status.mutable_task_id()->CopyFrom(task.task_id());
+    status.mutable_executor_id()->CopyFrom(executorId);
+    status.set_state(state);
+    status.set_source(TaskStatus::SOURCE_EXECUTOR);
+    status.set_uuid(uuid.toBytes());
+
+    Call call;
+    call.mutable_framework_id()->CopyFrom(frameworkId);
+    call.mutable_executor_id()->CopyFrom(executorId);
+
+    call.set_type(Call::UPDATE);
+
+    call.mutable_update()->mutable_status()->CopyFrom(status);
+
+    // Capture the status update.
+    updates[uuid] = call.update();
+
+    mesos->send(call);
+  }
+
+  void launch(const TaskInfo& task)
   {
     cout << "Starting task " << task.task_id().value() << endl;
+
+    tasks[task.task_id()] = task;
 
     std::thread thread([=]() {
       os::sleep(Seconds(random() % 10));
 
-      TaskStatus status;
-      status.mutable_task_id()->MergeFrom(task.task_id());
-      status.set_state(TASK_FINISHED);
-
-      driver->sendStatusUpdate(status);
+      update(task, TaskState::TASK_FINISHED);
     });
 
     thread.detach();
 
-    TaskStatus status;
-    status.mutable_task_id()->MergeFrom(task.task_id());
-    status.set_state(TASK_RUNNING);
-
-    driver->sendStatusUpdate(status);
+    update(task, TaskState::TASK_RUNNING);
   }
+
+private:
+  const FrameworkID frameworkId;
+  const ExecutorID executorId;
+  process::Owned<Mesos> mesos;
+  enum State
+  {
+    CONNECTED,
+    DISCONNECTED,
+    SUBSCRIBED
+  } state;
+
+  LinkedHashMap<UUID, Call::Update> updates; // Unacknowledged updates.
+  LinkedHashMap<TaskID, TaskInfo> tasks; // Unacknowledged tasks.
 };
 
 
 int main(int argc, char** argv)
 {
-  LongLivedExecutor executor;
-  MesosExecutorDriver driver(&executor);
-  return driver.run() == DRIVER_STOPPED ? 0 : 1;
+  FrameworkID frameworkId;
+  ExecutorID executorId;
+
+  Option<string> value;
+
+  value = os::getenv("MESOS_FRAMEWORK_ID");
+  if (value.isNone()) {
+    EXIT(EXIT_FAILURE)
+      << "Expecting 'MESOS_FRAMEWORK_ID' to be set in the environment";
+  }
+  frameworkId.set_value(value.get());
+
+  value = os::getenv("MESOS_EXECUTOR_ID");
+  if (value.isNone()) {
+    EXIT(EXIT_FAILURE)
+      << "Expecting 'MESOS_EXECUTOR_ID' to be set in the environment";
+  }
+  executorId.set_value(value.get());
+
+  process::Owned<LongLivedExecutor> executor(
+      new LongLivedExecutor(frameworkId, executorId));
+
+  process::spawn(executor.get());
+  process::wait(executor.get());
+
+  return EXIT_SUCCESS;
 }
