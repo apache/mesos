fc3c67b2f326fe7798b416daaf3606377bbc0983
Guangya Liu <gyliu513@gmail.com>
2016-04-19 10:35:19 -0700

    Ignored volumes with source in linux filesystem isolator.
    Review: https://reviews.apache.org/r/45373/

--- src/slave/containerizer/mesos/isolators/filesystem/linux.cpp
+++ src/slave/containerizer/mesos/isolators/filesystem/linux.cpp
@@ -466,6 +466,14 @@ Try<string> LinuxFilesystemIsolatorProcess::script(
   }
 
   foreach (const Volume& volume, executorInfo.container().volumes()) {
+    // NOTE: Volumes with source will be handled by the corresponding
+    // isolators (e.g., docker/volume).
+    if (volume.has_source()) {
+      VLOG(1) << "Ignored a volume with source for container '"
+              << containerId << "'";
+      continue;
+    }
+
     if (!volume.has_host_path()) {
       return Error("A volume misses 'host_path'");
     }
