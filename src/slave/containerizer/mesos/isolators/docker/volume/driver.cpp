d2d44d5e86cb2b76fcb2025b87031bc47456d035
Guangya Liu <gyliu513@gmail.com>
2016-04-20 12:04:25 -0700

    Added docker volume driver client for mount and unmount.
    Review: https://reviews.apache.org/r/45360/

---
+++ src/slave/containerizer/mesos/isolators/docker/volume/driver.cpp
@@ -0,0 +1,201 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <glog/logging.h>
+
+#include <process/collect.hpp>
+#include <process/io.hpp>
+#include <process/subprocess.hpp>
+
+#include "common/status_utils.hpp"
+
+#include "slave/containerizer/mesos/isolators/docker/volume/driver.hpp"
+
+namespace io = process::io;
+
+using std::string;
+using std::tuple;
+using std::vector;
+
+using process::Failure;
+using process::Future;
+using process::Owned;
+using process::Subprocess;
+
+namespace mesos {
+namespace internal {
+namespace slave {
+namespace docker {
+namespace volume {
+
+Try<Owned<DriverClient>> DriverClient::create(
+    const std::string& dvdcli)
+{
+  return Owned<DriverClient>(new DriverClient(dvdcli));
+}
+
+
+Future<string> DriverClient::mount(
+    const string& driver,
+    const string& name,
+    const Option<hashmap<string, string>>& options)
+{
+  // Refer to https://github.com/emccode/dvdcli for how dvdcli works.
+  // TODO(gyliu513): Add `explicitcreate` if 'options' is None. Refer
+  // to https://github.com/emccode/dvdcli/pull/20 for detail.
+  vector<string> argv = {
+    dvdcli,
+    "mount",
+    "--volumedriver=" + driver,
+    "--volumename=" + name,
+  };
+
+  if (options.isSome()) {
+    foreachpair (const string& key, const string& value, options.get()) {
+      argv.push_back("--volumeopts=" + key + "=" + value);
+    }
+  }
+
+  string command = strings::join(
+      ", ",
+      dvdcli,
+      strings::join(", ", argv));
+
+  VLOG(1) << "Invoking Docker Volume Driver 'mount' "
+          << "command '" << command << "'";
+
+  Try<Subprocess> s = subprocess(
+      dvdcli,
+      argv,
+      Subprocess::PATH("/dev/null"),
+      Subprocess::PIPE(),
+      Subprocess::PIPE());
+
+  if (s.isError()) {
+    return Failure("Failed to execute '" + command + "': " + s.error());
+  }
+
+  return await(
+      s->status(),
+      io::read(s->out().get()),
+      io::read(s->err().get()))
+    .then([](const tuple<
+        Future<Option<int>>,
+        Future<string>,
+        Future<string>>& t) -> Future<string> {
+      Future<Option<int>> status = std::get<0>(t);
+      if (!status.isReady()) {
+        return Failure(
+            "Failed to get the exit status of the subprocess: " +
+            (status.isFailed() ? status.failure() : "discarded"));
+      }
+
+      if (status->isNone()) {
+        return Failure("Failed to reap the subprocess");
+      }
+
+      if (status->get() != 0) {
+        Future<string> error = std::get<2>(t);
+        if (!error.isReady()) {
+          return Failure(
+              "Unexpected termination of the subprocess: " +
+              WSTRINGIFY(status->get()));
+        }
+
+        return Failure(
+            "Unexpected termination of the subprocess: " + error.get());
+      }
+
+      Future<string> output = std::get<1>(t);
+      if (!output.isReady()) {
+         return Failure(
+            "Failed to read stdout from the subprocess: " +
+            (output.isFailed() ? output.failure() : "discarded"));
+      }
+
+      return output;
+    });
+}
+
+
+Future<Nothing> DriverClient::unmount(
+    const string& driver,
+    const string& name)
+{
+  vector<string> argv = {
+    dvdcli,
+    "unmount",
+    "--volumedriver=" + driver,
+    "--volumename=" + name,
+  };
+
+  string command = strings::join(
+      ", ",
+      dvdcli,
+      strings::join(", ", argv));
+
+  VLOG(1) << "Invoking Docker Volume Driver 'unmount' "
+          << "command '" << command << "'";
+
+  Try<Subprocess> s = subprocess(
+      dvdcli,
+      argv,
+      Subprocess::PATH("/dev/null"),
+      Subprocess::PIPE(),
+      Subprocess::PIPE());
+
+  if (s.isError()) {
+    return Failure("Failed to execute '" + command + "': " + s.error());
+  }
+
+  return await(
+      s->status(),
+      io::read(s->err().get()))
+    .then([](const tuple<
+        Future<Option<int>>,
+        Future<string>>& t) -> Future<Nothing> {
+      Future<Option<int>> status = std::get<0>(t);
+      if (!status.isReady()) {
+        return Failure(
+            "Failed to get the exit status of the subprocess: " +
+            (status.isFailed() ? status.failure() : "discarded"));
+      }
+
+      if (status->isNone()) {
+        return Failure("Failed to reap the subprocess");
+      }
+
+      if (status->get() != 0) {
+        Future<string> error = std::get<1>(t);
+        if (!error.isReady()) {
+          return Failure(
+            "Unexpected termination of the subprocess: " +
+            WSTRINGIFY(status->get()));
+        }
+
+        return Failure(
+            "Unexpected termination of the subprocess: " + error.get());
+      }
+
+      return Nothing();
+    });
+}
+
+} // namespace volume {
+} // namespace docker {
+} // namespace slave {
+} // namespace internal {
+} // namespace mesos {
---
+++ src/slave/containerizer/mesos/isolators/docker/volume/driver.hpp
@@ -0,0 +1,87 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __ISOLATOR_DOCKER_VOLUME_DRIVER_HPP__
+#define __ISOLATOR_DOCKER_VOLUME_DRIVER_HPP__
+
+#include <string>
+
+#include <process/future.hpp>
+#include <process/owned.hpp>
+
+#include <stout/hashmap.hpp>
+#include <stout/none.hpp>
+#include <stout/nothing.hpp>
+#include <stout/option.hpp>
+#include <stout/try.hpp>
+
+namespace mesos {
+namespace internal {
+namespace slave {
+namespace docker {
+namespace volume {
+
+/**
+ * Abstraction for Docker Volume Driver Client.
+ *
+ * TODO(gyliu513): Update the docker volume driver client use docker
+ * volume driver API directly via curl.
+ *
+ * Refer to https://github.com/yp-engineering/rbd-docker-plugin for
+ * how `curl` works with docker volume driver API.
+ */
+class DriverClient
+{
+public:
+  /**
+   * Create a Docker Volume Driver Client.
+   */
+  static Try<process::Owned<DriverClient>> create(
+      const std::string& dvdcli);
+
+  virtual ~DriverClient() {}
+
+  /**
+   * Performs a 'mount' and returns the path of the mount point. If
+   * the 'options' field is specified, the 'mount' will do an implicit
+   * creation of the volume if it does not exist.
+   */
+  virtual process::Future<std::string> mount(
+      const std::string& driver,
+      const std::string& name,
+      const Option<hashmap<std::string, std::string>>& options = None());
+
+  /**
+   * Performs an 'unmount'.
+   */
+  virtual process::Future<Nothing> unmount(
+      const std::string& driver,
+      const std::string& name);
+
+private:
+  DriverClient(const std::string& _dvdcli)
+    : dvdcli(_dvdcli) {}
+
+  const std::string dvdcli;
+};
+
+} // namespace volume {
+} // namespace docker {
+} // namespace slave {
+} // namespace internal {
+} // namespace mesos {
+
+#endif // __ISOLATOR_DOCKER_VOLUME_DRIVER_HPP__
