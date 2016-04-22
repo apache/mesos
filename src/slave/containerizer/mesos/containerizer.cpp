f094cb6fe6aa063d6c4a6423176622f3661fa36f
Gilbert Song <songzihao1990@gmail.com>
2016-04-22 15:26:04 -0700

    Fixed isolator cleaup issue when destroying a provisioning container.
    Review: https://reviews.apache.org/r/46577/

--- src/slave/containerizer/mesos/containerizer.cpp
+++ src/slave/containerizer/mesos/containerizer.cpp
@@ -1434,9 +1434,10 @@ void MesosContainerizerProcess::destroy(
     await(container->provisionInfos)
       .onAny(defer(
           self(),
-          &Self::___destroy,
+          &Self::____destroy,
           containerId,
           None(),
+          list<Future<Nothing>>(),
           "Container destroyed while provisioning images"));
 
     return;
