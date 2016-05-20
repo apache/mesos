d78f2721590c49ec65d00cc506ca551235741429
Alex Clemmer <clemmer.alexander@gmail.com>
2016-05-20 13:58:43 -0700

    Stout: Removed implicit conversions in `permissions.hpp` on Windows.
    Review: https://reviews.apache.org/r/47390/

--- 3rdparty/stout/include/stout/os/permissions.hpp
+++ 3rdparty/stout/include/stout/os/permissions.hpp
@@ -24,21 +24,21 @@ struct Permissions
 {
   explicit Permissions(mode_t mode)
   {
-    owner.r = mode & S_IRUSR;
-    owner.w = mode & S_IWUSR;
-    owner.x = mode & S_IXUSR;
-    owner.rwx = mode & S_IRWXU;
-    group.r = mode & S_IRGRP;
-    group.w = mode & S_IWGRP;
-    group.x = mode & S_IXGRP;
-    group.rwx = mode & S_IRWXG;
-    others.r = mode & S_IROTH;
-    others.w = mode & S_IWOTH;
-    others.x = mode & S_IXOTH;
-    others.rwx = mode & S_IRWXO;
-    setuid = mode & S_ISUID;
-    setgid = mode & S_ISGID;
-    sticky = mode & S_ISVTX;
+    owner.r = (mode & S_IRUSR) != 0;
+    owner.w = (mode & S_IWUSR) != 0;
+    owner.x = (mode & S_IXUSR) != 0;
+    owner.rwx = (mode & S_IRWXU) != 0;
+    group.r = (mode & S_IRGRP) != 0;
+    group.w = (mode & S_IWGRP) != 0;
+    group.x = (mode & S_IXGRP) != 0;
+    group.rwx = (mode & S_IRWXG) != 0;
+    others.r = (mode & S_IROTH) != 0;
+    others.w = (mode & S_IWOTH) != 0;
+    others.x = (mode & S_IXOTH) != 0;
+    others.rwx = (mode & S_IRWXO) != 0;
+    setuid = (mode & S_ISUID) != 0;
+    setgid = (mode & S_ISGID) != 0;
+    sticky = (mode & S_ISVTX) != 0;
   }
 
   struct
