a8829e1ad07ba81099d9b9fabe1e93e2b36036e5
Alex Clemmer <clemmer.alexander@gmail.com>
2016-05-23 15:23:02 -0700

    Stout: Implemented `os::fsync`.
    Review: https://reviews.apache.org/r/47387/

---
+++ 3rdparty/stout/include/stout/os/windows/fsync.hpp
@@ -0,0 +1,44 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __STOUT_OS_WINDOWS_FSYNC_HPP__
+#define __STOUT_OS_WINDOWS_FSYNC_HPP__
+
+#include <io.h>
+
+#include <stout/error.hpp>
+#include <stout/nothing.hpp>
+#include <stout/try.hpp>
+#include <stout/windows.hpp>
+
+
+namespace os {
+
+inline Try<Nothing> fsync(int fd)
+{
+  const HANDLE handle = reinterpret_cast<HANDLE>(_get_osfhandle(fd));
+  if (handle == INVALID_HANDLE_VALUE) {
+    return WindowsError(
+        "os::fsync: Could not get handle for given file descriptor");
+  }
+
+  if (!FlushFileBuffers(handle)) {
+    return WindowsError(
+        "os::fsync: Could not flush file buffers for given file descriptor");
+  }
+
+  return Nothing();
+}
+
+} // namespace os {
+
+#endif // __STOUT_OS_WINDOWS_FSYNC_HPP__
