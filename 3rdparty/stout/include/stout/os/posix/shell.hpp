0c73ac2070340b667dc97767f71d39fbe15f2fcb
Jie Yu <yujie.jay@gmail.com>
2016-06-28 12:14:16 -0700

    Fixed an indentation issue in shell.hpp.
    Review: https://reviews.apache.org/r/49343

--- 3rdparty/stout/include/stout/os/posix/shell.hpp
+++ 3rdparty/stout/include/stout/os/posix/shell.hpp
@@ -32,14 +32,14 @@ namespace os {
 
 namespace Shell {
 
-  // Canonical constants used as platform-dependent args to `exec` calls.
-  // `name` is the command name, `arg0` is the first argument received
-  // by the callee, usually the command name and `arg1` is the second
-  // command argument received by the callee.
+// Canonical constants used as platform-dependent args to `exec`
+// calls. `name` is the command name, `arg0` is the first argument
+// received by the callee, usually the command name and `arg1` is the
+// second command argument received by the callee.
 
-  constexpr const char* name = "sh";
-  constexpr const char* arg0 = "sh";
-  constexpr const char* arg1 = "-c";
+constexpr const char* name = "sh";
+constexpr const char* arg0 = "sh";
+constexpr const char* arg1 = "-c";
 
 } // namespace Shell {
 
