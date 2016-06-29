3405d3b30ffeee64acc98b34aad030ef84f184d3
haosdent huang <haosdent@gmail.com>
2016-06-29 22:51:57 +0200

    Updated `dispatch` calls to use lambda in `http_tests.cpp`.
    Review: https://reviews.apache.org/r/46687/

--- 3rdparty/libprocess/src/tests/http_tests.cpp
+++ 3rdparty/libprocess/src/tests/http_tests.cpp
@@ -189,9 +189,8 @@ TEST(HTTPTest, EndpointsHelp)
 
   // Wait until the HttpProcess initialization has run so
   // that the route calls have completed.
-  std::function<Nothing()> f = []() { return Nothing(); };
+  Future<Nothing> initialized = dispatch(pid, []() { return Nothing(); });
 
-  Future<Nothing> initialized = dispatch(pid, f);
   AWAIT_READY(initialized);
 
   // Hit '/help' and wait for a 200 OK response.
@@ -260,9 +259,8 @@ TEST(HTTPTest, EndpointsHelpRemoval)
 
   // Wait until the HttpProcess initialization has run so
   // that the route calls have completed.
-  std::function<Nothing()> f = []() { return Nothing(); };
+  Future<Nothing> initialized = dispatch(pid, []() { return Nothing(); });
 
-  Future<Nothing> initialized = dispatch(pid, f);
   AWAIT_READY(initialized);
 
   // Hit '/help/<id>/body' and wait for a 200 OK response.
