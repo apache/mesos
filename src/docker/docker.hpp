ac1b43b13c1c22c9d5d7040466735efa31673db8
Benjamin Mahler <bmahler@apache.org>
2016-07-01 17:11:06 -0700

    Fixed dangerous use of Subprocess::PIPE as default Docker::run IO.
    Docker::run used Subprocess::PIPE as defaults for stdout and stderr,
    however the caller cannot obtain the read end of the pipe in order
    to read and avoid blocking the child. Also, internally these pipes
    are not read. This means the child process can block!
    
    For now, this changes the default to match stdout and stderr from
    the parent process. Ideally, we also prevent the caller from using
    Subprocess::PIPE since they cannot obtain the read end.
    
    Review: https://reviews.apache.org/r/49547

--- src/docker/docker.hpp
+++ src/docker/docker.hpp
@@ -130,8 +130,10 @@ public:
       const std::string& mappedDirectory,
       const Option<mesos::Resources>& resources = None(),
       const Option<std::map<std::string, std::string>>& env = None(),
-      const process::Subprocess::IO& _stdout = process::Subprocess::PIPE(),
-      const process::Subprocess::IO& _stderr = process::Subprocess::PIPE())
+      const process::Subprocess::IO& _stdout =
+        process::Subprocess::FD(STDOUT_FILENO),
+      const process::Subprocess::IO& _stderr =
+        process::Subprocess::FD(STDERR_FILENO))
     const;
 
   // Returns the current docker version.
