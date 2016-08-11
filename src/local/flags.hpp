1f988a35b513b6c21484b34471a5171adf63ddd3
Ammar Askar <ammar@ammaraskar.com>
2016-08-10 17:57:09 -0700

    Propagated work_dir flag from local runs to agents/masters.
    Review: https://reviews.apache.org/r/50003/

--- src/local/flags.hpp
+++ src/local/flags.hpp
@@ -18,6 +18,8 @@
 #define __LOCAL_FLAGS_HPP__
 
 #include <stout/flags.hpp>
+#include <stout/os.hpp>
+#include <stout/path.hpp>
 
 #include "logging/flags.hpp"
 
@@ -30,12 +32,32 @@ class Flags : public logging::Flags
 public:
   Flags()
   {
+    // `work_dir` is passed from here to the agents/master.
+    // This is necessary because `work_dir` is a required flag
+    // in agents/master and without this, the load call for their
+    // flags will spit out an error unless they have an env
+    // variable for the `work_dir` explicitly set.
+    // Since local mode is used strictly for non-production
+    // purposes, it is the one case where we deem it acceptable
+    // to set a default value for `work_dir`.
+    add(&Flags::work_dir,
+        "work_dir",
+        "Path of the master/agent work directory. This is where the\n"
+        "persistent information of the cluster will be stored.\n"
+        "Note that locations like `/tmp` which are cleaned\n"
+        "automatically are not suitable for the work directory\n"
+        "when running in production, since long-running masters\n"
+        "and agents could lose data when cleanup occurs.\n"
+        "(Example: `/var/lib/mesos`)",
+        path::join(os::temp(), "mesos", "local"));
+
     add(&Flags::num_slaves,
         "num_slaves",
         "Number of agents to launch for local cluster",
         1);
   }
 
+  std::string work_dir;
   int num_slaves;
 };
 
