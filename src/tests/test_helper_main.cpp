7a1bdf30ea9f16273e804f35f895b0d33297f03c
Benjamin Bannier <benjamin.bannier@mesosphere.io>
2016-08-25 16:32:08 +0200

    Added a missing stout header.

--- src/tests/test_helper_main.cpp
+++ src/tests/test_helper_main.cpp
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <stout/none.hpp>
 #include <stout/subcommand.hpp>
 
 #include "tests/active_user_test_helper.hpp"
