fa56d0458860b2b7f035c793a192d7b55180a125
Kevin Klues <klueska@gmail.com>
2016-10-12 22:34:21 -0700

    Added 'stout/wait.hpp' header.
    This was motivated by the need for a default definition of
    'W_EXITCODE' (since it is not technically POSIX compliant).
    
    Review: https://reviews.apache.org/r/52705/

---
+++ 3rdparty/stout/include/stout/os/wait.hpp
@@ -0,0 +1,37 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __STOUT_OS_WAIT_HPP__
+#define __STOUT_OS_WAIT_HPP__
+
+#ifdef __WINDOWS__
+// TODO(klueska): Move all `WAIT` related functions out of
+// `windows.hpp` and into this file.
+#include <windows.hpp>
+#else
+#include <sys/wait.h>
+#endif // __WINDOWS__
+
+
+#ifndef W_EXITCODE
+#define W_EXITCODE(ret, sig)  ((ret) << 8 | (sig))
+#endif
+
+
+namespace os {
+
+// TODO(klueska): Add helper functions for common wait related
+// operations in this header file.
+
+} // namespace os {
+
+#endif // __STOUT_OS_WAIT_HPP__
