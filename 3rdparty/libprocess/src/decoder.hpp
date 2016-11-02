315b9cdad33ee3fe653df1698d4f21711b10c0e9
Anand Mazumdar <anand@apache.org>
2016-11-01 21:32:55 -0700

    Fixed memory leak in request/response decoders.
    The leak can happen in cases where a client disconnects while the
    request/response is in progress.
    
    Review: https://reviews.apache.org/r/53361/

--- 3rdparty/libprocess/src/decoder.hpp
+++ 3rdparty/libprocess/src/decoder.hpp
@@ -60,6 +60,15 @@ public:
     parser.data = this;
   }
 
+  ~DataDecoder()
+  {
+    delete request;
+
+    foreach (http::Request* request, requests) {
+      delete request;
+    }
+  }
+
   std::deque<http::Request*> decode(const char* data, size_t length)
   {
     size_t parsed = http_parser_execute(&parser, &settings, data, length);
@@ -296,6 +305,15 @@ public:
     parser.data = this;
   }
 
+  ~ResponseDecoder()
+  {
+    delete response;
+
+    foreach (http::Response* response, responses) {
+      delete response;
+    }
+  }
+
   std::deque<http::Response*> decode(const char* data, size_t length)
   {
     size_t parsed = http_parser_execute(&parser, &settings, data, length);
@@ -505,6 +523,19 @@ public:
     http_parser_init(&parser, HTTP_RESPONSE);
 
     parser.data = this;
+  }
+
+  ~StreamingResponseDecoder()
+  {
+    delete response;
+
+    if (writer.isSome()) {
+      writer->fail("Decoder is being deleted");
+    }
+
+    foreach (http::Response* response, responses) {
+      delete response;
+    }
   }
 
   std::deque<http::Response*> decode(const char* data, size_t length)
