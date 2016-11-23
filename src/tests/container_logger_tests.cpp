1f93bdd9240d9d23456e4a5da1299dc4135512e7
Sivaram Kannan <sivaramsk@gmail.com>
2016-11-23 11:14:41 -0800

    Added test cases for the logrotate module with --switch_user.
    This adds two regression tests for running a task under a different
    user than the agent.  In both cases, the logrotate module should
    still rotate logs, but the user that owns the log files will differ.
    
    Review: https://reviews.apache.org/r/53699/

--- src/tests/container_logger_tests.cpp
+++ src/tests/container_logger_tests.cpp
@@ -40,6 +40,7 @@
 #include <stout/os/pstree.hpp>
 #include <stout/os/read.hpp>
 #include <stout/os/stat.hpp>
+#include <stout/os/su.hpp>
 
 #include "master/master.hpp"
 
@@ -89,6 +90,7 @@ using std::vector;
 using testing::_;
 using testing::AtMost;
 using testing::Return;
+using testing::WithParamInterface;
 
 namespace mesos {
 namespace internal {
@@ -577,6 +579,196 @@ TEST_F(ContainerLoggerTest, LOGROTATE_ModuleFDOwnership)
 
   driver.stop();
   driver.join();
+}
+
+
+// These tests are parameterized by the boolean `--switch-user` agent flag.
+class UserContainerLoggerTest
+  : public ContainerLoggerTest, public WithParamInterface<bool> {};
+
+INSTANTIATE_TEST_CASE_P(
+    bool,
+    UserContainerLoggerTest,
+    ::testing::Values(true, false));
+
+
+// Tests that the packaged logrotate container logger will rotate files when
+// the agent is root, but the executor is launched as a non-root user.
+//
+// 1. When `--switch_user` is true on the agent, the logger module should
+//    launch subprocesses with the same user as the executor.
+// 2. When `--switch_user` is false on the agent, the logger module should
+//    inherit the user of the agent.
+TEST_P(UserContainerLoggerTest, ROOT_LOGROTATE_RotateWithSwitchUserTrueOrFalse)
+{
+  // Create a master, agent, and framework.
+  Try<Owned<cluster::Master>> master = StartMaster();
+  ASSERT_SOME(master);
+
+  Future<SlaveRegisteredMessage> slaveRegisteredMessage =
+    FUTURE_PROTOBUF(SlaveRegisteredMessage(), _, _);
+
+  // We'll need access to these flags later.
+  slave::Flags flags = CreateSlaveFlags();
+
+  // Use the non-default container logger that rotates logs.
+  flags.container_logger = LOGROTATE_CONTAINER_LOGGER_NAME;
+
+  // Parameterize the `--switch_user` flag to test both options.
+  flags.switch_user = GetParam();
+
+  // In order for the unprivileged user to successfully chdir, the
+  // agent's work directory needs to have execute permissions.
+  Try<Nothing> chmod = os::chmod(
+      flags.work_dir, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+  ASSERT_SOME(chmod);
+
+  Fetcher fetcher;
+
+  // We use an actual containerizer + executor since we want something to run.
+  Try<MesosContainerizer*> _containerizer =
+    MesosContainerizer::create(flags, false, &fetcher);
+
+  CHECK_SOME(_containerizer);
+  Owned<MesosContainerizer> containerizer(_containerizer.get());
+
+  Owned<MasterDetector> detector = master.get()->createDetector();
+
+  Try<Owned<cluster::Slave>> slave =
+    StartSlave(detector.get(), containerizer.get(), flags);
+  ASSERT_SOME(slave);
+
+  AWAIT_READY(slaveRegisteredMessage);
+  SlaveID slaveId = slaveRegisteredMessage.get().slave_id();
+
+  MockScheduler sched;
+  MesosSchedulerDriver driver(
+      &sched, DEFAULT_FRAMEWORK_INFO, master.get()->pid, DEFAULT_CREDENTIAL);
+
+  Future<FrameworkID> frameworkId;
+  EXPECT_CALL(sched, registered(&driver, _, _))
+    .WillOnce(FutureArg<1>(&frameworkId));
+
+  // Wait for an offer, and start a task.
+  Future<vector<Offer>> offers;
+  EXPECT_CALL(sched, resourceOffers(&driver, _))
+    .WillOnce(FutureArg<1>(&offers))
+    .WillRepeatedly(Return()); // Ignore subsequent offers.
+
+  driver.start();
+  AWAIT_READY(frameworkId);
+
+  AWAIT_READY(offers);
+  EXPECT_NE(0u, offers.get().size());
+
+  // Start a task that spams stdout with 3 MB of (mostly blank) output.
+  // The logrotate container logger module is loaded with parameters that limit
+  // the log size to five files of 2 MB each.  After the task completes, there
+  // should be two files with a total size of 3 MB.  The "stdout" file should
+  // be 1 MB large.
+  TaskInfo task = createTask(
+      offers.get()[0],
+      "i=0; while [ $i -lt 3072 ]; "
+      "do printf '%-1024d\\n' $i; i=$((i+1)); done");
+
+  // Start the task as a non-root user.
+  task.mutable_command()->set_user("nobody");
+
+  Future<TaskStatus> statusRunning;
+  Future<TaskStatus> statusFinished;
+  EXPECT_CALL(sched, statusUpdate(&driver, _))
+    .WillOnce(FutureArg<1>(&statusRunning))
+    .WillOnce(FutureArg<1>(&statusFinished))
+    .WillRepeatedly(Return());       // Ignore subsequent updates.
+
+  driver.launchTasks(offers.get()[0].id(), {task});
+
+  AWAIT_READY(statusRunning);
+  EXPECT_EQ(TASK_RUNNING, statusRunning.get().state());
+
+  AWAIT_READY(statusFinished);
+  EXPECT_EQ(TASK_FINISHED, statusFinished.get().state());
+
+  driver.stop();
+  driver.join();
+
+  // The `LogrotateContainerLogger` spawns some `mesos-logrotate-logger`
+  // processes above, which continue running briefly after the container exits.
+  // Once they finish reading the container's pipe, they should exit.
+  Try<os::ProcessTree> pstrees = os::pstree(0);
+  ASSERT_SOME(pstrees);
+  foreach (const os::ProcessTree& pstree, pstrees.get().children) {
+    // Wait for the logger subprocesses to exit, for up to 5 seconds each.
+    Duration waited = Duration::zero();
+    do {
+      if (!os::exists(pstree.process.pid)) {
+        break;
+      }
+
+      // Push the clock ahead to speed up the reaping of subprocesses.
+      Clock::pause();
+      Clock::settle();
+      Clock::advance(Seconds(1));
+      Clock::resume();
+
+      os::sleep(Milliseconds(100));
+      waited += Milliseconds(100);
+    } while (waited < Seconds(5));
+
+    EXPECT_LE(waited, Seconds(5));
+  }
+
+  // Check for the expected log rotation.
+  string sandboxDirectory = path::join(
+      slave::paths::getExecutorPath(
+          flags.work_dir,
+          slaveId,
+          frameworkId.get(),
+          statusRunning->executor_id()),
+      "runs",
+      "latest");
+
+  ASSERT_TRUE(os::exists(sandboxDirectory));
+
+  // The leading log file should be owned by the logrotate module's
+  // companion binary's user.
+  string stdoutPath = path::join(sandboxDirectory, "stdout");
+  ASSERT_TRUE(os::exists(stdoutPath));
+
+  struct stat stdoutStat;
+  ASSERT_GE(::stat(stdoutPath.c_str(), &stdoutStat), 0);
+
+  // Depending on the `--switch_user`, the expected user is either
+  // "nobody" or "root".
+  Result<string> stdoutUser = os::user(stdoutStat.st_uid);
+  if (GetParam()) {
+    ASSERT_SOME_EQ("nobody", stdoutUser);
+  } else {
+    ASSERT_SOME_EQ("root", stdoutUser);
+  }
+
+  // The leading log file should be about half full (1 MB).
+  // NOTE: We don't expect the size of the leading log file to be precisely
+  // one MB since there is also the executor's output besides the task's stdout.
+  Try<Bytes> stdoutSize = os::stat::size(stdoutPath);
+  ASSERT_SOME(stdoutSize);
+  EXPECT_LE(1024u, stdoutSize->kilobytes());
+  EXPECT_GE(1050u, stdoutSize->kilobytes());
+
+  // We should only have files up to "stdout.1".
+  stdoutPath = path::join(sandboxDirectory, "stdout.2");
+  EXPECT_FALSE(os::exists(stdoutPath));
+
+  // The only rotated log file (2 MB each) should be present.
+  stdoutPath = path::join(sandboxDirectory, "stdout.1");
+  ASSERT_TRUE(os::exists(stdoutPath));
+
+  // NOTE: The rotated files are written in contiguous blocks, meaning that
+  // each file may be less than the maximum allowed size.
+  stdoutSize = os::stat::size(stdoutPath);
+  ASSERT_SOME(stdoutSize);
+  EXPECT_LE(2040u, stdoutSize->kilobytes());
+  EXPECT_GE(2048u, stdoutSize->kilobytes());
 }
 
 } // namespace tests {
