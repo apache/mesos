29c3f34a5af88ad6afbbc13dc7684ed94cad6a69
Srinivas Brahmaroutu <srbrahma@us.ibm.com>
2016-11-28 15:32:34 -0800

    CMake: Added example frameworks and executors.
    This builds some binaries that are mainly used in the example
    framework tests.
    
    This review replaces: https://reviews.apache.org/r/49870/
    and https://reviews.apache.org/r/50327/
    
    Review: https://reviews.apache.org/r/53755/

--- src/examples/cmake/ExamplesConfigure.cmake
+++ src/examples/cmake/ExamplesConfigure.cmake
@@ -31,3 +31,21 @@ set(TEST_MASTER_CONTENDER   testmastercontender)
 set(TEST_MASTER_DETECTOR    testmasterdetector)
 set(TEST_QOS_CONTROLLER     testqos_controller)
 set(TEST_RESOURCE_ESTIMATOR testresource_estimator)
+
+
+# Define example framework and executor targets.
+################################################
+set(BALLOON_EXECUTOR              balloon-executor)
+set(BALLOON_FRAMEWORK             balloon-framework)
+set(DISK_FULL_FRAMEWORK           disk-full-framework)
+set(DOCKER_NO_EXECUTOR_FRAMEWORK  docker-no-executor-framework)
+set(DYNAMIC_RESERVATION_FRAMEWORK dynamic-reservation-framework)
+set(LOAD_GENERATOR_FRAMEWORK      load-generator-framework)
+set(LONG_LIVED_EXECUTOR           long-lived-executor)
+set(LONG_LIVED_FRAMEWORK          long-lived-framework)
+set(NO_EXECUTOR_FRAMEWORK         no-executor-framework)
+set(PERSISTENT_VOLUME_FRAMEWORK   persistent-volume-framework)
+set(TEST_EXECUTOR                 test-executor)
+set(TEST_FRAMEWORK                test-framework)
+set(TEST_HTTP_EXECUTOR            test-http-executor)
+set(TEST_HTTP_FRAMEWORK           test-http-framework)
