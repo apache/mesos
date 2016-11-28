f4a6959bc386a9754db03715e33fddb58632d01a
Joseph Wu <joseph@mesosphere.io>
2016-11-28 15:32:28 -0800

    CMake: Move test-specific variables into StoutTestsConfigure.
    These definitions were originally in:
    `3rdparty/libprocess/cmake/ProcessTestsConfigure.cmake`
    
    By consolidating these definitions in `StoutTestsConfigure`, we can
    reuse the same definitions for libprocess and Mesos tests as well.
    
    This also rephrases/reformats some of the moved comments to get
    them below 80 characters per line.
    
    Review: https://reviews.apache.org/r/53961/

--- 3rdparty/stout/cmake/StoutTestsConfigure.cmake
+++ 3rdparty/stout/cmake/StoutTestsConfigure.cmake
@@ -14,16 +14,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+###############################################################
+# Defines the variables useful for tests, and exports them to the scope of
+# any file includes this file. There are a few important consequence of this:
+#
+#   * This file MUST be included before the third-party dependencies like gmock
+#     are configured/built/downloaded (if you're doing this). If this code isn't
+#     run first, then we won't know (e.g.) what folders to unpack the code to.
+#   * This file ONLY defines and exports variables for third-party dependencies
+#     that are required by the test suite, but are not a dependency that
+#     libprocess core takes. That is, this file handles the gmock dependency,
+#     but not the glog dependency (which the process library itself takes a
+#     dependency on).
+#   * This file and the config file for the libprocess "core" dependencies
+#     (e.g. glog, boost, etc.) are separate so that we can export the variables
+#     for the core dependencies (e.g., where to find the .so/.dll files) without
+#     having to also export the variables for test-only dependencies.
+
 set(
   STOUT_TESTS_TARGET stout_tests
   CACHE STRING "Target we use to refer to tests for the stout library")
 
 # COMPILER CONFIGURATION.
 #########################
+EXTERNAL("gmock" ${GMOCK_VERSION} "${MESOS_3RDPARTY_BIN}")
+
 if (APPLE)
   # GTEST on OSX needs its own tr1 tuple.
   add_definitions(-DGTEST_USE_OWN_TR1_TUPLE=1 -DGTEST_LANG_CXX11)
 endif (APPLE)
+
+set(GTEST_SRC ${GMOCK_ROOT}/gtest)
+
+# Convenience variables for include directories of third-party dependencies.
+set(GMOCK_INCLUDE_DIR ${GMOCK_ROOT}/include)
+set(GTEST_INCLUDE_DIR ${GTEST_SRC}/include)
+
+# Convenience variables for `lib` directories of built third-party dependencies.
+if (WIN32)
+  set(GMOCK_LIB_DIR ${GMOCK_ROOT}-build/${CMAKE_BUILD_TYPE})
+  set(GTEST_LIB_DIR ${GMOCK_ROOT}-build/gtest/${CMAKE_BUILD_TYPE})
+else (WIN32)
+  set(GMOCK_LIB_DIR ${GMOCK_ROOT}-lib/lib/)
+  set(GTEST_LIB_DIR ${GMOCK_ROOT}-build/gtest/lib/.libs)
+endif (WIN32)
+
+# Convenience variables for "lflags", the symbols we pass to CMake to generate
+# things like `-L/path/to/glog` or `-lglog`.
+set(GMOCK_LFLAG gmock)
+set(GTEST_LFLAG gtest)
 
 # DEFINE PROCESS LIBRARY DEPENDENCIES. Tells the process library build targets
 # download/configure/build all third-party libraries before attempting to build.
