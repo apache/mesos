9622bc9ebd8f4105e05a4e833d29f8625b6ec90d
Benjamin Hindman <benjamin.hindman@gmail.com>
2016-12-01 13:37:01 -0800

    Fixed bug on OSX with socket shutdown.

--- 3rdparty/libprocess/src/http.cpp
+++ 3rdparty/libprocess/src/http.cpp
@@ -1771,7 +1771,13 @@ Future<Nothing> serve(
         // Note that we don't look at the return value of
         // `Socket::shutdown` because the socket might already be
         // shutdown!
-        socket.shutdown(network::Socket::Shutdown::READ_WRITE);
+        //
+        // CAREFUL! We can't shutdown with Shutdown::READ_WRITE
+        // because on OSX if the socket is already shutdown with READ
+        // due to the call above then the call will fail rather than
+        // just treat it like a shutdown WRITE.
+        socket.shutdown(network::Socket::Shutdown::READ);
+        socket.shutdown(network::Socket::Shutdown::WRITE);
       });
 
   std::shared_ptr<Promise<Nothing>> promise(new Promise<Nothing>());
