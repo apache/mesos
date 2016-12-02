apache/mesos

commit bd1bd1eeee50dcb23d65dcadb4903822ffd88cfb

Added agent flags to enable/disable launching an io switchboard server.
Review: https://reviews.apache.org/r/53936/

src/slave/flags.hpp
@@ -69,6 +69,7 @@ public:
 
 #ifndef __WINDOWS__
   bool switch_user;
+  bool io_switchboard_enable_server;
 #endif // __WINDOWS__
   std::string frameworks_home;  // TODO(benh): Make an Option.
   Duration registration_backoff_factor;

src/slave/flags.cpp
@@ -248,6 +248,17 @@ mesos::internal::slave::Flags::Flags()
       "NOTE: This feature is not yet supported on Windows agent, and\n"
       "therefore the flag currently does not exist on that platform.",
       true);
+
+  add(&Flags::io_switchboard_enable_server,
+      "io_switchboard_enable_server",
+      "If set to `true`, the agent will launch a per-container sidecar\n"
+      "process that runs an HTTP serve to handle incoming\n"
+      "'ATTACH_CONTAINER_INPUT' and 'ATTACH_CONTAINER_OUTPUT' calls on\n"
+      "behalf of a container. If set to 'false', this functionality\n"
+      "will not be available. The default is 'false'.\n"
+      "NOTE: This feature is not yet supported on Windows agent, and\n"
+      "therefore the flag currently does not exist on that platform.",
+      false);
 #endif // __WINDOWS__
 
   add(&Flags::frameworks_home,

commit 49ccd13fa9cd50adaf11b4aff913a002e8b27cab

Fixed switchboard build break on Windows.
This commit will fix a build break on Windows 10, MSVC 1900, introduced
dually by commits `bd1bd1eeee50dcb23d65dcadb4903822ffd88cfb` and
`071ed5226be790aecd5ace1a7abda30b682078be`.

The recent additon of the I/O switchboard includes two things that break
the Windows build. This commit will correct both of them.

First, `switchboard_main.cpp` references several constructs that are
conditionally compiled out of the Windows build. One example is
`IOSwitchboardServerFlags`. This was never supposed to be built on
Windows, but it was accidentally added to the build, so this commit will
simply remove it from the Windows build.

Second, the I/O switchboard changes introduced a new flag,
`--io_switchboard_enable_server`, which is not implemented on Windows.
When this change was submitted, this flag was accidentally conditionally
not compiled on Windows builds, even though it is referenced in
`switchboard.cpp` (since initialization of the switchboard is different
when the server is not turned on). This commit will fix this issue by
simply moving the `#ifdef` down so that the symbol is defined and used
properly.

Review: https://reviews.apache.org/r/54324/

cmake/MesosConfigure.cmake
@@ -136,9 +136,11 @@ set(
   MESOS_FETCHER mesos-fetcher
   CACHE STRING "Target for fetcher")
 
-set(
-  MESOS_IO_SWITCHBOARD mesos-io-switchboard
-  CACHE STRING "Target for the IO switchboard")
+if (NOT WIN32)
+  set(
+    MESOS_IO_SWITCHBOARD mesos-io-switchboard
+    CACHE STRING "Target for the IO switchboard")
+endif (NOT WIN32)
 
 set(
   MESOS_MASTER mesos-master

src/slave/containerizer/mesos/CMakeLists.txt
@@ -21,27 +21,38 @@ set(CONTAINERIZER_EXECUTABLE_SRC
   main.cpp
   )
 
-set(MESOS_IO_SWITCHBOARD_SRC
-  ${MESOS_IO_SWITCHBOARD_SRC}
-  io/switchboard_main.cpp
-  )
+if (NOT WIN32)
+  set(MESOS_IO_SWITCHBOARD_SRC
+    ${MESOS_IO_SWITCHBOARD_SRC}
+    io/switchboard_main.cpp
+    )
+endif (NOT WIN32)
 
 # THE CONTAINERIZER EXECUTABLE.
 ###############################
 add_executable(${MESOS_CONTAINERIZER} ${CONTAINERIZER_EXECUTABLE_SRC})
-add_executable(${MESOS_IO_SWITCHBOARD} ${MESOS_IO_SWITCHBOARD_SRC})
+if (NOT WIN32)
+  add_executable(${MESOS_IO_SWITCHBOARD} ${MESOS_IO_SWITCHBOARD_SRC})
+endif (NOT WIN32)
 
 # ADD LINKER FLAGS (generates, e.g., -lglog on Linux).
 ######################################################
 target_link_libraries(${MESOS_CONTAINERIZER}  ${AGENT_LIBS} ${MESOS_LIBS_TARGET})
-target_link_libraries(${MESOS_IO_SWITCHBOARD} ${AGENT_LIBS} ${MESOS_LIBS_TARGET})
+if (NOT WIN32)
+  target_link_libraries(
+    ${MESOS_IO_SWITCHBOARD}
+    ${AGENT_LIBS}
+    ${MESOS_LIBS_TARGET})
+endif (NOT WIN32)
 
 # ADD BINARY DEPENDENCIES (tells CMake what to compile/build first).
 ####################################################################
-add_dependencies(
-  ${MESOS_TARGET}
-  ${MESOS_CONTAINERIZER}
-  ${MESOS_IO_SWITCHBOARD})
+add_dependencies(${MESOS_TARGET} ${MESOS_CONTAINERIZER})
+if (NOT WIN32)
+  add_dependencies(${MESOS_TARGET} ${MESOS_IO_SWITCHBOARD})
+endif (NOT WIN32)
 
 add_dependencies(${MESOS_CONTAINERIZER}  ${MESOS_LIBS_TARGET})
-add_dependencies(${MESOS_IO_SWITCHBOARD} ${MESOS_LIBS_TARGET})
+if (NOT WIN32)
+  add_dependencies(${MESOS_IO_SWITCHBOARD} ${MESOS_LIBS_TARGET})
+endif (NOT WIN32)

src/slave/flags.hpp
@@ -69,8 +69,8 @@ public:
 
 #ifndef __WINDOWS__
   bool switch_user;
-  bool io_switchboard_enable_server;
 #endif // __WINDOWS__
+  bool io_switchboard_enable_server;
   std::string frameworks_home;  // TODO(benh): Make an Option.
   Duration registration_backoff_factor;
   Duration authentication_backoff_factor;