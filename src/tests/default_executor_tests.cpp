119c4399ab4dc5c976c1918942f4add440e2cfc9
Benjamin Bannier <benjamin.bannier@mesosphere.io>
2016-12-13 10:50:59 -0800

    Acked correct task id in DefaultExecutorTest.KillTaskGroupOnTaskFailure.
    When acknowledging task status updates, this test was erroneously
    using a cached task id instead of the actual task id sent in the
    status update. This can lead to failed acknowledgements when
    status updates appear in different order.
    
    This patch just uses the task id sent as part of the status update in
    the acknowledgement.
    
    Review: https://reviews.apache.org/r/54664/

--- src/tests/default_executor_tests.cpp
+++ src/tests/default_executor_tests.cpp
@@ -565,7 +565,10 @@ TEST_F(DefaultExecutorTest, KillTaskGroupOnTaskFailure)
     call.set_type(Call::ACKNOWLEDGE);
 
     Call::Acknowledge* acknowledge = call.mutable_acknowledge();
-    acknowledge->mutable_task_id()->CopyFrom(taskInfo1.task_id());
+
+    acknowledge->mutable_task_id()->CopyFrom(
+        runningUpdate1->status().task_id());
+
     acknowledge->mutable_agent_id()->CopyFrom(offer.agent_id());
     acknowledge->set_uuid(runningUpdate1->status().uuid());
 
@@ -578,7 +581,10 @@ TEST_F(DefaultExecutorTest, KillTaskGroupOnTaskFailure)
     call.set_type(Call::ACKNOWLEDGE);
 
     Call::Acknowledge* acknowledge = call.mutable_acknowledge();
-    acknowledge->mutable_task_id()->CopyFrom(taskInfo2.task_id());
+
+    acknowledge->mutable_task_id()->CopyFrom(
+        runningUpdate2->status().task_id());
+
     acknowledge->mutable_agent_id()->CopyFrom(offer.agent_id());
     acknowledge->set_uuid(runningUpdate2->status().uuid());
 
