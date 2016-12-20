b743fb47447d467bd2198b55dce497c95e1c567c
Benjamin Bannier <benjamin.bannier@mesosphere.io>
2016-12-20 11:28:34 -0800

    Extended test coverage of posix/rlimits isolator.
    This change adds additional tests of the posix/rlimits isolator.
    
    Review: https://reviews.apache.org/r/54035/

--- src/tests/containerizer/posix_rlimits_isolator_tests.cpp
+++ src/tests/containerizer/posix_rlimits_isolator_tests.cpp
@@ -51,6 +51,222 @@ namespace tests {
 class PosixRLimitsIsolatorTest : public MesosTest {};
 
 
+// This test checks the behavior of passed invalid limits.
+TEST_F(PosixRLimitsIsolatorTest, InvalidLimits)
+{
+  Try<Owned<cluster::Master>> master = StartMaster();
+  ASSERT_SOME(master);
+
+  slave::Flags flags = CreateSlaveFlags();
+  flags.isolation = "posix/rlimits";
+
+  Owned<MasterDetector> detector = master.get()->createDetector();
+
+  Try<Owned<cluster::Slave>> slave = StartSlave(detector.get(), flags);
+  ASSERT_SOME(slave);
+
+  MockScheduler sched;
+
+  MesosSchedulerDriver driver(
+      &sched,
+      DEFAULT_FRAMEWORK_INFO,
+      master.get()->pid, DEFAULT_CREDENTIAL);
+
+  EXPECT_CALL(sched, registered(_, _, _))
+      .Times(1);
+
+  Future<vector<Offer>> offers;
+
+  EXPECT_CALL(sched, resourceOffers(_, _))
+      .WillOnce(FutureArg<1>(&offers))
+      .WillRepeatedly(Return()); // Ignore subsequent offers.
+
+  driver.start();
+
+  AWAIT_READY(offers);
+  ASSERT_NE(0u, offers->size());
+
+  TaskInfo task = createTask(
+      offers.get()[0].slave_id(),
+      offers.get()[0].resources(),
+      "true");
+
+  ContainerInfo* container = task.mutable_container();
+  container->set_type(ContainerInfo::MESOS);
+
+  // Set impossible limit soft > hard.
+  RLimitInfo rlimitInfo;
+  RLimitInfo::RLimit* rlimit = rlimitInfo.add_rlimits();
+  rlimit->set_type(RLimitInfo::RLimit::RLMT_CPU);
+  rlimit->set_soft(100);
+  rlimit->set_hard(1);
+
+  container->mutable_rlimit_info()->CopyFrom(rlimitInfo);
+
+  Future<TaskStatus> taskStatus;
+  EXPECT_CALL(sched, statusUpdate(&driver, _))
+      .WillOnce(FutureArg<1>(&taskStatus));
+
+  driver.launchTasks(offers.get()[0].id(), {task});
+
+  AWAIT_READY(taskStatus);
+  EXPECT_EQ(task.task_id(), taskStatus->task_id());
+  EXPECT_EQ(TASK_FAILED, taskStatus->state());
+  EXPECT_EQ(TaskStatus::REASON_EXECUTOR_TERMINATED, taskStatus->reason());
+
+  driver.stop();
+  driver.join();
+}
+
+
+// This test confirms that setting no values for the soft and hard
+// limits implies an unlimited resource.
+TEST_F(PosixRLimitsIsolatorTest, UnsetLimits) {
+  Try<Owned<cluster::Master>> master = StartMaster();
+  ASSERT_SOME(master);
+
+  slave::Flags flags = CreateSlaveFlags();
+  flags.isolation = "posix/rlimits";
+
+  Owned<MasterDetector> detector = master.get()->createDetector();
+
+  Try<Owned<cluster::Slave>> slave = StartSlave(detector.get(), flags);
+  ASSERT_SOME(slave);
+
+  MockScheduler sched;
+
+  MesosSchedulerDriver driver(
+      &sched,
+      DEFAULT_FRAMEWORK_INFO,
+      master.get()->pid,
+      DEFAULT_CREDENTIAL);
+
+  EXPECT_CALL(sched, registered(_, _, _))
+      .Times(1);
+
+  Future<vector<Offer>> offers;
+
+  EXPECT_CALL(sched, resourceOffers(_, _))
+      .WillOnce(FutureArg<1>(&offers))
+      .WillRepeatedly(Return()); // Ignore subsequent offers.
+
+  driver.start();
+
+  AWAIT_READY(offers);
+  ASSERT_NE(0u, offers->size());
+
+  TaskInfo task = createTask(
+      offers.get()[0].slave_id(),
+      offers.get()[0].resources(),
+      "exit `ulimit -c | grep -q unlimited`");
+
+  // Force usage of C locale as we interpret a potentially translated
+  // string in the task's command.
+  mesos::Environment::Variable* locale =
+      task.mutable_command()->mutable_environment()->add_variables();
+  locale->set_name("LC_ALL");
+  locale->set_value("C");
+
+  ContainerInfo* container = task.mutable_container();
+  container->set_type(ContainerInfo::MESOS);
+
+  // Setting rlimit for core without soft or hard limit signifies
+  // unlimited range.
+  RLimitInfo rlimitInfo;
+  RLimitInfo::RLimit* rlimit = rlimitInfo.add_rlimits();
+  rlimit->set_type(RLimitInfo::RLimit::RLMT_CORE);
+
+  container->mutable_rlimit_info()->CopyFrom(rlimitInfo);
+
+  Future<TaskStatus> statusRunning;
+  Future<TaskStatus> statusFinal;
+  EXPECT_CALL(sched, statusUpdate(&driver, _))
+      .WillOnce(FutureArg<1>(&statusRunning))
+      .WillOnce(FutureArg<1>(&statusFinal));
+
+  driver.launchTasks(offers.get()[0].id(), {task});
+
+  AWAIT_READY(statusRunning);
+  EXPECT_EQ(task.task_id(), statusRunning->task_id());
+  EXPECT_EQ(TASK_RUNNING, statusRunning->state());
+
+  AWAIT_READY(statusFinal);
+  EXPECT_EQ(task.task_id(), statusFinal->task_id());
+  EXPECT_EQ(TASK_FINISHED, statusFinal->state());
+
+  driver.stop();
+  driver.join();
+}
+
+
+// This test confirms that setting just one of the soft/hard limits is
+// an error.
+TEST_F(PosixRLimitsIsolatorTest, BothSoftAndHardLimitSet)
+{
+  Try<Owned<cluster::Master>> master = StartMaster();
+  ASSERT_SOME(master);
+
+  slave::Flags flags = CreateSlaveFlags();
+  flags.isolation = "posix/rlimits";
+
+  Owned<MasterDetector> detector = master.get()->createDetector();
+
+  Try<Owned<cluster::Slave>> slave = StartSlave(detector.get(), flags);
+  ASSERT_SOME(slave);
+
+  MockScheduler sched;
+
+  MesosSchedulerDriver driver(
+      &sched,
+      DEFAULT_FRAMEWORK_INFO,
+      master.get()->pid,
+      DEFAULT_CREDENTIAL);
+
+  EXPECT_CALL(sched, registered(_, _, _))
+      .Times(1);
+
+  Future<vector<Offer>> offers;
+
+  EXPECT_CALL(sched, resourceOffers(_, _))
+      .WillOnce(FutureArg<1>(&offers))
+      .WillRepeatedly(Return()); // Ignore subsequent offers.
+
+  driver.start();
+
+  AWAIT_READY(offers);
+  ASSERT_NE(0u, offers->size());
+
+  TaskInfo task = createTask(
+      offers.get()[0].slave_id(),
+      offers.get()[0].resources(),
+      "true");
+
+  ContainerInfo* container = task.mutable_container();
+  container->set_type(ContainerInfo::MESOS);
+
+  RLimitInfo rlimitInfo;
+  RLimitInfo::RLimit* rlimit = rlimitInfo.add_rlimits();
+  rlimit->set_type(RLimitInfo::RLimit::RLMT_CORE);
+  rlimit->set_soft(1);
+
+  container->mutable_rlimit_info()->CopyFrom(rlimitInfo);
+
+  Future<TaskStatus> status;
+  EXPECT_CALL(sched, statusUpdate(&driver, _))
+      .WillOnce(FutureArg<1>(&status));
+
+  driver.launchTasks(offers.get()[0].id(), {task});
+
+  AWAIT_READY(status);
+  EXPECT_EQ(task.task_id(), status->task_id());
+  EXPECT_EQ(TASK_FAILED, status->state());
+  EXPECT_EQ(TaskStatus::REASON_EXECUTOR_TERMINATED, status->reason());
+
+  driver.stop();
+  driver.join();
+}
+
+
 // This test confirms that if a task exceeds configured resource
 // limits it is forcibly terminated.
 TEST_F(PosixRLimitsIsolatorTest, TaskExceedingLimit)
@@ -75,9 +291,10 @@ TEST_F(PosixRLimitsIsolatorTest, TaskExceedingLimit)
       DEFAULT_CREDENTIAL);
 
   EXPECT_CALL(sched, registered(_, _, _))
-    .Times(1);
+      .Times(1);
 
   Future<vector<Offer>> offers;
+
   EXPECT_CALL(sched, resourceOffers(_, _))
       .WillOnce(FutureArg<1>(&offers))
       .WillRepeatedly(Return()); // Ignore subsequent offers.
