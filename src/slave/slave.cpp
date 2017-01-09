c61227383179ccaf22c5b58376aa3b4713ffe38a
Neil Conway <neil.conway@gmail.com>
2017-01-09 12:17:44 -0800

    Improved comments in agent.
    Review: https://reviews.apache.org/r/55300/

--- src/slave/slave.cpp
+++ src/slave/slave.cpp
@@ -1155,9 +1155,10 @@ void Slave::registered(
       VLOG(1) << "Checkpointing SlaveInfo to '" << path << "'";
       CHECK_SOME(state::checkpoint(path, info));
 
-      // If we don't get a ping from the master, trigger a
-      // re-registration. This needs to be done once registered,
-      // in case we never receive an initial ping.
+      // Setup a timer so that the agent attempts to re-register if it
+      // doesn't receive a ping from the master for an extended period
+      // of time. This needs to be done once registered, in case we
+      // never receive an initial ping.
       Clock::cancel(pingTimer);
 
       pingTimer = delay(
@@ -1235,9 +1236,10 @@ void Slave::reregistered(
       state = RUNNING;
       statusUpdateManager->resume(); // Resume status updates.
 
-      // If we don't get a ping from the master, trigger a
-      // re-registration. This needs to be done once re-registered,
-      // in case we never receive an initial ping.
+      // Setup a timer so that the agent attempts to re-register if it
+      // doesn't receive a ping from the master for an extended period
+      // of time. This needs to be done once re-registered, in case we
+      // never receive an initial ping.
       Clock::cancel(pingTimer);
 
       pingTimer = delay(
@@ -4270,12 +4272,13 @@ void Slave::executorMessage(
   metrics.valid_framework_messages++;
 }
 
+
 void Slave::ping(const UPID& from, bool connected)
 {
   VLOG(1) << "Received ping from " << from;
 
   if (!connected && state == RUNNING) {
-    // This could happen if there is a one way partition between
+    // This could happen if there is a one-way partition between
     // the master and slave, causing the master to get an exited
     // event and marking the slave disconnected but the slave
     // thinking it is still connected. Force a re-registration with
@@ -4285,11 +4288,7 @@ void Slave::ping(const UPID& from, bool connected)
     detection.discard();
   }
 
-  // If we don't get a ping from the master, trigger a
-  // re-registration. This can occur when the master no
-  // longer considers the slave to be registered, so it is
-  // essential for the slave to attempt a re-registration
-  // when this occurs.
+  // We just received a ping from the master, so reset the ping timer.
   Clock::cancel(pingTimer);
 
   pingTimer = delay(
