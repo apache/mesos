apache/mesos

commit c7fc1377b33c4eb83a01167bdb53c102c06b9a99

CMake: Added correct values for some build flags.
Currently, the CMake build does not implement certain build flags
as the autotools solution does. For example, `BUILD_DATE` is hard coded,
and the `BUILD_USER` flags is always "frank". This commit will add
correct values for all such flags, with the exception of `BUILD_FLAGS`.

In addition, this commit will take this opportunity to clean up the
organization of the build flags. Currently, we call `add_definitions`
individually for each flag we wish to define. This commit will introduce
a new variable, `MESOS_CPPFLAGS`, which holds the build flags for the
project. We call `add_definitions` once, when `MESOS_CPPFLAGS` is
completely populated.

Review: https://reviews.apache.org/r/55040/

cmake/CompilationConfigure.cmake
@@ -190,16 +190,22 @@ if (WIN32)
   # to give the build system fine-grained control over what code is #ifdef'd
   # out in the future.  Using only flags defined by our build system to control
   # this logic is the clearest and most stable way of accomplishing this.
-  add_definitions(-D__WINDOWS__)
-  add_definitions(-DHAVE_LIBZ)
+  set(MESOS_CPPFLAGS
+    ${MESOS_CPPFLAGS}
+    -D__WINDOWS__
+    -DHAVE_LIBZ
+    )
 
   # Defines to disable warnings generated by Visual Studio when using
   # deprecated functions in CRT and the use of insecure functions in CRT.
   # TODO(dpravat): Once the entire codebase is changed to use secure CRT
   # functions, these defines should be removed.
-  add_definitions(-D_SCL_SECURE_NO_WARNINGS)
-  add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-  add_definitions(-D_CRT_NONSTDC_NO_WARNINGS)
+  set(MESOS_CPPFLAGS
+    ${MESOS_CPPFLAGS}
+    -D_SCL_SECURE_NO_WARNINGS
+    -D_CRT_SECURE_NO_WARNINGS
+    -D_CRT_NONSTDC_NO_WARNINGS
+    )
 
   # Directory structure definitions.
   # TODO(hausdorff): (MESOS-5455) These are placeholder values.
@@ -221,8 +227,11 @@ if (WIN32)
   #
   # [1] https://google-glog.googlecode.com/svn/trunk/doc/glog.html#windows
   # [2] https://code.google.com/p/google-glog/source/browse/trunk/src/windows/glog/logging.h?r=113
-  add_definitions(-DNOGDI)
-  add_definitions(-DNOMINMAX)
+  set(MESOS_CPPFLAGS
+    ${MESOS_CPPFLAGS}
+    -DNOGDI
+    -DNOMINMAX
+    )
 endif (WIN32)
 
 # GLOBAL CONFIGURATION.
@@ -232,19 +241,55 @@ if (HAS_AUTHENTICATION)
   # `-DHAS_AUTHENTICATION=${HAS_AUTHENTICATION}`, as this will define the
   # symbol, and our intention is to only define it if the CMake variable
   # `HAS_AUTHENTICATION` is set.
-  add_definitions(-DHAS_AUTHENTICATION=1)
+  set(MESOS_CPPFLAGS
+    ${MESOS_CPPFLAGS}
+    -DHAS_AUTHENTICATION=1
+    )
 endif (HAS_AUTHENTICATION)
 
 # Enable the INT64 support for PicoJSON.
-add_definitions(-DPICOJSON_USE_INT64)
-
 # NOTE: PicoJson requires __STDC_FORMAT_MACROS to be defined before importing
 # 'inttypes.h'.  Since other libraries may also import this header, it must
 # be globally defined so that PicoJson has access to the macros, regardless
 # of the order of inclusion.
-add_definitions(-D__STDC_FORMAT_MACROS)
+set(MESOS_CPPFLAGS
+  ${MESOS_CPPFLAGS}
+  -DPICOJSON_USE_INT64
+  -D__STDC_FORMAT_MACROS
+  )
 
-add_definitions(-DPKGLIBEXECDIR="${PKG_LIBEXEC_INSTALL_DIR}")
-add_definitions(-DLIBDIR="${LIB_INSTALL_DIR}")
-add_definitions(-DVERSION="${PACKAGE_VERSION}")
-add_definitions(-DPKGDATADIR="${DATA_INSTALL_PREFIX}")
+set(MESOS_CPPFLAGS
+  ${MESOS_CPPFLAGS}
+  -DPKGLIBEXECDIR="${PKG_LIBEXEC_INSTALL_DIR}"
+  -DLIBDIR="${LIB_INSTALL_DIR}"
+  -DVERSION="${PACKAGE_VERSION}"
+  -DPKGDATADIR="${DATA_INSTALL_PREFIX}"
+  )
+
+# Add build information to Mesos flags.
+string(TIMESTAMP BUILD_DATE "%Y-%m-%d %H:%M:%S UTC" UTC)
+string(TIMESTAMP BUILD_TIME "%s" UTC)
+if (WIN32)
+  set(BUILD_USER "$ENV{USERNAME}")
+else (WIN32)
+  set(BUILD_USER "$ENV{USER}")
+endif (WIN32)
+
+# TODO(hausdorff): (MESOS-5902) Populate this value when we integrate Java
+# support.
+set(BUILD_JAVA_JVM_LIBRARY "")
+
+# NOTE: The quotes in these definitions are necessary. Without them, the
+# preprocessor will interpret the symbols as (e.g.) int literals and uquoted
+# identifiers, rather than the string values our code expects.
+set(MESOS_CPPFLAGS
+  ${MESOS_CPPFLAGS}
+  -DUSE_STATIC_LIB
+  -DBUILD_DATE="${BUILD_DATE}"
+  -DBUILD_TIME="${BUILD_TIME}"
+  -DBUILD_USER="${BUILD_USER}"
+  -DBUILD_JAVA_JVM_LIBRARY="${BUILD_JAVA_JVM_LIBRARY}"
+  )
+
+# TODO(hausdorff): (MESOS-5455) `BUILD_FLAGS` is currently a placeholder value.
+add_definitions(${MESOS_CPPFLAGS} -DBUILD_FLAGS="")

src/CMakeLists.txt
@@ -474,20 +474,10 @@ if (LINUX)
   set(MESOS_SRC ${MESOS_SRC} ${LINUX_SRC})
 endif (LINUX)
 
-# TODO(hausdorff): (MESOS-5455) These are placeholder values. Transition away
-# from them.
-set(BUILD_DATE "\"2016-3-3 10:20\"")
-set(BUILD_TIME "\"1111100\"")
-set(BUILD_USER "\"frank\"")
-set(BUILD_FLAGS "\"\"")
-set(BUILD_JAVA_JVM_LIBRARY "")
-
 # Include source for Windows build.
 if (WIN32)
   set(MESOS_SRC ${MESOS_SRC} ${WIN32_SRC})
 endif (WIN32)
-
-add_definitions(-DUSE_STATIC_LIB -DBUILD_DATE=${BUILD_DATE} -DBUILD_TIME=${BUILD_TIME} -DBUILD_USER=${BUILD_USER} -DBUILD_FLAGS=${BUILD_FLAGS} -DBUILD_JAVA_JVM_LIBRARY="")
 
 # INCLUDE DIRECTIVES FOR MESOS LIBRARY (generates, e.g., -I/path/to/thing
 # on Linux).

commit 6d7b25adc67af53ba987caf7c9e838b2e2aa85ca

CMake: Add `build_config.hpp.in` for `BUILD_TIME` variables.
Commit c7fc1377b introduced a bug that prevented any incremental
recompilation.  By defining the `BUILD_TIME` in `MESOS_CPPFLAGS`,
every build was seen as having a root dependency (the flags)
change, causing a rebuild of every single source in Mesos
(especially bad on ccache-less systems).

This patch introduces a CMake `configure_file` directive which
takes in `build_config.hpp.in` and emits `build_config.hpp` with the
`BUILD_TIME`, `BUILD_DATE`, and `BUILD_USER` variables defined.
As this is a CMake-only file, we guard the new header with a static
definition `-DUSE_CMAKE_BUILD_CONFIG`.  The result is that the date,
time, and user are set at the point of configuration (invocation of
CMake) instead of at build, thus allowing for incremental rebuilds.

Review: https://reviews.apache.org/r/57052/

cmake/CompilationConfigure.cmake
@@ -286,14 +286,27 @@ set(MESOS_CPPFLAGS
   -DPKGDATADIR="${DATA_INSTALL_PREFIX}"
   )
 
-# Add build information to Mesos flags.
+# Calculate some build information.
 string(TIMESTAMP BUILD_DATE "%Y-%m-%d %H:%M:%S UTC" UTC)
-string(TIMESTAMP BUILD_TIME "%s" UTC)
 if (WIN32)
+  string(TIMESTAMP BUILD_TIME "%s" UTC)
   set(BUILD_USER "$ENV{USERNAME}")
 else (WIN32)
+  execute_process(
+    COMMAND date +%s
+    OUTPUT_VARIABLE BUILD_TIME
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
   set(BUILD_USER "$ENV{USER}")
 endif (WIN32)
+
+# Emit the BUILD_DATE, BUILD_TIME, and BUILD_USER variables into a file.
+# This will be updated each time `cmake` is run.
+configure_file(
+  "${CMAKE_SOURCE_DIR}/src/common/build_config.hpp.in"
+  "${CMAKE_BINARY_DIR}/src/common/build_config.hpp"
+  @ONLY
+  )
 
 # TODO(hausdorff): (MESOS-5902) Populate this value when we integrate Java
 # support.
@@ -305,9 +318,7 @@ set(BUILD_JAVA_JVM_LIBRARY "")
 set(MESOS_CPPFLAGS
   ${MESOS_CPPFLAGS}
   -DUSE_STATIC_LIB
-  -DBUILD_DATE="${BUILD_DATE}"
-  -DBUILD_TIME="${BUILD_TIME}"
-  -DBUILD_USER="${BUILD_USER}"
+  -DUSE_CMAKE_BUILD_CONFIG
   -DBUILD_JAVA_JVM_LIBRARY="${BUILD_JAVA_JVM_LIBRARY}"
   )
 

src/common/build.cpp
@@ -23,6 +23,15 @@
 
 #include "common/build.hpp"
 
+// NOTE: On CMake, instead of defining `BUILD_DATE|TIME|USER` as
+// compiler flags, we instead emit a header file with the definitions.
+// This facilitates incremental builds as the compiler flags will
+// no longer change with every invocation of the build.
+// TODO(josephw): After deprecating autotools, remove this guard.
+#ifdef USE_CMAKE_BUILD_CONFIG
+#include "common/build_config.hpp"
+#endif // USE_CMAKE_BUILD_CONFIG
+
 namespace mesos {
 namespace internal {
 namespace build {

src/common/build_config.hpp.in
@@ -1,0 +1,26 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __COMMON_BUILD_CONFIG_HPP__
+#define __COMMON_BUILD_CONFIG_HPP__
+
+#cmakedefine BUILD_DATE "@BUILD_DATE@"
+#cmakedefine BUILD_TIME "@BUILD_TIME@"
+#cmakedefine BUILD_USER "@BUILD_USER@"
+
+// TODO(andschwa): Define GIT_SHA etc. for parity with autotools.
+
+#endif // __COMMON_BUILD_CONFIG_HPP__