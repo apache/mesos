apache/mesos

commit 5f159cdcbe07ce453cc9b0ecc8834a8551a19f10

Added the `FollowSymlink` argument for `os::stat` on Windows.
Review: https://reviews.apache.org/r/56028/

3rdparty/stout/include/stout/os/windows/stat.hpp
@@ -31,9 +31,15 @@
 namespace os {
 namespace stat {
 
-inline bool isdir(const std::string& path)
+inline bool isdir(
+    const std::string& path,
+    const FollowSymlink follow = FOLLOW_SYMLINK)
 {
   struct _stat s;
+
+  if (follow == DO_NOT_FOLLOW_SYMLINK) {
+      return Error("Non-following stat not supported for '" + path + "'");
+  }
 
   if (::_stat(path.c_str(), &s) < 0) {
     return false;
@@ -43,9 +49,15 @@ inline bool isdir(const std::string& path)
 }
 
 
-inline bool isfile(const std::string& path)
+inline bool isfile(
+    const std::string& path,
+    const FollowSymlink follow = FOLLOW_SYMLINK)
 {
   struct _stat s;
+
+  if (follow == DO_NOT_FOLLOW_SYMLINK) {
+      return Error("Non-following stat not supported for '" + path + "'");
+  }
 
   if (::_stat(path.c_str(), &s) < 0) {
     return false;
@@ -62,15 +74,6 @@ inline bool islink(const std::string& path)
 
   return symlink.isSome();
 }
-
-
-// Describes the different semantics supported for the implementation
-// of `size` defined below.
-enum FollowSymlink
-{
-  DO_NOT_FOLLOW_SYMLINK,
-  FOLLOW_SYMLINK
-};
 
 
 // Returns the size in Bytes of a given file system entry. When
@@ -109,15 +112,19 @@ inline Try<Bytes> size(
 }
 
 
-inline Try<long> mtime(const std::string& path)
+inline Try<long> mtime(
+    const std::string& path,
+    const FollowSymlink follow = FOLLOW_SYMLINK)
 {
-  Try<::internal::windows::SymbolicLink> symlink =
-    ::internal::windows::query_symbolic_link_data(path);
+  if (follow == DO_NOT_FOLLOW_SYMLINK) {
+    Try<::internal::windows::SymbolicLink> symlink =
+      ::internal::windows::query_symbolic_link_data(path);
 
-  if (symlink.isSome()) {
-    return Error(
-        "Requested mtime for '" + path +
-        "', but symbolic links don't have an mtime on Windows");
+    if (symlink.isSome()) {
+      return Error(
+          "Requested mtime for '" + path +
+          "', but symbolic links don't have an mtime on Windows");
+    }
   }
 
   struct _stat s;
@@ -137,9 +144,15 @@ inline Try<long> mtime(const std::string& path)
 }
 
 
-inline Try<mode_t> mode(const std::string& path)
+inline Try<mode_t> mode(
+    const std::string& path,
+    const FollowSymlink follow = FOLLOW_SYMLINK)
 {
   struct _stat s;
+
+  if (follow == DO_NOT_FOLLOW_SYMLINK) {
+      return Error("Non-following stat not supported for '" + path + "'");
+  }
 
   if (::_stat(path.c_str(), &s) < 0) {
     return ErrnoError("Error invoking stat for '" + path + "'");
@@ -149,9 +162,17 @@ inline Try<mode_t> mode(const std::string& path)
 }
 
 
-inline Try<dev_t> dev(const std::string& path)
+inline Try<dev_t> dev(
+    const std::string& path,
+    const FollowSymlink follow = FOLLOW_SYMLINK)
 {
   struct _stat s;
+
+  if (follow == DO_NOT_FOLLOW_SYMLINK) {
+      return WindowsError(
+          ERROR_NOT_SUPPORTED,
+          "Error invoking stat for '" + path + "'");
+  }
 
   if (::_stat(path.c_str(), &s) < 0) {
     return ErrnoError("Error invoking stat for '" + path + "'");
@@ -161,9 +182,17 @@ inline Try<dev_t> dev(const std::string& path)
 }
 
 
-inline Try<ino_t> inode(const std::string& path)
+inline Try<ino_t> inode(
+    const std::string& path,
+    const FollowSymlink follow = FOLLOW_SYMLINK)
 {
   struct _stat s;
+
+  if (follow == DO_NOT_FOLLOW_SYMLINK) {
+      return WindowsError(
+          ERROR_NOT_SUPPORTED,
+          "Error invoking stat for '" + path + "'");
+  }
 
   if (::_stat(path.c_str(), &s) < 0) {
     return ErrnoError("Error invoking stat for '" + path + "'");

commit 1de39e676a0dc5f78eeff303cb0eba5467168b9f

Windows: Fixed return of bad types in stat.hpp.
Commit 5f159cdcb introduced `return Error(...)` logic to functions
which return `bool`, not `Try<bool>`, which broke the Windows build.

Furthermore, in the instances of `isdir` and `isfile`, erroring
when asked to not follow a symlink is not correct. The semantics
of symlinks provide clear answers to `isdir` and `isfile` when the
target is a link, and is not being followed (it is neither a regular
file nor a directory).

We explicitly match the POSIX semantics for `isfile` where `S_IFREG`
returns `false` for symbolic links.

For the functions `mode` and `dev`, which return types wrapped by `Try`,
we should only error if asked not to follow symlinks, and the target is
actually a symlink. If it is not a symlink to begin with, we should not
prematurely error. If it is a symlink, we should error because there is
no equivalent of `lstat` on Windows to obtain `st_mode` or `st_dev` of a
symlink itself.

Review: https://reviews.apache.org/r/57926/

3rdparty/stout/include/stout/os/windows/stat.hpp
@@ -31,14 +31,19 @@
 namespace os {
 namespace stat {
 
+// Forward declaration.
+inline bool islink(const std::string& path);
+
 inline bool isdir(
     const std::string& path,
     const FollowSymlink follow = FOLLOW_SYMLINK)
 {
   struct _stat s;
 
-  if (follow == DO_NOT_FOLLOW_SYMLINK) {
-      return Error("Non-following stat not supported for '" + path + "'");
+  // A symlink itself is not a directory.
+  // If it's not a link, we ignore `follow`.
+  if (follow == DO_NOT_FOLLOW_SYMLINK && islink(path)) {
+    return false;
   }
 
   if (::_stat(path.c_str(), &s) < 0) {
@@ -55,8 +60,12 @@ inline bool isfile(
 {
   struct _stat s;
 
-  if (follow == DO_NOT_FOLLOW_SYMLINK) {
-      return Error("Non-following stat not supported for '" + path + "'");
+  // A symlink itself is a file, but not a regular file.
+  // On POSIX, this check is done with `S_IFREG`, which
+  // returns false for symbolic links.
+  // If it's not a link, we ignore `follow`.
+  if (follow == DO_NOT_FOLLOW_SYMLINK && islink(path)) {
+    return false;
   }
 
   if (::_stat(path.c_str(), &s) < 0) {
@@ -150,8 +159,8 @@ inline Try<mode_t> mode(
 {
   struct _stat s;
 
-  if (follow == DO_NOT_FOLLOW_SYMLINK) {
-      return Error("Non-following stat not supported for '" + path + "'");
+  if (follow == DO_NOT_FOLLOW_SYMLINK && islink(path)) {
+    return Error("lstat not supported for symlink '" + path + "'");
   }
 
   if (::_stat(path.c_str(), &s) < 0) {
@@ -168,8 +177,8 @@ inline Try<dev_t> dev(
 {
   struct _stat s;
 
-  if (follow == DO_NOT_FOLLOW_SYMLINK) {
-      return Error("Non-following stat not supported for '" + path + "'");
+  if (follow == DO_NOT_FOLLOW_SYMLINK && islink(path)) {
+    return Error("lstat not supported for symlink '" + path + "'");
   }
 
   if (::_stat(path.c_str(), &s) < 0) {