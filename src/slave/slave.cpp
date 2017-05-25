27a57a9e3a5a10d1fe03207ada3a409a570a0b48
Benjamin Mahler <bmahler@apache.org>
2017-05-25 15:23:30 -0700

    Don't crash when re-registering executor from an unknown framework.
    Rather than crashing the agent, the agent will tell the executor
    to shut down.
    
    Review: https://reviews.apache.org/r/59589

--- src/slave/slave.cpp
+++ src/slave/slave.cpp
@@ -3978,11 +3978,14 @@ void Slave::reregisterExecutor(
     return;
   }
 
-  LOG(INFO) << "Re-registering executor '" << executorId
-            << "' of framework " << frameworkId;
+  if (!frameworks.contains(frameworkId)) {
+    LOG(WARNING) << "Shutting down executor '" << executorId << "'"
+                 << " of framework " << frameworkId
+                 << " because the framework is unknown";
 
-  CHECK(frameworks.contains(frameworkId))
-    << "Unknown framework " << frameworkId;
+    reply(ShutdownExecutorMessage());
+    return;
+  }
 
   Framework* framework = frameworks[frameworkId];
 
