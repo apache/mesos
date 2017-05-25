127c16ff553758171664bd27c1fde95e69b71d18
Benjamin Mahler <bmahler@apache.org>
2017-05-25 16:49:22 -0700

    Added logging of executor re-registration messages.
    Review: https://reviews.apache.org/r/59592

--- src/slave/slave.cpp
+++ src/slave/slave.cpp
@@ -3570,6 +3570,10 @@ void Slave::reregisterExecutor(
         state == RUNNING || state == TERMINATING)
     << state;
 
+  LOG(INFO) << "Received re-registration message from"
+            << " executor '" << executorId << "'"
+            << " of framework " << frameworkId;
+
   if (state == TERMINATING) {
     LOG(WARNING) << "Shutting down executor '" << executorId << "'"
                  << " of framework " << frameworkId
