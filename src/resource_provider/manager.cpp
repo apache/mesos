17c563e455b13bfd9dd371f4f4643677163559a9
Jie Yu <yujie.jay@gmail.com>
2017-06-08 12:09:01 -0700

    Added stubs for the ResourceProviderManager.
    ResourceProviderManager will be used to manage resource providers,
    both local and external. It'll be used by the agent to manage local
    resource providers and used by the master to manage external resource
    providers. It'll expose an API endpoint allowing resource providers to
    subscribe to the manager.
    
    Review: https://reviews.apache.org/r/59934

---
+++ src/resource_provider/manager.cpp
@@ -0,0 +1,90 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "resource_provider/manager.hpp"
+
+#include <glog/logging.h>
+
+#include <process/dispatch.hpp>
+#include <process/id.hpp>
+#include <process/process.hpp>
+
+namespace http = process::http;
+
+using process::Failure;
+using process::Future;
+using process::Owned;
+using process::Process;
+using process::ProcessBase;
+
+using process::dispatch;
+using process::spawn;
+using process::terminate;
+using process::wait;
+
+using process::http::authentication::Principal;
+
+namespace mesos {
+namespace internal {
+
+class ResourceProviderManagerProcess
+  : public Process<ResourceProviderManagerProcess>
+{
+public:
+  ResourceProviderManagerProcess()
+    : ProcessBase(process::ID::generate("resource-provider-manager")) {}
+
+  Future<http::Response> api(
+      const http::Request& request,
+      const Option<Principal>& principal);
+};
+
+
+Future<http::Response> ResourceProviderManagerProcess::api(
+    const http::Request& request,
+    const Option<Principal>& principal)
+{
+  return Failure("Unimplemented");
+}
+
+
+ResourceProviderManager::ResourceProviderManager()
+  : process(new ResourceProviderManagerProcess())
+{
+  spawn(CHECK_NOTNULL(process.get()));
+}
+
+
+ResourceProviderManager::~ResourceProviderManager()
+{
+  terminate(process.get());
+  wait(process.get());
+}
+
+
+Future<http::Response> ResourceProviderManager::api(
+    const http::Request& request,
+    const Option<Principal>& principal) const
+{
+  return dispatch(
+      process.get(),
+      &ResourceProviderManagerProcess::api,
+      request,
+      principal);
+}
+
+} // namespace internal {
+} // namespace mesos {
---
+++ src/resource_provider/manager.hpp
@@ -0,0 +1,56 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __RESOURCE_PROVIDER_MANAGER_HPP__
+#define __RESOURCE_PROVIDER_MANAGER_HPP__
+
+#include <process/authenticator.hpp>
+#include <process/future.hpp>
+#include <process/http.hpp>
+#include <process/owned.hpp>
+
+namespace mesos {
+namespace internal {
+
+// Forward declarations.
+class ResourceProviderManagerProcess;
+
+
+class ResourceProviderManager
+{
+public:
+  ResourceProviderManager();
+  ~ResourceProviderManager();
+
+  ResourceProviderManager(
+      const ResourceProviderManager& other) = delete;
+
+  ResourceProviderManager& operator=(
+      const ResourceProviderManager& other) = delete;
+
+  // The API endpoint handler.
+  process::Future<process::http::Response> api(
+      const process::http::Request& request,
+      const Option<process::http::authentication::Principal>& principal) const;
+
+private:
+  process::Owned<ResourceProviderManagerProcess> process;
+};
+
+} // namespace internal {
+} // namespace mesos {
+
+#endif // __RESOURCE_PROVIDER_MANAGER_HPP__
