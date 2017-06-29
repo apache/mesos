5ae32b2e920dc58c365ff3708bbdeda776ada263
Armand Grillet <agrillet@mesosphere.io>
2017-06-29 17:35:33 +0900

    Fixed a regression in support/apply-reviews.py.
    While cleaning up this script for PyLint conformance in
    09fc35978c77ed0f22df1823f5c9220f1ed098ed we introduced usage of an
    incorrect value \`patch\` when this script was run under Windows.
    
    This commit restores the previous data flow. The issue was the cause
    of the Mesos Reviewbot Windows error messages seen when review
    requests were posted on the Review Board the past few days.
    
    Review: https://reviews.apache.org/r/60529/

--- support/apply-reviews.py
+++ support/apply-reviews.py
@@ -186,12 +186,12 @@ def ssl_create_default_context():
 def fetch_patch(options):
     """Fetches a patch from Review Board or GitHub."""
     if platform.system() == 'Windows':
-        patch = urllib2.urlopen(
+        response = urllib2.urlopen(
             patch_url(options),
             context=ssl_create_default_context())
 
         with open('%s.patch' % patch_id(options), 'wb') as patch:
-            patch.write(patch.read())
+            patch.write(response.read())
     else:
         # NOTE: SSL contexts are only supported in Python 2.7.9+. The version
         # of Python running on the non-Windows ASF CI machines is sometimes
