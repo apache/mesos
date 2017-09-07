0db6a64670c64a7f905e883c1e7b429f03578654
Kapil Arya <kapil@mesosphere.io>
2017-09-07 13:37:51 -0400

    Updated RC tagging+voting mechanism.
    Updated RC tagging+voting mechanism by eliminating duplicated work in
    tag/vote scripts. The vote script has been updated to incorporate the
    steps from tag.sh.
    
    Review: https://reviews.apache.org/r/62166

--- support/vote.sh
+++ support/vote.sh
@@ -30,15 +30,18 @@ read -p "Hit enter to continue ... "
 
 MESOS_GIT_URL="https://git-wip-us.apache.org/repos/asf/mesos.git"
 
-WORK_DIR=`mktemp -d /tmp/mesos-vote-XXXX`
+# Get the absolute path of the local git clone.
+MESOS_GIT_LOCAL=$(cd "$(dirname $0)"/..; pwd)
+
+WORK_DIR=`mktemp -d /tmp/mesos-tag-vote-XXXX`
 atexit "rm -rf ${WORK_DIR}"
 
 pushd ${WORK_DIR}
 
 echo "${GREEN}Checking out ${TAG}${NORMAL}"
 
-# First checkout the release tag.
-git clone --depth 1 --branch ${TAG} ${MESOS_GIT_URL}
+# Make a shallow clone from the local git repository.
+git clone --shared ${MESOS_GIT_LOCAL} --branch ${TAG} mesos
 
 pushd mesos
 
@@ -68,6 +71,9 @@ MAVEN_REPO=""
 while [ -z ${MAVEN_REPO} ]; do
   read  -p "Please *close* the staging repository and provide its URL here: " MAVEN_REPO
 done
+
+echo "${GREEN}Pushing the git tag to the repository...${NORMAL}"
+git push ${MESOS_GIT_URL} refs/tags/${TAG}
 
 # Build the distribution.
 echo "${GREEN}Building the distribution ...${NORMAL}"
@@ -149,7 +155,7 @@ ${SVN_DEV_REPO}/${TAG}/${TARBALL}.asc
 The PGP key used to sign the release is here:
 https://dist.apache.org/repos/dist/release/mesos/KEYS
 
-The JAR is up in Maven in a staging repository here:
+The JAR is in a staging repository here:
 ${MAVEN_REPO}
 
 Please vote on releasing this package as Apache Mesos ${VERSION}!
