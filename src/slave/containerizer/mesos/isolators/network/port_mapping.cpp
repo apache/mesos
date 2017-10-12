58b722a4aa6b14e725c004fc3524ffbf86b4898e
Jie Yu <yujie.jay@gmail.com>
2017-10-12 14:08:42 -0700

    Synced the MTU for container eth0 in port mapping isolator.
    Currently, the MTU for container eth0 uses a default MTU value (which
    is 1500). However, the host eth0's MTU is not guaranteed to be the
    default value (e.g., on EC2). And this will breaks path MTU discovery.
    
    This patches forces the MTU of the container's eth0 to be the same as
    the host eth0. See more details in MESOS-7130.
    
    Review: https://reviews.apache.org/r/62743/

--- src/slave/containerizer/mesos/isolators/network/port_mapping.cpp
+++ src/slave/containerizer/mesos/isolators/network/port_mapping.cpp
@@ -3966,7 +3966,8 @@ string PortMappingIsolatorProcess::scripts(Info* info)
   // checksum offloading ensures the TCP layer will checksum and drop
   // it.
   script << "ethtool -K " << eth0 << " rx off\n";
-  script << "ip link set " << eth0 << " address " << hostMAC << " up\n";
+  script << "ip link set " << eth0 << " address " << hostMAC
+         << " mtu " << hostEth0MTU << " up\n";
   script << "ip addr add " << hostIPNetwork << " dev " << eth0 << "\n";
 
   // Set up the default gateway to match that of eth0.
