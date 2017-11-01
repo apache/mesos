712a951b44361985379a830f231e28194ebe0172
Michael Park <mpark@apache.org>
2017-11-01 14:53:46 -0700

    Removed `apply-review.sh` script. Please use `apply-reviews.py`.

--- support/apply-review.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -f support/apply-reviews.py ]; then
-  echo 'Please run this script from the root of Mesos source directory.'
-  exit 1
-fi
-
-exec python support/apply-reviews.py "$@"
