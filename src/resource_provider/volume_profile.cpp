86548e1a6ef5e0c4e7bc03e861c4b5c4dc7eae38
Joseph Wu <josephwu@apache.org>
2017-11-20 16:29:18 -0800

    Added default VolumeProfile module implementation.
    By default, Mesos and the Storage Local Resource Provider (SLRP) will
    not expect profiles to be used. This is partly due to the experimental
    nature of the "profile" field and partly because some explicit operator
    intervention is required to get profiles defined. There is no default
    profile that is accepted by CSI plugins.
    
    Review: https://reviews.apache.org/r/64352

---
+++ src/resource_provider/volume_profile.cpp
@@ -0,0 +1,101 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+
+#include <mesos/mesos.hpp>
+
+#include <mesos/module/volume_profile.hpp>
+
+#include <mesos/resource_provider/volume_profile.hpp>
+
+#include <process/future.hpp>
+
+#include <stout/hashset.hpp>
+
+#include <csi/spec.hpp>
+
+#include "module/manager.hpp"
+
+using std::string;
+using std::tuple;
+
+using process::Failure;
+using process::Future;
+
+using google::protobuf::Map;
+
+namespace mesos {
+namespace internal {
+
+// The default implementation does nothing and always returns a Failure
+// whenever called.
+class DefaultVolumeProfileAdaptor : public VolumeProfileAdaptor
+{
+public:
+  DefaultVolumeProfileAdaptor() {}
+
+  ~DefaultVolumeProfileAdaptor() {}
+
+  virtual Future<VolumeProfileAdaptor::ProfileInfo> translate(
+      const string& profile,
+      const string& csiPluginInfoType) override
+  {
+    return Failure("By default, volume profiles are not supported");
+  }
+
+  virtual Future<hashset<string>> watch(
+      const hashset<string>& knownProfiles,
+      const string& csiPluginInfoType) override
+  {
+    // If the input set of profiles is empty, that means the caller is in sync
+    // with this module. Hence, we return a future that will never be satisified
+    // because this module will never return a non-empty set of profiles.
+    if (knownProfiles.empty()) {
+      return Future<hashset<string>>();
+    }
+
+    return hashset<string>::EMPTY;
+  }
+};
+
+} // namespace internal {
+
+
+Try<VolumeProfileAdaptor*> VolumeProfileAdaptor::create(
+    const Option<string>& moduleName)
+{
+  if (moduleName.isNone()) {
+    LOG(INFO) << "Creating default volume profile adaptor module";
+    return new internal::DefaultVolumeProfileAdaptor();
+  }
+
+  LOG(INFO)
+    << "Creating volume profile adaptor module '" << moduleName.get() << "'";
+
+  Try<VolumeProfileAdaptor*> result =
+    modules::ModuleManager::create<VolumeProfileAdaptor>(moduleName.get());
+
+  if (result.isError()) {
+    return Error(
+        "Failed to initialize volume profile adaptor module: "
+        + result.error());
+  }
+
+  return result;
+}
+
+} // namespace mesos {
