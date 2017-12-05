3b9db404cb0b50edef1958423b26364e63ccaa27
Michael Park <mpark@apache.org>
2017-12-04 17:25:33 -0800

    Added `cpp17::invoke` in <stout/cpp17.hpp>.
    Review: https://reviews.apache.org/r/64248/

---
+++ 3rdparty/stout/include/stout/cpp17.hpp
@@ -0,0 +1,65 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// limitations under the License.
+
+#ifndef __STOUT_CPP17_HPP__
+#define __STOUT_CPP17_HPP__
+
+#include <utility>
+
+// This file contains implementation of C++17 standard library features.
+// Once we adopt C++17, this file should be removed and usages of its
+// functionality should be replaced with the standard library equivalents
+// by replacing `cpp17` with `std` and including the appropriate headers.
+
+// Note that code in this file may not follow stout conventions strictly
+// as it uses names as defined in C++ standard.
+
+namespace cpp17 {
+
+// <functional>
+
+// `std::invoke`
+
+#ifdef __WINDOWS__
+using std::invoke;
+#else
+#define RETURN(...) -> decltype(__VA_ARGS__) { return __VA_ARGS__; }
+
+// NOTE: This implementation is not strictly conforming currently
+// as attempting to use it with `std::reference_wrapper` will result
+// in compilation failure.
+
+template <typename F, typename... As>
+constexpr auto invoke(F&& f, As&&... as)
+  RETURN(std::forward<F>(f)(std::forward<As>(as)...))
+
+template <typename B, typename T, typename D>
+constexpr auto invoke(T B::*pmv, D&& d)
+  RETURN(std::forward<D>(d).*pmv)
+
+template <typename Pmv, typename Ptr>
+constexpr auto invoke(Pmv pmv, Ptr&& ptr)
+  RETURN((*std::forward<Ptr>(ptr)).*pmv)
+
+template <typename B, typename T, typename D, typename... As>
+constexpr auto invoke(T B::*pmf, D&& d, As&&... as)
+  RETURN((std::forward<D>(d).*pmf)(std::forward<As>(as)...))
+
+template <typename Pmf, typename Ptr, typename... As>
+constexpr auto invoke(Pmf pmf, Ptr&& ptr, As&&... as)
+  RETURN(((*std::forward<Ptr>(ptr)).*pmf)(std::forward<As>(as)...))
+
+#undef RETURN
+#endif
+
+} // namespace cpp17 {
+
+#endif // __STOUT_CPP17_HPP__
---
+++ 3rdparty/stout/tests/cpp17_tests.cpp
@@ -0,0 +1,58 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+
+#include <gtest/gtest.h>
+
+#include <stout/cpp17.hpp>
+
+int f(int i) { return i + 101; }
+
+TEST(Invoke, Free)
+{
+  // Invoke a free function.
+  EXPECT_EQ(202, cpp17::invoke(f, 101));
+}
+
+
+TEST(Invoke, Lambda)
+{
+  // Invoke a lambda.
+  EXPECT_EQ(42, cpp17::invoke([]() { return 42; }));
+}
+
+
+TEST(Invoke, Member) {
+  // Invoke a member function.
+  struct S
+  {
+    int f(int i) const { return n + i; }
+    int n;
+  };
+
+  S s{101};
+  EXPECT_EQ(303, cpp17::invoke(&S::f, s, 202));
+
+  // Invoke (access) a data member.
+  EXPECT_EQ(101, cpp17::invoke(&S::n, s));
+}
+
+
+TEST(Invoke, FunctionObject)
+{
+  // Invoke a function object.
+  struct F
+  {
+    int operator()(int i) const { return i + 202; }
+  };
+
+  EXPECT_EQ(404, cpp17::invoke(F{}, 202));
+}
