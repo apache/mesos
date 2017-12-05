ececa115966067551e00098f973f0104a0782730
Michael Park <mpark@apache.org>
2017-12-05 09:57:51 -0800

    Removed `constexpr`-ness from `cpp17::invoke`.
    `std::invoke` is not marked `constexpr`.
    
    Review: https://reviews.apache.org/r/64332/

--- 3rdparty/stout/include/stout/cpp17.hpp
+++ 3rdparty/stout/include/stout/cpp17.hpp
@@ -38,23 +38,23 @@ using std::invoke;
 // in compilation failure.
 
 template <typename F, typename... As>
-constexpr auto invoke(F&& f, As&&... as)
+auto invoke(F&& f, As&&... as)
   RETURN(std::forward<F>(f)(std::forward<As>(as)...))
 
 template <typename B, typename T, typename D>
-constexpr auto invoke(T B::*pmv, D&& d)
+auto invoke(T B::*pmv, D&& d)
   RETURN(std::forward<D>(d).*pmv)
 
 template <typename Pmv, typename Ptr>
-constexpr auto invoke(Pmv pmv, Ptr&& ptr)
+auto invoke(Pmv pmv, Ptr&& ptr)
   RETURN((*std::forward<Ptr>(ptr)).*pmv)
 
 template <typename B, typename T, typename D, typename... As>
-constexpr auto invoke(T B::*pmf, D&& d, As&&... as)
+auto invoke(T B::*pmf, D&& d, As&&... as)
   RETURN((std::forward<D>(d).*pmf)(std::forward<As>(as)...))
 
 template <typename Pmf, typename Ptr, typename... As>
-constexpr auto invoke(Pmf pmf, Ptr&& ptr, As&&... as)
+auto invoke(Pmf pmf, Ptr&& ptr, As&&... as)
   RETURN(((*std::forward<Ptr>(ptr)).*pmf)(std::forward<As>(as)...))
 
 #undef RETURN
