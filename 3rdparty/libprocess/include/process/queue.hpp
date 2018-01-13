e48e00afd4dd4ef3cc5ab3f4d87b8ef695b193bf
Benjamin Mahler <bmahler@apache.org>
2018-01-13 13:31:33 -0800

    Added support for discarding a Future from Queue::get.
    Review: https://reviews.apache.org/r/65150

--- 3rdparty/libprocess/include/process/queue.hpp
+++ 3rdparty/libprocess/include/process/queue.hpp
@@ -31,6 +31,7 @@ class Queue
 public:
   Queue() : data(new Data()) {}
 
+  // TODO(bmahler): Take a T&& here instead.
   void put(const T& t)
   {
     // NOTE: We need to grab the promise 'date->promises.front()' but
@@ -54,16 +55,44 @@ public:
 
   Future<T> get()
   {
+    Future<T> future;
+
     synchronized (data->lock) {
       if (data->elements.empty()) {
-        data->promises.push_back(Owned<Promise<T>>(new Promise<T>()));
-        return data->promises.back()->future();
+        data->promises.emplace_back(new Promise<T>());
+        future = data->promises.back()->future();
       } else {
-        Future<T> future = Future<T>(data->elements.front());
+        T t = std::move(data->elements.front());
         data->elements.pop();
-        return future;
+        return Future<T>(std::move(t));
       }
     }
+
+    // If there were no items available, we set up a discard
+    // handler. This is done here to minimize the amount of
+    // work done within the critical section above.
+    auto weak_data = std::weak_ptr<Data>(data);
+
+    future.onDiscard([weak_data, future]() {
+      auto data = weak_data.lock();
+      if (!data) {
+        return;
+      }
+
+      synchronized (data->lock) {
+        for (auto it = data->promises.begin();
+             it != data->promises.end();
+             ++it) {
+          if ((*it)->future() == future) {
+            (*it)->discard();
+            data->promises.erase(it);
+            break;
+          }
+        }
+      }
+    });
+
+    return future;
   }
 
   size_t size() const
