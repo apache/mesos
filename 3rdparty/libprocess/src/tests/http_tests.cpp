a94a8a67900ceabdce5a83233e66938d6dd1917a
Alexander Rukletsov <alexr@apache.org>
2018-01-25 19:48:34 +0100

    Added explicit scopes for clarity in HTTPTest.Endpoints test.

--- 3rdparty/libprocess/src/tests/http_tests.cpp
+++ 3rdparty/libprocess/src/tests/http_tests.cpp
@@ -237,66 +237,73 @@ TEST_P(HTTPTest, Endpoints)
   Http http;
 
   // First hit '/body' (using explicit sockets and HTTP/1.0).
-  Try<inet::Socket> create = inet::Socket::create();
-  ASSERT_SOME(create);
+  {
+    Try<inet::Socket> create = inet::Socket::create();
+    ASSERT_SOME(create);
 
-  inet::Socket socket = create.get();
+    inet::Socket socket = create.get();
 
-  AWAIT_READY(socket.connect(http.process->self().address));
+    AWAIT_READY(socket.connect(http.process->self().address));
 
-  std::ostringstream out;
-  out << "GET /" << http.process->self().id << "/body"
-      << " HTTP/1.0\r\n"
-      << "Connection: Keep-Alive\r\n"
-      << "\r\n";
+    std::ostringstream out;
+    out << "GET /" << http.process->self().id << "/body"
+        << " HTTP/1.0\r\n"
+        << "Connection: Keep-Alive\r\n"
+        << "\r\n";
 
-  const string data = out.str();
+    const string data = out.str();
+    const string response = "HTTP/1.1 200 OK";
 
-  EXPECT_CALL(*http.process, body(_))
-    .WillOnce(Return(http::OK()));
+    EXPECT_CALL(*http.process, body(_))
+      .WillOnce(Return(http::OK()));
 
-  AWAIT_READY(socket.send(data));
-
-  string response = "HTTP/1.1 200 OK";
-
-  AWAIT_EXPECT_EQ(response, socket.recv(response.size()));
-
-  // Now hit '/pipe' (by using http::get).
-  http::Pipe pipe;
-  http::OK ok;
-  ok.type = http::Response::PIPE;
-  ok.reader = pipe.reader();
-
-  Future<Nothing> request;
-  EXPECT_CALL(*http.process, pipe(_))
-    .WillOnce(DoAll(FutureSatisfy(&request),
-                    Return(ok)));
-
-  Future<http::Response> future =
-    http::get(http.process->self(), "pipe", None(), None(), GetParam());
-
-  AWAIT_READY(request);
-
-  // Write the response.
-  http::Pipe::Writer writer = pipe.writer();
-  EXPECT_TRUE(writer.write("Hello World\n"));
-  EXPECT_TRUE(writer.close());
-
-  AWAIT_READY(future);
-  EXPECT_EQ(http::Status::OK, future->code);
-  EXPECT_EQ(http::Status::string(http::Status::OK), future->status);
-
-  EXPECT_SOME_EQ("chunked", future->headers.get("Transfer-Encoding"));
-  EXPECT_EQ("Hello World\n", future->body);
+    AWAIT_READY(socket.send(data));
+    AWAIT_EXPECT_EQ(response, socket.recv(response.size()));
+  }
 
   // Test that an endpoint handler failure results in a 500.
-  EXPECT_CALL(*http.process, body(_))
-    .WillOnce(Return(Future<http::Response>::failed("failure")));
+  {
+    EXPECT_CALL(*http.process, body(_))
+      .WillOnce(Return(Future<http::Response>::failed("failure")));
 
-  future = http::get(http.process->self(), "body", None(), None(), GetParam());
+    Future<http::Response> response =
+      http::get(http.process->self(), "body", None(), None(), GetParam());
 
-  AWAIT_ASSERT_RESPONSE_STATUS_EQ(http::InternalServerError().status, future);
-  EXPECT_EQ("failure", future->body);
+    AWAIT_ASSERT_RESPONSE_STATUS_EQ(
+        http::InternalServerError().status,
+        response);
+    EXPECT_EQ("failure", response->body);
+  }
+
+  // Now hit '/pipe' (by using http::get).
+  {
+    http::Pipe pipe;
+    http::OK ok;
+    ok.type = http::Response::PIPE;
+    ok.reader = pipe.reader();
+
+    Future<Nothing> request;
+    EXPECT_CALL(*http.process, pipe(_))
+      .WillOnce(DoAll(FutureSatisfy(&request),
+                      Return(ok)));
+
+    Future<http::Response> future =
+      http::get(http.process->self(), "pipe", None(), None(), GetParam());
+
+    AWAIT_READY(request);
+
+    // Write the response.
+    http::Pipe::Writer writer = pipe.writer();
+    EXPECT_TRUE(writer.write("Hello World\n"));
+    EXPECT_TRUE(writer.close());
+
+    AWAIT_READY(future);
+    EXPECT_EQ(http::Status::OK, future->code);
+    EXPECT_EQ(http::Status::string(http::Status::OK), future->status);
+
+    EXPECT_SOME_EQ("chunked", future->headers.get("Transfer-Encoding"));
+    EXPECT_EQ("Hello World\n", future->body);
+  }
 }
 
 
