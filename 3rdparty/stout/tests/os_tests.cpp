81349a12381cdcd6ba94d1531ff0da263b1c2d1c
Michael Park <mpark@apache.org>
2018-01-29 11:44:33 -0800

    Revert "Replaced the `Os.Which` test to use `cat` rather than `ping`."
    Turns out Windows does not have `cat` installed by default.
    Considering we alraedy shell out to the `ping` command in
    `src/tests/containerizer/capabilities_test_helper.cpp`,
    we'll just pick up `ping` as a dependency for now.
    
    This reverts commit 981767895f1770701efeca3b8efd5c66cd34d8fc.

--- 3rdparty/stout/tests/os_tests.cpp
+++ 3rdparty/stout/tests/os_tests.cpp
@@ -1039,7 +1039,7 @@ TEST_F(OsTest, SYMLINK_Realpath)
 TEST_F(OsTest, Which)
 {
   // TODO(jieyu): Test PATH search ordering and file execution bit.
-  Option<string> which = os::which("cat");
+  Option<string> which = os::which("ping");
   ASSERT_SOME(which);
 
   which = os::which("bar");
