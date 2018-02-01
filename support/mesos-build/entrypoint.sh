7760e6e235ccaa9cb0e8cc05f3c5277b42e80e56
Michael Park <mpark@apache.org>
2018-02-01 11:14:28 -0800

    Introduced `mesos-build` along with pre-built docker images.
    The layout of `mesos-build` is similar to `mesos-tidy`.
    
    The `mesos-build.sh` will be the user-facing driver, and `buildbot.sh`
    invokes `mesos-build.sh` along with the clean-up (`docker rmi`) phase.
    The `mesos-build` directory contains docker files and a common
    `entrypoint.sh`. The docker images are built with all of the
    dependencies required, and the various testing configurations are set
    within `entrypoint.sh`.
    
    Review: https://reviews.apache.org/r/65464

---
+++ support/mesos-build/entrypoint.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+set -o pipefail
+
+SRCDIR=/tmp/SRC
+
+# Prepare sources
+git clone --depth 1 file:///SRC "${SRCDIR}"
+
+cd "${SRCDIR}"
+
+export GTEST_OUTPUT=xml:report.xml
+export DISTCHECK_CONFIGURE_FLAGS=${CONFIGURATION}
+export ${ENVIRONMENT}
+
+case ${COMPILER} in
+  gcc)
+    export CC=gcc
+    export CXX=g++
+    ;;
+  clang)
+    export CC=clang
+    export CXX=clang++
+    ;;
+  *)
+    echo "Unknown compiler ${COMPILER}"
+    exit 1
+    ;;
+esac
+
+case ${BUILDTOOL} in
+  autotools)
+    ./bootstrap
+    mkdir build && cd build
+    ../configure ${CONFIGURATION}
+    make -j "${JOBS}" distcheck 2>&1
+    ;;
+  cmake)
+    # Transform autotools-like parameters to cmake-like.
+    # Remove "'".
+    CONFIGURATION=${CONFIGURATION//\'/""}
+    # Replace "-" with "_".
+    CONFIGURATION=${CONFIGURATION//-/"_"}
+    # Replace "__" with "-D".
+    CONFIGURATION=${CONFIGURATION//__/"-D"}
+    # To Upper Case.
+    CONFIGURATION=${CONFIGURATION^^}
+
+    # Add "=1" suffix to each variable.
+    IFS=' ' read -r  -a array <<< "${CONFIGURATION}"
+
+    CONFIGURATION=""
+    for element in "${array[@]}"
+    do
+      CONFIGURATION="${CONFIGURATION} $element=1"
+    done
+
+    mkdir build && cd build
+    cmake ${CONFIGURATION} ..
+    make -j "${JOBS}" check 2>&1
+    ;;
+  *)
+    echo "Unknown build tool ${BUILDTOOL}"
+    exit 1
+    ;;
+esac
