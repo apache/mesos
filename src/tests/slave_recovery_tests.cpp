01bb8e72d6bfa43be96a7be07d272ae1f280e675
Meng Zhu <mzhu@mesosphere.io>
2018-02-08 17:19:48 -0800

    Fixed two slave recovery tests.
    These two tests are affected by the lingering executor fix
    (#65109). We should wait to make sure tasks are actually launched
    on the executor before trigger the agent failover. Otherwise, the
    container will be shutdown upon re-registration because the executor
    has never received any task.
    
    Review: https://reviews.apache.org/r/65572/

--- src/tests/slave_recovery_tests.cpp
+++ src/tests/slave_recovery_tests.cpp
@@ -5125,7 +5125,11 @@ TEST_F(MesosContainerizerSlaveRecoveryTest, CGROUPS_ROOT_PidNamespaceForward)
   // Scheduler expectations.
   EXPECT_CALL(sched, registered(_, _, _));
 
+  Future<TaskStatus> statusStarting;
+  Future<TaskStatus> statusRunning;
   EXPECT_CALL(sched, statusUpdate(_, _))
+    .WillOnce(FutureArg<1>(&statusStarting))
+    .WillOnce(FutureArg<1>(&statusRunning))
     .WillRepeatedly(Return());
 
   Future<vector<Offer>> offers1;
@@ -5156,6 +5160,13 @@ TEST_F(MesosContainerizerSlaveRecoveryTest, CGROUPS_ROOT_PidNamespaceForward)
   ASSERT_EQ(1u, containers->size());
 
   ContainerID containerId = *(containers->begin());
+
+  // Wait until task is running.
+  AWAIT_READY(statusStarting);
+  EXPECT_EQ(TASK_STARTING, statusStarting->state());
+
+  AWAIT_READY(statusRunning);
+  EXPECT_EQ(TASK_RUNNING, statusRunning->state());
 
   // Stop the slave.
   slave.get()->terminate();
@@ -5229,7 +5240,11 @@ TEST_F(MesosContainerizerSlaveRecoveryTest, CGROUPS_ROOT_PidNamespaceBackward)
   // Scheduler expectations.
   EXPECT_CALL(sched, registered(_, _, _));
 
+  Future<TaskStatus> statusStarting;
+  Future<TaskStatus> statusRunning;
   EXPECT_CALL(sched, statusUpdate(_, _))
+    .WillOnce(FutureArg<1>(&statusStarting))
+    .WillOnce(FutureArg<1>(&statusRunning))
     .WillRepeatedly(Return());
 
   Future<vector<Offer>> offers1;
@@ -5260,6 +5275,13 @@ TEST_F(MesosContainerizerSlaveRecoveryTest, CGROUPS_ROOT_PidNamespaceBackward)
   ASSERT_EQ(1u, containers->size());
 
   ContainerID containerId = *(containers->begin());
+
+  // Wait until task is running.
+  AWAIT_READY(statusStarting);
+  EXPECT_EQ(TASK_STARTING, statusStarting->state());
+
+  AWAIT_READY(statusRunning);
+  EXPECT_EQ(TASK_RUNNING, statusRunning->state());
 
   // Stop the slave.
   slave.get()->terminate();
