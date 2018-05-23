07f5c00038d4ae22fbb72f8979f5f013b37780fd
Akash Gupta <akash-gupta@hotmail.com>
2018-05-23 14:01:49 -0700

    Windows: Fixed inheritance in `Subprocess::PIPE()`.
    `Subprocess::PIPE()` was creating inheritable pipes and files. On
    Windows, our policy is to make everything not inheritable and then
    have `CreateProcessW` set the the right pipes to be inheritable to
    avoid any handle leaks.
    
    Review: https://reviews.apache.org/r/66958/

--- 3rdparty/libprocess/src/subprocess_windows.cpp
+++ 3rdparty/libprocess/src/subprocess_windows.cpp
@@ -36,8 +36,6 @@
 #include <stout/os/pipe.hpp>
 #include <stout/os/strerror.hpp>
 
-#include <stout/internal/windows/inherit.hpp>
-
 using std::array;
 using std::string;
 
@@ -46,43 +44,30 @@ namespace process {
 using InputFileDescriptors = Subprocess::IO::InputFileDescriptors;
 using OutputFileDescriptors = Subprocess::IO::OutputFileDescriptors;
 
-// Opens an inheritable pipe[1] represented as a pair of file handles. On
-// success, the first handle returned receives the 'read' handle of the pipe,
-// while the second receives the 'write' handle. The pipe handles can then be
-// passed to a child process, as exemplified in [2].
+// Opens a pair of file handles. On success, the first handle returned receives
+// the 'read' handle of the pipe, while the second receives the 'write' handle.
+// The pipe handles can then be passed to a child process, as shown in [1].
 //
-// [1] https://msdn.microsoft.com/en-us/library/windows/desktop/aa379560(v=vs.85).aspx
-// [2] https://msdn.microsoft.com/en-us/library/windows/desktop/ms682499(v=vs.85).aspx
+// [1] https://msdn.microsoft.com/en-us/library/windows/desktop/ms682499(v=vs.85).aspx
 Subprocess::IO Subprocess::PIPE()
 {
   return Subprocess::IO(
       []() -> Try<InputFileDescriptors> {
-        const Try<array<int_fd, 2>> pipefd = os::pipe();
+        // Create STDIN pipe and set the 'read' component to be not overlapped,
+        // because we're sending it to the child process.
+        const Try<array<int_fd, 2>> pipefd = os::pipe(false, true);
         if (pipefd.isError()) {
           return Error(pipefd.error());
-        }
-
-        // Create STDIN pipe and set the 'write' component to not be
-        // inheritable.
-        const Try<Nothing> inherit =
-          ::internal::windows::set_inherit(pipefd.get()[1], false);
-        if (inherit.isError()) {
-          return Error(inherit.error());
         }
 
         return InputFileDescriptors{pipefd.get()[0], pipefd.get()[1]};
       },
       []() -> Try<OutputFileDescriptors> {
-        const Try<array<int_fd, 2>> pipefd = os::pipe();
+        // Create STDOUT pipe and set the 'write' component to be not
+        // overlapped, because we're sending it to the child process.
+        const Try<array<int_fd, 2>> pipefd = os::pipe(true, false);
         if (pipefd.isError()) {
           return Error(pipefd.error());
-        }
-
-        // Create OUT pipe and set the 'read' component to not be inheritable.
-        const Try<Nothing> inherit =
-          ::internal::windows::set_inherit(pipefd.get()[0], false);
-        if (inherit.isError()) {
-          return Error(inherit.error());
         }
 
         return OutputFileDescriptors{pipefd.get()[0], pipefd.get()[1]};
@@ -100,12 +85,6 @@ Subprocess::IO Subprocess::PATH(const string& path)
           return Error(open.error());
         }
 
-        const Try<Nothing> inherit =
-          ::internal::windows::set_inherit(open.get(), true);
-        if (inherit.isError()) {
-          return Error(inherit.error());
-        }
-
         return InputFileDescriptors{open.get(), None()};
       },
       [path]() -> Try<OutputFileDescriptors> {
@@ -113,12 +92,6 @@ Subprocess::IO Subprocess::PATH(const string& path)
 
         if (open.isError()) {
           return Error(open.error());
-        }
-
-        const Try<Nothing> inherit =
-          ::internal::windows::set_inherit(open.get(), true);
-        if (inherit.isError()) {
-          return Error(inherit.error());
         }
 
         return OutputFileDescriptors{None(), open.get()};
