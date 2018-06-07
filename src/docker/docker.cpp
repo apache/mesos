42f21f38fe700d5b7d47c7e5969d215b14360bb3
Kjetil Joergensen <kjetijor@gmail.com>
2018-06-07 13:13:37 -0700

    Used /dev/null in Docker::__pull to avoid deadlock.
    Avoids deadlock between docker pull and mesos-slave when output from
    docker pull > pipe buffer allocated by the kernel.
    
    Review: https://reviews.apache.org/r/67489/
    (cherry picked from commit c855fbb4accf675c35b46a86383c9da153c0ce32)

--- src/docker/docker.cpp
+++ src/docker/docker.cpp
@@ -1735,7 +1735,7 @@ Future<Docker::Image> Docker::__pull(
       path,
       argv,
       Subprocess::PATH(os::DEV_NULL),
-      Subprocess::PIPE(),
+      Subprocess::PATH(os::DEV_NULL),
       Subprocess::PIPE(),
       nullptr,
       environment);
