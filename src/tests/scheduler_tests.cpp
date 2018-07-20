9af920c75d1fcd001be86c419a418b93d5bf0cf0
James Peach <jpeach@apache.org>
2018-07-19 21:21:43 -0700

    Switched the virtual keyword to `override`.

--- src/tests/scheduler_tests.cpp
+++ src/tests/scheduler_tests.cpp
@@ -2263,7 +2263,7 @@ class SchedulerSSLTest
 // These test setup/teardown methods are only needed when compiled with SSL.
 #ifdef USE_SSL_SOCKET
 protected:
-  virtual void SetUp()
+  void SetUp() override
   {
     MesosTest::SetUp();
 
