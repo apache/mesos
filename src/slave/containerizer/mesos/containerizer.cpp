03fe69797849335aa25e7e22b5c34f3fe622e8f5
James Peach <jpeach@apache.org>
2018-08-17 11:45:56 -0700

    Made the containerizer launch be explicit about O_CLOEXEC.
    Since the containerizer launch depends on the inherited pipe to signal
    the forked child, be explicit about manipulating O_CLOEXEC on the pipe
    file descriptors. Make sure to close the pipe on the error paths.
    
    Review: https://reviews.apache.org/r/63280/
    (cherry picked from commit 7569ffd05903be9b5284100f67eeffb35fcc7703)

--- src/slave/containerizer/mesos/containerizer.cpp
+++ src/slave/containerizer/mesos/containerizer.cpp
@@ -1981,6 +1981,9 @@ Future<Containerizer::LaunchResult> MesosContainerizerProcess::_launch(
       whitelistFds);
 
   if (forked.isError()) {
+    os::close(pipes[0]);
+    os::close(pipes[1]);
+
     return Failure("Failed to fork: " + forked.error());
   }
 
@@ -1999,6 +2002,9 @@ Future<Containerizer::LaunchResult> MesosContainerizerProcess::_launch(
     if (checkpointed.isError()) {
       LOG(ERROR) << "Failed to checkpoint container's forked pid to '"
                  << pidCheckpointPath.get() << "': " << checkpointed.error();
+
+      os::close(pipes[0]);
+      os::close(pipes[1]);
 
       return Failure("Could not checkpoint container's pid");
     }
@@ -2023,6 +2029,9 @@ Future<Containerizer::LaunchResult> MesosContainerizerProcess::_launch(
   checkpointed = slave::state::checkpoint(pidPath, stringify(pid));
 
   if (checkpointed.isError()) {
+    os::close(pipes[0]);
+    os::close(pipes[1]);
+
     return Failure("Failed to checkpoint the container pid to"
                    " '" + pidPath + "': " + checkpointed.error());
   }
