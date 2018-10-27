f267be6abf3747d609969f1bd955be3e5e1af2f1
Jie Yu <yujie.jay@gmail.com>
2018-10-27 11:47:06 -0700

    Updated Mesos-mini scripts.
    This patch fixed some issues in the Mesos-mini build scripts and adapted
    to the new centos Docker build workflow.
    
    (cherry picked from commit 20ed47b839b2713912b02770a381a9e000b52ea8)

--- support/mesos-mini/build.sh
+++ support/mesos-mini/build.sh
@@ -8,20 +8,19 @@ set -o errexit -o nounset -o pipefail -o verbose
 CURRENT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 SUPPORT_DIR="${CURRENT_DIR}/.."
 
-DOCKER_IMAGE_MINI=${DOCKER_IMAGE_MINI:-"mesos/mesos-mini"}
-DOCKER_IMAGE_PACKAGING=${DOCKER_IMAGE_PACKAGING:-"mesos/mesos-centos-packaging"}
-DOCKER_IMAGE_DISTRO=${DOCKER_IMAGE_DISTRO:-"mesos/mesos-centos"}
+DOCKER_IMAGE=${DOCKER_IMAGE_MINI:-"mesos/mesos-mini"}
 DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG:-"latest"}
+DOCKER_IMAGE_DISTRO=${DOCKER_IMAGE_DISTRO:-"mesos/mesos-centos"}
 
 if ! [ -x "$(command -v docker)" ]; then
   echo 'Error: docker is not installed.' >&2
   exit 1
 fi
 
-DOCKER_IMAGE_PACKAGING=${DOCKER_IMAGE_PACKAGING} \
-DOCKER_IMAGE_DISTRO=${DOCKER_IMAGE_DISTRO} \
-DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} \
-"${SUPPORT_DIR}/packaging/centos/build-docker-image.sh"
+DOCKER_IMAGE=${DOCKER_IMAGE_DISTRO} \
+"${SUPPORT_DIR}/packaging/centos/build-docker-centos.sh"
 
-docker tag ${DOCKER_IMAGE_DISTRO}:${DOCKER_IMAGE_TAG} "mesos/mesos-centos"
-docker build -t ${DOCKER_IMAGE_MINI}:${DOCKER_IMAGE_TAG} "${CURRENT_DIR}/"
+docker build \
+  --build-arg "BASE_IMAGE_NAME=${DOCKER_IMAGE_DISTRO}" \
+  -t "${DOCKER_IMAGE}:${DOCKER_IMAGE_TAG}" \
+  "${CURRENT_DIR}/"
--- support/mesos-mini/docker_env.sh
+++ support/mesos-mini/docker_env.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset -o pipefail -o verbose
 
-CGROUP=`grep memory /proc/1/cgroup | cut -d: -f3`
+CGROUP=$(grep memory /proc/1/cgroup | cut -d: -f3)
 
 cat <<EOF > "/etc/docker/env"
 CGROUP_PARENT=${CGROUP}/docker
