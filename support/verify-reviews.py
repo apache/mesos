3badf7179992e61f30f5a79da9d481dd451c7c2f
Armand Grillet <agrillet@mesosphere.io>
2018-12-18 15:47:56 +0100

    Simplified verify-reviews.py to be more similar to the Python 2 script.
    We have switched the Python 2 support scripts to Python 3 a few months
    ago. During this transition, 'verify-reviews.py' has been modified,
    mainly to work better on Windows: https://reviews.apache.org/r/67505/.
    
    The Python 3 script has then replaced the Python 2 one and we now face
    an issue with the script, MESOS-9459. Due to all the changes made
    between the Python 2 and Python 3 scripts, it is hard for us to
    know when the script started failing and why.
    
    To solve this issue, this commit takes the state of 'verify-reviews.py'
    as it was in 74121798f24fca372180b8c4bc00b4df07d46240, applies 2to3,
    adds the '--out-file' option, then adds the content of:
    * ac766c8bd0fba348399d8eac52f75f62e64db64e
    * 43eba285cb5c8622a6be8a5bdad637ed53431e85
    * 5b348b6070f0d0403cb69b6a7fa638fc46b7ff49
    * 568fcdfd29788d9df89a51ffae7969c2bf0ea173
    
    This gives a clean diff between 74121798f24fca372180b8c4bc00b4df07d46240
    and this commit, fixes the issue raised in MESOS-9459, and does not
    remove existing features. However, this uses less our common library
    developed in the chain that landed while migrating from Python 2 to 3.
    
    This should be followed by a refactoring of 'verify-review.py' and
    'common.py' to use our library more while not breaking the script
    when using Reviewbot.
    
    Review: https://reviews.apache.org/r/69559/

--- support/verify-reviews.py
+++ support/verify-reviews.py
@@ -34,8 +34,7 @@ The script performs the following sequence:
 """
 
 import argparse
-import time
-import datetime
+import atexit
 import json
 import os
 import platform
@@ -45,50 +44,85 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
-from common import ReviewBoardHandler, ReviewError, REVIEWBOARD_URL
+from datetime import datetime
+
+from common import REVIEWBOARD_URL
 
 REVIEW_SIZE = 1000000  # 1 MB in bytes.
-# This is the mesos repo ID obtained from querying the reviews.apache.org API
-MESOS_REPOSITORY_ID = 122
+
+# Parse arguments.
+parser = argparse.ArgumentParser(
+    description="Reviews that need verification from the Review Board")
+parser.add_argument("-u", "--user", type=str, required=True,
+                    help="Review Board user name")
+parser.add_argument("-p", "--password", type=str, required=True,
+                    help="Review Board user password")
+parser.add_argument("-r", "--reviews", type=int, required=False,
+                    default=-1, help="The number of reviews to fetch,"
+                                     " that will need verification")
+
+# Unless otherwise specified consider pending review requests to Mesos updated
+# since 03/01/2014.
+group = "mesos"
+last_updated = "2014-03-01T00:00:00"
+query_parameters = "?to-groups=%s&status=pending&last-updated-from=%s" \
+    % (group, last_updated)
+parser.add_argument("-q", "--query", type=str, required=False,
+                    help="Query parameters", default=query_parameters)
+
+parser.add_argument("-o", "--out-file", type=str, required=False,
+                    help="The out file with the reviews IDs that"
+                         " need verification")
+parser.add_argument("--skip-verify", action='store_true', required=False,
+                    help="Skip the verification and just write the review"
+                         " ids that need verification")
+
+parameters = parser.parse_args()
+USER = parameters.user
+PASSWORD = parameters.password
+NUM_REVIEWS = parameters.reviews
+QUERY_PARAMS = parameters.query
+OUT_FILE = parameters.out_file
+SKIP_VERIFY = parameters.skip_verify
 
 
-def parse_parameters():
-    """Method to parse the arguments for argparse."""
-    parser = argparse.ArgumentParser(
-        description="Reviews that need verification from the Review Board")
-    parser.add_argument("-u", "--user", type=str, required=True,
-                        help="Review Board user name")
-    parser.add_argument("-p", "--password", type=str, required=True,
-                        help="Review Board user password")
-    parser.add_argument("-r", "--reviews", type=int, required=False,
-                        default=-1, help="The number of reviews to fetch,"
-                                         " that will need verification")
-    parser.add_argument("--skip-verify", action='store_true', required=False,
-                        help="Skip the verification and just write the review"
-                             " ids that need verification")
-    default_hours_behind = 8
-    datetime_before = (datetime.datetime.now() -
-                       datetime.timedelta(hours=default_hours_behind))
-    datetime_before_string = datetime_before.isoformat()
-    default_query = {"status": "pending", "repository": MESOS_REPOSITORY_ID,
-                     "last-updated-from": datetime_before_string.split(".")[0]}
-    parser.add_argument("-q", "--query", type=str, required=False,
-                        help="Query parameters, passed as string in JSON"
-                             " format. Example: '%s'" % json.dumps(
-                                 default_query),
-                        default=json.dumps(default_query))
-    parser.add_argument("-o", "--out-file", type=str, required=False,
-                        help="The out file with the reviews IDs that"
-                             " need verification")
-
-    return parser.parse_args()
+class ReviewError(Exception):
+    """Exception returned by post_review()."""
+    pass
 
 
 def shell(command):
     """Run a shell command."""
     print(command)
-    return subprocess.check_output(
-        command, stderr=subprocess.STDOUT, shell=True)
+    out = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    return out.decode(sys.stdout.encoding)
+
+
+HEAD = shell("git rev-parse HEAD")
+
+
+def api(url, data=None):
+    """Call the ReviewBoard API."""
+    try:
+        auth_handler = urllib.request.HTTPBasicAuthHandler()
+        auth_handler.add_password(
+            realm="Web API",
+            uri="reviews.apache.org",
+            user=USER,
+            passwd=PASSWORD)
+
+        opener = urllib.request.build_opener(auth_handler)
+        urllib.request.install_opener(opener)
+
+        if isinstance(data, str):
+            data = str.encode(data)
+        return json.loads(urllib.request.urlopen(url, data=data).read())
+    except urllib.error.HTTPError as err:
+        print("Error handling URL %s: %s (%s)" % (url, err.reason, err.read()))
+        exit(1)
+    except urllib.error.URLError as err:
+        print("Error handling URL %s: %s" % (url, err.reason))
+        exit(1)
 
 
 def apply_review(review_id):
@@ -97,7 +131,7 @@ def apply_review(review_id):
     shell("%s support/apply-reviews.py -n -r %s" % (sys.executable, review_id))
 
 
-def apply_reviews(review_request, reviews, handler):
+def apply_reviews(review_request, reviews):
     """Apply multiple reviews at once."""
     # If there are no reviewers specified throw an error.
     if not review_request["target_people"]:
@@ -115,37 +149,34 @@ def apply_reviews(review_request, reviews, handler):
     # First recursively apply the dependent reviews.
     for review in review_request["depends_on"]:
         review_url = review["href"]
-        print("Dependent review: %s" % review_url)
-        apply_reviews(handler.api(review_url)["review_request"],
-                      reviews, handler)
+        print("Dependent review: %s " % review_url)
+        apply_reviews(api(review_url)["review_request"], reviews)
 
     # Now apply this review if not yet submitted.
     if review_request["status"] != "submitted":
         apply_review(review_request["id"])
 
 
-def post_review(review_request, message, handler):
+def post_review(review_request, message):
     """Post a review on the review board."""
     print("Posting review: %s" % message)
 
     review_url = review_request["links"]["reviews"]["href"]
     data = urllib.parse.urlencode({'body_top': message, 'public': 'true'})
-    handler.api(review_url, data)
+    api(review_url, data)
 
 
-# @atexit.register
+@atexit.register
 def cleanup():
     """Clean the git repository."""
     try:
         shell("git clean -fd")
-        HEAD = shell("git rev-parse HEAD")
-        print(HEAD)
-        shell("git checkout HEAD -- %s" % HEAD)
+        shell("git reset --hard %s" % HEAD)
     except subprocess.CalledProcessError as err:
         print("Failed command: %s\n\nError: %s" % (err.cmd, err.output))
 
 
-def verify_review(review_request, handler):
+def verify_review(review_request):
     """Verify a review."""
     print("Verifying review %s" % review_request["id"])
     build_output = "build_" + str(review_request["id"])
@@ -153,10 +184,11 @@ def verify_review(review_request, handler):
     try:
         # Recursively apply the review and its dependents.
         reviews = []
-        apply_reviews(review_request, reviews, handler)
+        apply_reviews(review_request, reviews)
 
         reviews.reverse()  # Reviews are applied in the reverse order.
 
+        command = ""
         if platform.system() == 'Windows':
             command = "support\\windows-build.bat"
 
@@ -182,34 +214,27 @@ def verify_review(review_request, handler):
             # output. `pipefail` ensures that the exit status of the build
             # command ispreserved even after tee'ing.
             subprocess.check_call(['bash', '-c',
-                                   'set -o pipefail; %s 2>&1 | tee %s'
+                                   ('set -o pipefail; %s 2>&1 | tee %s')
                                    % (command, build_output)])
 
         # Success!
         post_review(
             review_request,
-            "Patch looks great!\n\n"
-            "Reviews applied: %s\n\n"
-            "Passed command: %s" % (reviews, command), handler)
+            "Patch looks great!\n\n" \
+            "Reviews applied: %s\n\n" \
+            "Passed command: %s" % (reviews, command))
     except subprocess.CalledProcessError as err:
         # If we are here because the docker build command failed, read the
         # output from `build_output` file. For all other command failures read
         # the output from `e.output`.
-        #
-        # Decode the RHS so that `output` is always a string.
         if os.path.exists(build_output):
-            output = open(build_output).read().decode(sys.stdout.encoding)
+            output = open(build_output).read()
         else:
             output = err.output.decode(sys.stdout.encoding)
 
         if platform.system() == 'Windows':
             # We didn't output anything during the build (because `tee`
             # doesn't exist), so we print the output to stdout upon error.
-
-            # Pylint raises a no-member error on that line due to a bug
-            # fixed in pylint 1.7.
-            # TODO(ArmandGrillet): Remove this once pylint updated to >= 1.7.
-            # pylint: disable=no-member
             sys.stdout.buffer.write(output.encode())
 
         # Truncate the output when posting the review as it can be very large.
@@ -224,24 +249,73 @@ def verify_review(review_request, handler):
             "Bad patch!\n\n" \
             "Reviews applied: %s\n\n" \
             "Failed command: %s\n\n" \
-            "Error:\n%s" % (reviews, err.cmd, output), handler)
+            "Error:\n%s" % (reviews, err.cmd, output))
     except ReviewError as err:
         post_review(
             review_request,
             "Bad review!\n\n" \
             "Reviews applied: %s\n\n" \
-            "Error:\n%s" % (reviews, err.args[0]), handler)
+            "Error:\n%s" % (reviews, err.args[0]))
 
     # Clean up.
-    # cleanup()
+    cleanup()
 
 
-def verification_needed_write(review_ids, parameters):
+def needs_verification(review_request):
+    """Return True if this review request needs to be verified."""
+    print("Checking if review: %s needs verification" % review_request["id"])
+
+    # Skip if the review blocks another review.
+    if review_request["blocks"]:
+        print("Skipping blocking review %s" % review_request["id"])
+        return False
+
+    diffs_url = review_request["links"]["diffs"]["href"]
+    diffs = api(diffs_url)
+
+    if "diffs" not in diffs:  # No diffs attached!
+        print("Skipping review %s as it has no diffs" % review_request["id"])
+        return False
+
+    # Get the timestamp of the latest diff.
+    timestamp = diffs["diffs"][-1]["timestamp"]
+    rb_date_format = "%Y-%m-%dT%H:%M:%SZ"
+    diff_time = datetime.strptime(timestamp, rb_date_format)
+    print("Latest diff timestamp: %s" % diff_time)
+
+    # Get the timestamp of the latest review from this script.
+    reviews_url = review_request["links"]["reviews"]["href"]
+    reviews = api(reviews_url + "?max-results=200")
+    review_time = None
+    for review in reversed(reviews["reviews"]):
+        if review["links"]["user"]["title"] == USER:
+            timestamp = review["timestamp"]
+            review_time = datetime.strptime(timestamp, rb_date_format)
+            print("Latest review timestamp: %s" % review_time)
+            break
+
+    # TODO: Apply this check recursively up the dependency chain.
+    changes_url = review_request["links"]["changes"]["href"]
+    changes = api(changes_url)
+    dependency_time = None
+    for change in changes["changes"]:
+        if "depends_on" in change["fields_changed"]:
+            timestamp = change["timestamp"]
+            dependency_time = datetime.strptime(timestamp, rb_date_format)
+            print("Latest dependency change timestamp: %s" % dependency_time)
+            break
+
+    # Needs verification if there is a new diff, or if the dependencies changed,
+    # after the last time it was verified.
+    return not review_time or review_time < diff_time or \
+        (dependency_time and review_time < dependency_time)
+
+
+def write_review_ids(review_ids):
     """Write the IDs of the review requests that need verification."""
-    num_reviews = len(review_ids)
-    print("%s review requests need verification" % num_reviews)
-    if parameters.out_file is not None:
-        with open(parameters.out_file, 'w') as f:
+    print("%s review requests need verification" % len(review_ids))
+    if OUT_FILE is not None:
+        with open(OUT_FILE, 'w') as f:
             f.write('\n'.join(review_ids))
     else:
         print('\n'.join(review_ids))
@@ -249,46 +323,19 @@ def verification_needed_write(review_ids, parameters):
 
 def main():
     """Main function to verify the submitted reviews."""
-    parameters = parse_parameters()
-    print("\n%s - Running %s" % (time.strftime('%m-%d-%y_%T'),
-                                 os.path.abspath(__file__)))
-    # The colon from timestamp gets encoded and we don't want it to be encoded.
-    # Replacing %3A with colon.
-    query_string = urllib.parse.urlencode(
-        json.loads(parameters.query)).replace("%3A", ":")
-    review_requests_url = "%s/api/review-requests/?%s" % (REVIEWBOARD_URL,
-                                                          query_string)
-    handler = ReviewBoardHandler(parameters.user, parameters.password)
-    num_reviews = 0
+    review_requests_url = \
+        "%s/api/review-requests/%s" % (REVIEWBOARD_URL, QUERY_PARAMS)
+
+    review_requests = api(review_requests_url)
     review_ids = []
-    review_requests = handler.api(review_requests_url)
     for review_request in reversed(review_requests["review_requests"]):
-        if parameters.reviews == -1 or num_reviews < parameters.reviews:
-            try:
-                needs_verification = handler.needs_verification(review_request)
-                if not needs_verification:
-                    continue
-                # An exception is raised if cyclic dependencies are found
-                handler.get_dependent_review_ids(review_request)
-            except ReviewError as err:
-                message = ("Bad review!\n\n"
-                           "Error:\n%s" % (err.args[0]))
-                handler.post_review(review_request, message, handler)
-                continue
-            except Exception as err:
-                print("Error occured: %s" % err)
-                needs_verification = False
-                print("WARNING: Cannot find if review %s needs"
-                      " verification" % (review_request["id"]))
-            if not needs_verification:
-                continue
+        if (NUM_REVIEWS == -1 or len(review_ids) < NUM_REVIEWS) and \
+           needs_verification(review_request):
+            if not SKIP_VERIFY:
+                verify_review(review_request)
             review_ids.append(str(review_request["id"]))
-            num_reviews += 1
-            if not parameters.skip_verify:
-                verify_review(review_request, handler)
 
-    verification_needed_write(review_ids, parameters)
-
+    write_review_ids(review_ids)
 
 if __name__ == '__main__':
     main()
