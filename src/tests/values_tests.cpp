c27ad6de27afb0bb2badca5968d9d2c3dc5191b0
Meng Zhu <mzhu@mesosphere.io>
2019-01-05 13:28:27 -0800

    Disallowed nan, inf and so on when parsing Value::Scalar.
    Scalar values are intended to be finite numbers. This
    patch checks `nan`, `inf` and so on when parsing
    `Value::Scalar`. Only normal or zero numbers (as defined
    in `std::fpclassify()`) are allowed.
    
    Also added related tests.
    
    Review: https://reviews.apache.org/r/69673

--- src/tests/values_tests.cpp
+++ src/tests/values_tests.cpp
@@ -87,6 +87,15 @@ TEST(ValuesTest, InvalidInput)
 
   // Test when giving empty string.
   EXPECT_ERROR(parse("  "));
+
+  EXPECT_ERROR(parse("nan"));
+  EXPECT_ERROR(parse("-nan"));
+
+  EXPECT_ERROR(parse("inf"));
+  EXPECT_ERROR(parse("-inf"));
+
+  EXPECT_ERROR(parse("infinity"));
+  EXPECT_ERROR(parse("-infinity"));
 }
 
 
