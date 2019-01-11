baac18160460771c49e56b54660ad91c06a36ad4
Jie Yu <yujie.jay@gmail.com>
2019-01-10 21:41:12 -0800

    Fixed a bug in docker_containerizer_tests.cpp.
    Forgot to call MesosTest::SetUp() and MesosTest::TearDown() in the
    override methods.
    
    Review: https://reviews.apache.org/r/69714
    (cherry picked from commit 2bfd152e8ff1d42d5cff90b65cc9fdff52297800)

--- src/tests/containerizer/docker_containerizer_tests.cpp
+++ src/tests/containerizer/docker_containerizer_tests.cpp
@@ -190,6 +190,8 @@ public:
     foreach (const Docker::Container& container, containers.get()) {
       AWAIT_READY_FOR(docker.get()->rm(container.id, true), Seconds(30));
     }
+
+    MesosTest::TearDown();
   }
 };
 
