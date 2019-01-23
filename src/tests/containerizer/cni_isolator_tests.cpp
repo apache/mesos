cda45e593cca9811ca5726ed0b72ba76fb574a41
Gilbert Song <songzihao1990@gmail.com>
2019-01-22 17:13:15 -0800

    Fixed a test flakiness in ROOT_CleanupAfterReboot.
    Review: https://reviews.apache.org/r/69809
    (cherry picked from commit b54b493d6d6ae479d2154ed05f9615a11d662391)

--- src/tests/containerizer/cni_isolator_tests.cpp
+++ src/tests/containerizer/cni_isolator_tests.cpp
@@ -2146,7 +2146,8 @@ TEST_F(CniIsolatorTest, ROOT_CleanupAfterReboot)
   EXPECT_CALL(sched, statusUpdate(&driver, _))
     .WillOnce(FutureArg<1>(&statusStarting))
     .WillOnce(FutureArg<1>(&statusRunning))
-    .WillOnce(FutureArg<1>(&statusGone));
+    .WillOnce(FutureArg<1>(&statusGone))
+    .WillRepeatedly(Return()); // Ignore subsequent updates.
 
   driver.launchTasks(offer.id(), {task});
 
