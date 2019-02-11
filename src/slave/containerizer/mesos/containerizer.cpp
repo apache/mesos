5e4bb80a0fa4a74baebca63877536a433a9f3cce
Andrei Budnik <abudnik@mesosphere.com>
2019-02-11 12:51:28 -0800

    Enabled launcher sealing depending on corresponding compiler flag.
    This patch enables containerizer launcher sealing when
    `--enable-launcher-sealing` compiler flag is enabled. By default,
    this compiler flag is disabled, hence launcher sealing is disabled.
    
    Review: https://reviews.apache.org/r/69950/
    (cherry picked from commit d1bff48a677771f77dbcbdd55acfaeae2afa6b9a)

--- src/slave/containerizer/mesos/containerizer.cpp
+++ src/slave/containerizer/mesos/containerizer.cpp
@@ -49,9 +49,9 @@
 
 #include "hook/manager.hpp"
 
-#ifdef __linux__
+#ifdef ENABLE_LAUNCHER_SEALING
 #include "linux/memfd.hpp"
-#endif // __linux__
+#endif // ENABLE_LAUNCHER_SEALING
 
 #include "module/manager.hpp"
 
@@ -534,7 +534,7 @@ Try<MesosContainerizer*> MesosContainerizer::create(
 
   Option<int_fd> initMemFd;
 
-#ifdef __linux__
+#ifdef ENABLE_LAUNCHER_SEALING
   // Clone the launcher binary in memory for security concerns.
   Try<int_fd> memFd = memfd::cloneSealedFile(
       path::join(flags.launcher_dir, MESOS_CONTAINERIZER));
@@ -547,7 +547,7 @@ Try<MesosContainerizer*> MesosContainerizer::create(
   }
 
   initMemFd = memFd.get();
-#endif // __linux__
+#endif // ENABLE_LAUNCHER_SEALING
 
   return new MesosContainerizer(Owned<MesosContainerizerProcess>(
       new MesosContainerizerProcess(
