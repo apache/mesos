e4ac39b123c892ec2593381918600c260a75fc32
Jie Yu <yujie.jay@gmail.com>
2019-02-11 12:51:19 -0800

    Added Linux memfd support.
    This patch adds the memfd support to allow us to copy a regular file
    into a sealed memfd. This can be useful when one wants to protect a file
    from being overwritten.
    
    Review: https://reviews.apache.org/r/69943/

---
+++ src/linux/memfd.hpp
@@ -0,0 +1,39 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __LINUX_MEMFD_HPP__
+#define __LINUX_MEMFD_HPP__
+
+#include <string>
+
+#include <stout/try.hpp>
+
+#include <stout/os/int_fd.hpp>
+
+namespace mesos {
+namespace internal {
+namespace memfd {
+
+// Clone a file into a sealed private copy such that any attempt to
+// modify it will not modify the original binary. Returns the memfd of
+// the sealed copy.
+Try<int_fd> cloneSealedFile(const std::string& filePath);
+
+} // namespace memfd {
+} // namespace internal {
+} // namespace mesos {
+
+#endif // __LINUX_MEMFD_HPP__
