0c491b6fc79d5cafd4e070475d820ba0a4b3ebe7
Benjamin Mahler <bmahler@apache.org>
2019-02-12 18:30:33 -0500

    Fixed an allocator crash during reservation tracking.
    The allocator tracks reservation scalar quantities in a per-role
    map. The track and untrack functions are supposed to ensure that
    empty entries are never present, however the track function will
    insert an empty entry in the case of a reservation of non-scalar
    resources. The untrack function will expect an entry for the role
    when untracking, even if untracking a non-scalar reservation.
    
    So, the following situation leads to a crash:
    
    (1) track ports reservation1 for role: {role: {}}
    (2) track ports reservation2 for role: {role: {}}
    (3) untrack ports reservation1 for role: {}
    (4) untrack ports reservation2 for role: CHECK failure,
        untrackReservation expects an entry for role.
    
    This patch ensures we ignore reservations that are empty once
    stripped of non scalar quantities.
    
    Review: https://reviews.apache.org/r/69989

--- src/master/allocator/mesos/hierarchical.cpp
+++ src/master/allocator/mesos/hierarchical.cpp
@@ -2625,6 +2625,10 @@ void HierarchicalAllocatorProcess::trackReservations(
     const Resources scalarQuantitesToTrack =
       resources.createStrippedScalarQuantity();
 
+    if (scalarQuantitesToTrack.empty()) {
+      continue; // Do not insert an empty entry.
+    }
+
     reservationScalarQuantities[role] += scalarQuantitesToTrack;
   }
 }
@@ -2635,12 +2639,17 @@ void HierarchicalAllocatorProcess::untrackReservations(
 {
   foreachpair (const string& role,
                const Resources& resources, reservations) {
+    const Resources scalarQuantitesToUntrack =
+      resources.createStrippedScalarQuantity();
+
+    if (scalarQuantitesToUntrack.empty()) {
+      continue; // Do not CHECK for the role if there's nothing to untrack.
+    }
+
     CHECK(reservationScalarQuantities.contains(role));
     Resources& currentReservationQuantity =
         reservationScalarQuantities.at(role);
 
-    const Resources scalarQuantitesToUntrack =
-      resources.createStrippedScalarQuantity();
     CHECK(currentReservationQuantity.contains(scalarQuantitesToUntrack));
     currentReservationQuantity -= scalarQuantitesToUntrack;
 
