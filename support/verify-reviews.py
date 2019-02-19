apache/mesos

commit ccd1970d1be47306d15cffc68edadfe3926d265a

Fixed verify-reviews.py to not abort review verification prematurely.
Previously `support/verify-reviews.py` would abort prematurely
whenever a review a patch could not be applied. This was due to
the `shell` function used to call `support/apply-reviews.py` invoking
`exit(1)` on the first error which stopped iteration over all
outstanding reviews.

In this patch that explicit call to `exit` is removed, and instead we
let a possible `subprocess.CalledProcessError` propagate up for it to
be handled at a higher level where it is translated into a
`ReviewError`. Currently this will post a note on the review in
question to (1) notify the submitter, and (2) prevent the review from
being checked again.

With the changes here the script will not stop verify reviews in cases
where a review does not apply.

Review: https://reviews.apache.org/r/70000

support/verify-reviews.py
@@ -113,13 +113,8 @@ class ReviewError(Exception):
 
 def shell(command, working_dir=None):
     """Run a shell command."""
-    try:
-        out = subprocess.check_output(
-            command, stderr=subprocess.STDOUT, cwd=working_dir, shell=True)
-    except subprocess.CalledProcessError as err:
-        error = err.output.decode("utf-8")
-        print("Error running command '%s': %s" % (command, error))
-        exit(1)
+    out = subprocess.check_output(
+        command, stderr=subprocess.STDOUT, cwd=working_dir, shell=True)
     return out.decode(sys.stdout.encoding)
 
 
@@ -183,7 +178,16 @@ def apply_reviews(review_request, reviews):
 
     # Now apply this review if not yet submitted.
     if review_request["status"] != "submitted":
-        apply_review(review_request["id"])
+        # If the patch does not apply we translate the error to a
+        # `ReviewError`; otherwise we let the original exception
+        # propagate.
+        try:
+            apply_review(review_request["id"])
+        except subprocess.CalledProcessError as err:
+            if "patch does not apply" in err.output:
+                raise ReviewError(err.output.decode("utf-8"))
+            else:
+                raise err
 
 
 def post_review(review_request, message):

commit d5ff6c39252875bfa39344639518109e154e599e

Fixed type error in verify-reviews.py.
This patch fixes a type error introduced in `ccd1970d1be` where we
attempted to use a string API on undecoded UTF8 strings. With this
patch the string is properly decoded.

Review: https://reviews.apache.org/r/70012

support/verify-reviews.py
@@ -184,8 +184,9 @@ def apply_reviews(review_request, reviews):
         try:
             apply_review(review_request["id"])
         except subprocess.CalledProcessError as err:
-            if "patch does not apply" in err.output:
-                raise ReviewError(err.output.decode("utf-8"))
+            error = err.output.decode("utf-8")
+            if "patch does not apply" in error:
+                raise ReviewError(error)
             else:
                 raise err
 