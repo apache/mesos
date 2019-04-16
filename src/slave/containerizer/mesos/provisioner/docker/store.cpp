a6df0e2d8dd666cdda9ae4905a43a1850ff466bf
Gilbert Song <songzihao1990@gmail.com>
2019-04-15 21:47:10 -0700

    Supported docker manifest v2s2 config with image GC.
    After the docker manifest v2 schema2 support, the manifest config
    is saved as a layer in image store. However, the config file
    cannot survive if the image GC is triggered.
    
    This patch supports checkpointing the config path in provisioner,
    so that the provisioner has the information of configs so that
    they will not be deleted during GC.
    
    (cherry picked from commit cd75916020080232a27df4597f9de4dc2dc961d9)

--- src/slave/containerizer/mesos/provisioner/docker/store.cpp
+++ src/slave/containerizer/mesos/provisioner/docker/store.cpp
@@ -431,6 +431,10 @@ Future<ImageInfo> StoreProcess::__get(
         v1.error());
   }
 
+  if (image.has_config_digest()) {
+    return ImageInfo{layerPaths, v1.get(), None(), configPath};
+  }
+
   return ImageInfo{layerPaths, v1.get()};
 }
 
