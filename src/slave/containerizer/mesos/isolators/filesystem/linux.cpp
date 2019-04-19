beaae8df702e51102069b2b0502e924697ae36a2
Qian Zhang <zhq527725@gmail.com>
2019-04-19 17:22:45 +0800

    Made nested contaienr can access its sandbox via `MESOS_SANDBOX`.
    Previously in MESOS-8332 we narrowed task sandbox permissions from 0755
    to 0750 which will cause nested container may not has permission to
    access its sandbox via the environment variable `MESOS_SANDBOX`. Now in
    this patch, for nested container which does not have its own rootfs, we
    bind mount its sandbox to the directory specified via the agent flag
    `--sandbox_directory` and set `MESOS_SANDBOX` to `--sandbox_directory`
    as well, in this way such nested container will have the permission
    to access its sandbox via `MESOS_SANDBOX`.
    
    Review: https://reviews.apache.org/r/70514

--- src/slave/containerizer/mesos/isolators/filesystem/linux.cpp
+++ src/slave/containerizer/mesos/isolators/filesystem/linux.cpp
@@ -202,6 +202,16 @@ Try<Isolator*> LinuxFilesystemIsolatorProcess::create(const Flags& flags)
     }
   }
 
+  // Create sandbox directory. We will bind mount the sandbox of nested
+  // container which does not have its own rootfs to this directory. See
+  // `prepare` for details.
+  Try<Nothing> mkdir = os::mkdir(flags.sandbox_directory);
+  if (mkdir.isError()) {
+    return Error(
+        "Failed to create sandbox directory at '" +
+        flags.sandbox_directory + "': " + mkdir.error());
+  }
+
   Owned<MesosIsolatorProcess> process(
       new LinuxFilesystemIsolatorProcess(flags));
 
@@ -394,6 +404,21 @@ Future<Option<ContainerLaunchInfo>> LinuxFilesystemIsolatorProcess::prepare(
     ContainerMountInfo* mount = launchInfo.add_mounts();
     mount->set_source(containerConfig.directory());
     mount->set_target(sandbox);
+    mount->set_flags(MS_BIND | MS_REC);
+  } else if (containerId.has_parent()) {
+    // For nested container which does not have its own rootfs, bind mount its
+    // sandbox to the directory specified via `flags.sandbox_directory` (e.g.,
+    // `/mnt/mesos/sandbox`) in its own mount namespace and set the environment
+    // variable `MESOS_SANDBOX` to `flags.sandbox_directory` (see the `_launch`
+    // method of `MesosContainerizerProcess` for details). The reason that we do
+    // this is, in MESOS-8332 we narrowed task sandbox permissions from 0755 to
+    // 0750, since nested container's sandbox is subdirectory under its parent's
+    // sandbox, if we still set `MESOS_SANDBOX` to `containerConfig.directory()`
+    // for nested container, it will not have permission to access its sandbox
+    // via `MESOS_SANDBOX` if its user is different from its parent's user.
+    ContainerMountInfo* mount = launchInfo.add_mounts();
+    mount->set_source(containerConfig.directory());
+    mount->set_target(flags.sandbox_directory);
     mount->set_flags(MS_BIND | MS_REC);
   }
 
