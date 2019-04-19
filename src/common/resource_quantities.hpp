085b3826235dc07a065f8c65111e7caf0b0dd0c5
Greg Mann <gregorywmann@gmail.com>
2019-04-19 00:34:15 -0700

    Enabled construction of `ResourceQuantities` from `Resources`.
    This patch adds a new static method which enables the
    construction of `ResourceQuantities` from `Resources`.
    Namely, this permits the inclusion of sets and ranges in the
    input resources used to construct `ResourceQuantities`.
    
    (cherry-picked from commit cbae57b7e790b8b46c79052975406d603e7d175a)

--- src/common/resource_quantities.hpp
+++ src/common/resource_quantities.hpp
@@ -22,6 +22,7 @@
 #include <vector>
 
 #include <mesos/mesos.hpp>
+#include <mesos/resources.hpp>
 
 #include <stout/try.hpp>
 
@@ -74,6 +75,12 @@ public:
   // will be returned.
   static Try<ResourceQuantities> fromString(const std::string& text);
 
+  // Take `Resources` and combine them into `ResourceQuantities`. This function
+  // assumes that the provided resources have already been validated; for
+  // example, it assumes that ranges do not overlap and that sets do not contain
+  // duplicate items.
+  static ResourceQuantities fromResources(const Resources& resources);
+
   ResourceQuantities();
 
   ResourceQuantities(const ResourceQuantities& that) = default;
@@ -110,6 +117,7 @@ public:
 
 private:
   void add(const std::string& name, const Value::Scalar& scalar);
+  void add(const std::string& name, double value);
 
   // List of name quantity pairs sorted by name.
   // Arithmetic and comparison operations benefit from this sorting.
