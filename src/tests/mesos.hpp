13e0f23bd7cae6f5d0e3dc4cf81facb675561ab3
Greg Mann <gregorywmann@gmail.com>
2019-04-26 08:54:00 -0700

    Reverted several unintended cherry-pick commits.
    This patch reverts three commits unintentionally merged while
    cherry-picking the fix for MESOS-9619:
    
    * "Added unit test for a master validation helper function."
      4e10c57d9e523f5c74a9665721c48a6523be56fc
    
    * "Added tests for overlapping ranges and sets in task/executor ..."
      74a46a8c963821f25ec9898707368ba239843160
    
    * "Extended a v1 test helper."
      c4a504ee878a5915b989d21d0382d3a0dea4561b

--- src/tests/mesos.hpp
+++ src/tests/mesos.hpp
@@ -2070,8 +2070,7 @@ inline mesos::v1::Parameters parameterize(Args&&... args)
 inline mesos::v1::scheduler::Call createCallAccept(
     const mesos::v1::FrameworkID& frameworkId,
     const mesos::v1::Offer& offer,
-    const std::vector<mesos::v1::Offer::Operation>& operations,
-    const Option<mesos::v1::Filters>& filters = None())
+    const std::vector<mesos::v1::Offer::Operation>& operations)
 {
   mesos::v1::scheduler::Call call;
   call.set_type(mesos::v1::scheduler::Call::ACCEPT);
@@ -2082,10 +2081,6 @@ inline mesos::v1::scheduler::Call createCallAccept(
 
   foreach (const mesos::v1::Offer::Operation& operation, operations) {
     accept->add_operations()->CopyFrom(operation);
-  }
-
-  if (filters.isSome()) {
-    accept->mutable_filters()->CopyFrom(filters.get());
   }
 
   return call;
