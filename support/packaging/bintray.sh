35fa8420762f1e8cf8df211ddcdaa0cc77104bc2
Benno Evers <bevers@mesosphere.com>
2019-05-17 11:24:23 +0200

    Updated bintray upload scripts to remove hard-coded accounts.
    This updates the Jenkinsfile used in the ASF Jenkins as well
    as the associated upload script for bintray to make the
    used credential id configurable and to upload the built packages
    to the official `apache/mesos` bintray account.
    
    Review: https://reviews.apache.org/r/70573/

--- support/packaging/bintray.sh
+++ support/packaging/bintray.sh
@@ -4,34 +4,21 @@
 # The following environment variables must be set:
 #   BINTRAY_CREDS (In the form <username>:<api-key>)
 #   MESOS_TAG (1.4.0-rc1, 1.4.0, etc.)
-
-# The following default to "mesos" if not specified.
-#   BINTRAY_ORG
-#   BINTRAY_PKG
-
-
-# Sample yum repo file:
-# #bintray-mesos-el - packages by mesos from Bintray
-# [bintray-mesos-el]
-# name=bintray-mesos-el
-# baseurl=https://dl.bintray.com/mesos/el/7/x86_64
-# gpgcheck=0
-# repo_gpgcheck=0
-# enabled=1
 #
-# #bintray-mesos-el-srpm - source rpm packages by mesos from Bintray
-# [bintray-mesos-el-srpm]
-# name=bintray-mesos-el-srpm
-# baseurl=https://dl.bintray.com/mesos/el/7/SRPMS
-# gpgcheck=0
-# repo_gpgcheck=0
-# enabled=1
+# The following default to "apache" if not specified.
+#   BINTRAY_ORG
+#
+# The following default to "mesos" if not specified.
+#   BINTRAY_PKG
+#   BINTRAY_REPO
+
 
 set -o errexit -o nounset
 
 API=https://api.bintray.com
 
-BINTRAY_ORG=${BINTRAY_ORG:-mesos}
+BINTRAY_ORG=${BINTRAY_ORG:-apache}
+BINTRAY_REPO=${BINTRAY_REPO:-mesos}
 BINTRAY_PKG=${BINTRAY_PKG:-mesos}
 
 PKG_PATH=$1
@@ -44,18 +31,15 @@ case "$PKG_FILENAME" in
   *-rc*)       REPO_SUFFIX="-testing" ;;
 esac
 
-REPO_BASE=""
-if [ "${PKG_PATH##*.}" = "rpm" ]; then
-  REPO="el${REPO_SUFFIX}"
-fi
-
 REPO_PATH=""
-case "$PKG_PATH" in
-  *.el7.src.rpm)    REPO_PATH=7/SRPMS ;;
-  *.el6.src.rpm)    REPO_PATH=6/SRPMS ;;
+case "$PKG_FILENAME" in
+  *.el7.src.rpm)    REPO_PATH=el7${REPO_SUFFIX}/SRPMS ;;
+  *.el6.src.rpm)    REPO_PATH=el6${REPO_SUFFIX}/SRPMS ;;
 
-  *.el7.x86_64.rpm) REPO_PATH=7/x86_64 ;;
-  *.el6.x86_64.rpm) REPO_PATH=6/x86_64 ;;
+  *.el7.x86_64.rpm) REPO_PATH=el7${REPO_SUFFIX}/x86_64 ;;
+  *.el6.x86_64.rpm) REPO_PATH=el6${REPO_SUFFIX}/x86_64 ;;
+
+  *) echo "Do not know how to handle file ${PKG_PATH}."; exit 1 ;;
 esac
 
 
@@ -63,7 +47,7 @@ esac
 # The following command is inspired from an example script provided by bintray:
 #  https://github.com/bintray/bintray-examples/blob/master/bash-example/pushToBintray.sh
 
-echo "Uploading ${PKG_FILENAME} to ${API}/content/${BINTRAY_ORG}/${REPO}/${REPO_PATH}/${PKG_FILENAME}..."
+echo "Uploading ${PKG_FILENAME} to ${API}/content/${BINTRAY_ORG}/${BINTRAY_REPO}/${REPO_PATH}/${PKG_FILENAME}..."
 
 result=$(curl                           \
   -u${BINTRAY_CREDS}                    \
@@ -74,10 +58,12 @@ result=$(curl                           \
   -T ${PKG_PATH}                        \
   -H X-Bintray-Package:${BINTRAY_PKG}   \
   -H X-Bintray-Version:${MESOS_VERSION} \
-  -H X-Bintray-Publish:1                \
-  ${API}/content/${BINTRAY_ORG}/${REPO}/${REPO_PATH}/${PKG_FILENAME})
+  -H X-Bintray-Publish:0                \
+  ${API}/content/${BINTRAY_ORG}/${BINTRAY_REPO}/${REPO_PATH}/${PKG_FILENAME})
 
 if [ $result -ne 201 ]; then
-  echo "Package ${PKG_FILENAME} upload to ${API}/content/${BINTRAY_ORG}/${REPO}/${REPO_PATH}/${PKG_FILENAME} failed with status ${result}"
+  echo "Package ${PKG_FILENAME} upload to ${API}/content/${BINTRAY_ORG}/${BINTRAY_REPO}/${REPO_PATH}/${PKG_FILENAME} failed with status ${result}"
   exit 1
+else
+  echo "Package ${PKG_FILEAME} uploaded successfully, don't forget to head over to bintray to publish the uploaded artifacts."
 fi
