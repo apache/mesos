86ce596a352ed7f6ebd87702c21d669d6c4be7af
Benjamin Mahler <bmahler@apache.org>
2019-10-04 13:53:19 -0400

    Fixed master::Slave::hasExecutor performance issue.
    This is a backport of the broader fix in MESOS-9948.

--- src/master/master.cpp
+++ src/master/master.cpp
@@ -12476,7 +12476,7 @@ bool Slave::hasExecutor(const FrameworkID& frameworkId,
                         const ExecutorID& executorId) const
 {
   return executors.contains(frameworkId) &&
-    executors.get(frameworkId)->contains(executorId);
+    executors.at(frameworkId).contains(executorId);
 }
 
 
